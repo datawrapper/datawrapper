b1a04fe862d4bff8a9767de057590822ea52e828
Gregor Aisch <contact@vis4.net>
2012-05-22 15:17:08 +0200

    removed propel from vendor (use PEAR package instead)

--- vendor/propel/generator/lib/behavior/aggregate_column/templates/queryUpdateRelated.php
+++
@@ -1,8 +0,0 @@
-
-protected function updateRelated<?php echo $relationName ?>s($con)
-{
-	foreach ($this-><?php echo $variableName ?>s as $<?php echo $variableName ?>) {
-		$<?php echo $variableName ?>-><?php echo $updateMethodName ?>($con);
-	}
-	$this-><?php echo $variableName ?>s = array();
-}
--- vendor/propel/generator/lib/behavior/i18n/templates/objectPostDelete.php
+++
@@ -1,5 +0,0 @@
-
-// emulate delete cascade
-<?php echo $i18nQueryName ?>::create()
-	->filterBy<?php echo $objectClassname ?>($this)
-	->delete($con);
