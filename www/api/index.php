190f2d472e85b2fc0214c825b66abd8301670704
Gregor Aisch <contact@vis4.net>
2012-05-23 01:50:08 +0200

    included propel head, moved api functionality into seperate files

--- www/api/index.php
+++ www/api/index.php
@@ -8,9 +8,22 @@
  * directory elsewhere, ensure that it is added to your include path
  * or update this file path as needed.
  */
-require '../../lib/Slim/Slim.php';
+require '../../vendor/Slim/Slim.php';
+
+// include our JSON view
 require 'views/JSONView.php';
+
+// Include the main Propel script
+require_once '../../vendor/propel/runtime/lib/Propel.php';
+
+// Initialize Propel with the runtime configuration
+Propel::init("../../lib/datawrapper/build/conf/datawrapper-conf.php");
+
+// Add the generated 'classes' directory to the include path
+set_include_path("../../lib/datawrapper/build/classes" . PATH_SEPARATOR . get_include_path());
+
 require '../../lib/dw/Datawrapper.php';
+
 
 /**
  * Step 2: Instantiate the Slim application
@@ -19,15 +32,7 @@ require '../../lib/dw/Datawrapper.php';
  * However, we could also pass a key-value array of settings.
  * Refer to the online documentation for available settings.
  */
-$app = new Slim(array(
-    'view' => 'JSONView'
-));
-
-/**
- * Step 2a: Instantiate the Datawrapper application
- *
- */
-Datawrapper::init();
+$app = new Slim(array( 'view' => 'JSONView' ));
 
 /**
  * Step 3: Define the Slim application routes
@@ -52,48 +57,12 @@ $app->get('/', function () use ($app) {
     $app->render('json-error.php', $data, 200);
 });
 
-$app->post('/login', function() {
-    echo 'You are logged in now.';
-});
 
-/* returns the currently selected frontend language */
-$app->get('/lang', function() use ($app) {
-    $lang = DW::getLanguage();
-    $app->render('json-ok.php', array('lang' => $lang));
-});
 
-/* set a new language */
-$app->put('/lang', function() use ($app) {
-    $data = json_decode($app->request()->getBody());
-    DW::setLanguage( $data->lang );
-    echo 'the language has been set to ' . $data->lang;
-});
+require_once '../../lib/dw-api/users.php';
+require_once '../../lib/dw-api/session.php';
+require_once '../../lib/dw-api/charts.php';
 
-/* return a list of all charts by the logged user */
-$app->get('/charts/', function() use ($app) {
-
-});
-
-/* load chart meta data */
-$app->get('/charts/:id', function($id) use ($app) {
-    $res = DW::getChartMetaData($id);
-    $app->render('json-error.php', $res, 200);
-});
-
-/* check user and update chart meta data */
-$app->put('/charts/:id', function($id) use ($app) {
-    if (DW::checkLogin()) {
-        if (DW::chartIsWritable($id)) {
-            $data = json_decode($app->request()->getBody());
-            DW::setChartMetaData($id, $data);
-        } else {
-            $app->render('json-error.php', array('code' => 'access-denied', 'msg' => 'You don\'t have the rights to modifiy this chart'));
-        }
-    } else {
-        $app->render('json-error.php', array('code' => 'need-login', 'msg' => 'you must be logged in'));
-    }
-    $res = DW::setChartMetaData($id);
-});
 
 /**
  * Step 4: Run the Slim application
