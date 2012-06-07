b5a7ca4a0573c9b0daaa129731788e20cebabe6d
Gregor Aisch <contact@vis4.net>
2012-06-07 15:32:00 +0200

    update

--- www/index.php
+++ www/index.php
@@ -148,6 +148,22 @@ $app->get('/chart/:id/describe', function ($id) use ($app) {
     });
 });
 
+
+/**
+ * API: get data to a chart
+ *
+ * @param chart_id chart id
+ */
+$app->get('/chart/:id/data', function($chart_id) use ($app) {
+    $chart = ChartQuery::create()->findPK($chart_id);
+    if (!empty($chart)) {
+        print $chart->loadData();
+    } else {
+        error_chart_not_found($chart_id);
+    }
+});
+
+
 /**
  * Step 4: Run the Slim application
  *
