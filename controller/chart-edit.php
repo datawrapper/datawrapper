1c1578f69a1d1e2cb3e2f0a495df5c4c251a68d6
Gregor Aisch <contact@vis4.net>
2012-06-17 19:14:09 +0200

    update

--- controller/chart-edit.php
+++ controller/chart-edit.php
@@ -2,5 +2,15 @@
 
 // redirects to the last edited step of the chart editor (todo)
 $app->get('/charts/:id/edit', function($chartid) use ($app) {
-    $app->redirect('/chart/'.$chartid.'/upload');
+    check_chart_exists($chartid, function($chart) use ($app) {
+        $step = 'upload';
+        switch ($chart->getLastEditStep()) {
+            case 0:
+            case 1: $step = 'upload'; break;
+            case 2: $step = 'describe'; break;
+            case 3: $step = 'visualize'; break;
+            default: $step = 'publish';
+        }
+        $app->redirect('/chart/'.$chart->getId() . '/' . $step);
+    });
 });
