b8d2e04916429ee7b92c0d6fae70c68f7fdf9de5
Gregor Aisch <contact@vis4.net>
2012-08-19 23:42:48 +0200

    ..

--- www/static/visualizations/bar-chart/bar-chart.js
+++ www/static/visualizations/bar-chart/bar-chart.js
@@ -16,7 +16,6 @@
 
             var me = this,
             sortBars = me.get('sort-values'),
-            isVertical = me.get('orientation', 'vertical') == 'vertical',
             c = me.initCanvas({}),
             chart_width = c.w - c.lpad - c.rpad,
             series_gap = 0.05, // pull from theme
@@ -33,8 +32,6 @@
 
             $('.tooltip').hide();
 
-            if (isVertical) me.horzGrid();
-
             _.each(me.chart.dataSeries(sortBars), function(series, s) {
                 _.each(series.data, function(val, r) {
                     var d = me.barDimensions(series, s, r);
@@ -45,50 +42,26 @@
                         'fill': fill
                     }).data('strokeCol', stroke), series);
 
-                    if (isVertical) {
-                        var val_y = val > 0 ? d.y - 10 : d.y + d.h + 10,
-                            lbl_y = val <= 0 ? d.y - 10 : d.y + d.h + 5;
-                        if (me.chart.isHighlighted(series)) {
-                            // add value labels
-                            me.registerSeriesLabel(me.label(d.x + d.w * 0.5, val_y, me.chart.formatValue(series.data[r]),{
-                                w: d.w,
-                                align: 'center',
-                                cl: 'value'
-                        }
 
-                        // add series label
-                        me.registerSeriesLabel(me.label(d.x + d.w * 0.5, lbl_y, series.name, {
-                            w: d.w,
-                            align: 'center',
-                            valign: val > 0 ? 'top' : 'bottom',
-                            cl: me.chart.isHighlighted(series) ? 'series highlighted' : 'series'
-                        }), series);
+                    var val_x = val > 0 ? d.x + d.w + 10 : d.x- 5,
+                        lbl_x = val <= 0 ? d.x + d.w + 5 : d.x- 10;
+                    me.registerSeriesLabel(me.label(val_x, d.y + d.h * 0.5, me.chart.formatValue(series.data[r]),{
+                        w: 40,
+                        align: val > 0 ? 'left' : 'right',
+                        cl: 'value'
+                    }), series);
 
-                    } else {
-                        var val_x = val > 0 ? d.x + d.w + 10 : d.x- 5,
-                            lbl_x = val <= 0 ? d.x + d.w + 5 : d.x- 10;
-                        me.registerSeriesLabel(me.label(val_x, d.y + d.h * 0.5, me.chart.formatValue(series.data[r]),{
-                            w: 40,
-                            align: val > 0 ? 'left' : 'right',
-                            cl: 'value'
-                        }), series);
-
-                        // add series label
-                        me.registerSeriesLabel(me.label(lbl_x, d.y + d.h * 0.5, series.name,{
-                            w: 80,
-                            align: val <= 0 ? 'left' : 'right',
-                            cl: me.chart.isHighlighted(series) ? 'series highlighted' : 'series'
-                        }), series);
-                    }
+                    // add series label
+                    me.registerSeriesLabel(me.label(lbl_x, d.y + d.h * 0.5, series.name,{
+                        w: 80,
+                        align: val <= 0 ? 'left' : 'right',
+                        cl: me.chart.isHighlighted(series) ? 'series highlighted' : 'series'
+                    }), series);
 
                 });
             });
 
-            if (isVertical) {
-                var y = c.h - me.__scales.y(0) - c.bpad;
-                me.path([['M', c.lpad, y], ['L', c.w - c.rpad, y]], 'axis')
-                    .attr(me.theme.yAxis);
-            } else if (me.__domain[0] < 0) {
+            if (me.__domain[0] < 0) {
                 var x = c.lpad + me.__scales.y(0);
                 me.path([['M', x, c.tpad], ['L', x, c.h - c.bpad]], 'axis')
                     .attr(me.theme.yAxis);
@@ -111,42 +84,24 @@
                 y: d3.scale.linear().domain([dMin, dMax])
             };
 
-            if (me.get('orientation', 'vertical') == 'horizontal') {
-                me.__scales.y.rangeRound([c.lpad, c.w-c.rpad-c.lpad-30]);
-            } else {
-                me.__scales.y.rangeRound([0, c.h - c.bpad - c.tpad]);
-            }
+            me.__scales.y.rangeRound([c.lpad, c.w-c.rpad-c.lpad-30]);
         },
 
         barDimensions: function(series, s, r) {
             var me = this, w, h, x, y, i, cw, n = me.chart.dataSeries().length,
                 sc = me.__scales, c = me.__canvas, bw, pad = 0.35, vspace = 0.1;
 
-            if (me.get('orientation', 'vertical') == 'horizontal') {
-                cw = c.h - c.bpad - c.tpad;
-                bw = Math.min(24, cw / (n + (n-1) * pad));
-                w = sc.y(series.data[r]) - sc.y(0);
-                h = bw;
-                if (w > 0) {
-                    x = c.lpad + sc.y(0);
-                } else {
-                    x = c.lpad + sc.y(0) + w;
-                    w *= -1;
-                }
-                y = Math.round(c.tpad + s * (bw + bw * pad));
+            cw = c.h - c.bpad - c.tpad;
+            bw = Math.min(24, cw / (n + (n-1) * pad));
+            w = sc.y(series.data[r]) - sc.y(0);
+            h = bw;
+            if (w > 0) {
+                x = c.lpad + sc.y(0);
             } else {
-                cw = (c.w - c.lpad - c.rpad) * (1 - vspace - vspace);
-                bw = cw / (n + (n-1) * pad);
-                h = sc.y(series.data[r]) - sc.y(0);
-                w = bw;
-                if (h >= 0) {
-                    y = c.h - c.bpad - sc.y(0) - h;
-                } else {
-                    y = c.h - c.bpad - sc.y(0);
-                    h *= -1;
-                }
-                x = Math.round((c.w - c.lpad - c.rpad) * vspace + c.lpad + s * (bw + bw * pad));
+                x = c.lpad + sc.y(0) + w;
+                w *= -1;
             }
+            y = Math.round(c.tpad + s * (bw + bw * pad));
             return { w: w, h: h, x: x, y: y };
         },
 
@@ -160,32 +115,6 @@
 
         hideTooltip: function() {
             
-        },
-
-        horzGrid: function() {
-            // draw tick marks and labels
-            var me = this,
-                yscale = me.__scales.y,
-                c = me.__canvas,
-                domain = me.__domain,
-                styles = me.__styles,
-                ticks = me.getYTicks(c.h);
-
-            _.each(ticks, function(val, t) {
-                if (t == ticks.length-1) return;
-                var y = c.h - c.bpad - yscale(val), x = c.lpad;
-                if (val >= domain[0] && val <= domain[1]) {
-                    // c.paper.text(x, y, val).attr(styles.labels).attr({ 'text-anchor': 'end' });
-                    me.label(x+2, y-10, me.chart.formatValue(val, t == ticks.length-1), { align: 'left', cl: 'axis' });
-                    if (me.theme.yTicks) {
-                        me.path([['M', c.lpad-25, y], ['L', c.lpad-20,y]], 'tick');
-                    }
-                    if (me.theme.horizontalGrid) {
-                        me.path([['M', c.lpad, y], ['L', c.w - c.rpad,y]], 'grid')
-                            .attr(me.theme.horizontalGrid);
-                    }
-                }
-            });
         },
 
         hoverSeries: function(series) {
