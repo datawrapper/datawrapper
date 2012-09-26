6c9c7f4f4a8e2a8adec38c244646a4aeceaa65e9
Gregor Aisch <contact@vis4.net>
2012-09-26 17:23:58 +0200

    allowed to limit themes to domains or email addresses

--- lib/utils/themes.php
+++ lib/utils/themes.php
@@ -4,12 +4,19 @@
 
 function get_themes_meta() {
     $res = array();
+    $user = DatawrapperSession::getInstance()->getUser();
+    $email = $user->getEmail();
+    $domain = substr($email, strpos($email, '@'));
     $files = glob('static/themes/*/meta.json');
     if (count($files) > 0) {
         foreach ($files as $file) {
             $id = substr($file, 14, -10);
             $meta = get_theme_meta($id);
-            $res[] = $meta;
+            if (!isset($meta['restrict'])  // no restriction at all
+                 || $meta['restrict'] == $domain  // check for email domain
+                 || $meta['restrict'] == $email  // check for entire email address
+                 || $user->isAdmin())  // of course, admins can see all, too
+                $res[] = $meta;
         }
     }
     return $res;
