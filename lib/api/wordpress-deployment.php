f6bd30eff872b483138479f24c2285860e90c92b
Gregor Aisch <contact@vis4.net>
2012-09-27 10:35:21 +0200

    removed localhost from allows wordpress ips

--- lib/api/wordpress-deployment.php
+++ lib/api/wordpress-deployment.php
@@ -8,7 +8,7 @@ $app->post('/wordpress', function() use ($app) {
 
     if (defined('WORDPRESS_SECRET')) {
         // check that the request comes from the right server
-        $wordpress_ips = array('85.13.138.130', '127.0.0.1');
+        $wordpress_ips = array('85.13.138.130');
         if (in_array($_SERVER['REMOTE_ADDR'], $wordpress_ips)) {
             if ($app->request()->post('secret') == WORDPRESS_SECRET) {
                 file_put_contents("../../scripts/new_wp_content.txt", "1");
