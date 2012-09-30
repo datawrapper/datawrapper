a488fd66afbc4cdbd010b57145fe5934123b929d
Gregor Aisch <contact@vis4.net>
2012-10-01 00:15:16 +0200

    changed IP for wp deployment

--- lib/api/wordpress-deployment.php
+++ lib/api/wordpress-deployment.php
@@ -8,7 +8,7 @@ $app->post('/wordpress', function() use ($app) {
 
     if (defined('WORDPRESS_SECRET')) {
         // check that the request comes from the right server
-        $wordpress_ips = array('85.13.138.130');
+        $wordpress_ips = array('176.9.139.73');
         if (in_array($_SERVER['REMOTE_ADDR'], $wordpress_ips)) {
             if ($app->request()->post('secret') == WORDPRESS_SECRET) {
                 file_put_contents("../../scripts/new_wp_content.txt", "1");
