291e51fd4bf32116dbc9243a89c1fe297c575e40
Gregor Aisch <contact@vis4.net>
2012-09-30 22:04:37 +0200

    use ?popup=1 to get content without header and footer

--- controller/xhr.php
+++ controller/xhr.php
@@ -44,32 +44,3 @@ $app->get('/xhr/:chartid/vis-options', function($id) use ($app) {
     });
 });
 
-/**
- * load a page from the docs for a modal window
- */
-require_once '../templates/imported/pages.inc.php';
-
-$urls = array();
-
-foreach ($docs_pages as $lang => $pages) {
-    foreach ($pages as $url => $p) {
-        if (!in_array($url, $urls)) $urls[] = $url;
-    }
-}
-
-foreach ($urls as $url) {
-    $app->get('/popup/' . $url, function() use ($app, $url, $docs_pages) {
-        $page = array();
-        add_header_vars($page, 'about');
-        $lang = $page['language'];
-        $tpl_path = 'imported/' . $lang . '/' . str_replace('/', '-', $url) . '.twig';
-        if (!empty($docs_pages[$lang]) && !empty($docs_pages[$lang][$url])) {
-            $page['title'] = $docs_pages[$lang][$url]['title'];
-            add_docs_vars($page, $url);
-            $page['xhr'] = true;
-            $app->render($tpl_path, $page);
-        } else {
-            error_page('about', _('Not found'), _('Sorry, but this content is not available in your language, yet.'));
-        }
-    });
-}
