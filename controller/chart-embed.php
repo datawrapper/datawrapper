23c6b08a59d5a73843fb88c7b6c15ac6fde81228
Gregor Aisch <contact@vis4.net>
2012-10-22 09:16:42 +0200

    making sure that "/" is appended to chart urls

--- controller/chart-embed.php
+++ controller/chart-embed.php
@@ -7,7 +7,7 @@ require_once '../lib/utils/chart_content.php';
 /*
  * Shows a preview of a chart for display in an iFrame
  */
-$app->get('/chart/:id/?', function ($id) use ($app) {
+$app->get('/chart/:id/', function ($id) use ($app) {
     check_chart_public($id, function($user, $chart) use ($app) {
         $page = get_chart_content($chart, $user, $app->request()->get('minify') == 1);
         $page['padding'] = $app->request()->params('padding');
@@ -16,4 +16,8 @@ $app->get('/chart/:id/?', function ($id) use ($app) {
         $res['Cache-Control'] = 'max-age=0';
         $app->render('chart.twig', $page);
     });
+});
+
+$app->get('/chart/:id', function ($id) use ($app) {
+    $app->redirect('/chart/' . $id . '/');
 });
