361d59f3f40cacb07999bda5e4c9073bda601cac
Gregor Aisch <contact@vis4.net>
2012-12-20 17:14:32 +0100

    adding thumb mode (no texts and labels)

--- controller/chart-embed.php
+++ controller/chart-embed.php
@@ -12,7 +12,7 @@ $app->get('/chart/:id/', function ($id) use ($app) {
 
     check_chart_public($id, function($user, $chart) use ($app) {
         $page = get_chart_content($chart, $user, $app->request()->get('minify') == 1);
-        $page['padding'] = $app->request()->params('padding');
+        $page['thumb'] = $app->request()->params('t') == 1;
         $page['innersvg'] = $app->request()->get('innersvg') == 1;
         $page['plain'] = $app->request()->get('plain') == 1;
         $page['fullscreen'] = $app->request()->get('fs') == 1;
