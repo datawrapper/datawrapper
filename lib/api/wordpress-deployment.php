42867ce63a3057f8673f985b602dc7fea523bb54
Gregor Aisch <contact@vis4.net>
2013-01-25 21:29:33 +0100

    Always allow content pushing from same IP

--- lib/api/wordpress-deployment.php
+++ lib/api/wordpress-deployment.php
@@ -1,7 +1,9 @@
 <?php
 
 /*
- * accepts push notifications from wordpress
+ * Accepts push notifications from Wordpress after the Datawrapper
+ * documentation has been updated. This sets a flag which forces a
+ * /scripts/update_content.phpto pull new content via Wordpress JSON API.
  */
 
 $app->post('/wordpress', function() use ($app) {
@@ -9,6 +11,8 @@ $app->post('/wordpress', function() use ($app) {
     if (isset($GLOBALS['dw_config']['wordpress']['secret'])) {
         // check that the request comes from the right server
         $wordpress_ips = $GLOBALS['dw_config']['wordpress']['ips'];
+        // always allow pushing from same IP
+        $wordpress_ips[] = $_SERVER['SERVER_ADDR'];
         if (in_array($_SERVER['REMOTE_ADDR'], $wordpress_ips)) {
             if ($app->request()->post('secret') == $GLOBALS['dw_config']['wordpress']['secret']) {
                 file_put_contents("../../scripts/new_wp_content.txt", "1");
@@ -21,6 +25,3 @@ $app->post('/wordpress', function() use ($app) {
     $app->notFound();
 });
 
-$app->get('/worpress', function() use ($app) {
-    $app->notFound();
-});
