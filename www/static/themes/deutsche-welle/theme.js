5420dbf3202f170654495c78b6e00e06947c64b7
Gregor Aisch <contact@vis4.net>
2013-01-30 11:55:17 +0100

    fixed issues with deutsche-welle theme

--- www/static/themes/deutsche-welle/theme.js
+++ www/static/themes/deutsche-welle/theme.js
@@ -1,27 +1,29 @@
 (function(){
 
-    // Default Theme
-    // -------------
-
-    // Every theme will inherit the properties of this
-    // theme. They can override everything or just a bit
-    // of them. Also, every theme can extend any other
-    // existing theme.
+    // Deutsche Welle Theme
+    // --------------------
 
     Datawrapper.Themes.DeutscheWelle = $.extend(true, {}, Datawrapper.Themes.Base, {
 
-        hpadding: 20,
-        vpadding: -10,
+        hpadding: 0,
+        vpadding: 10,
 
         colors: {
             palette: ['#DC0F6E', '#009BFF', '#82B905', '#82141E', '#E14614', '#C8D205', '#EB8C14'],
             positive: '#009BFF',
             negative: '#DC0F6E',
-            background: '#E6E3E0'
+            background: '#ffffff'
         },
 
         horizontalGrid: {
             stroke: '#D2CEC7'
+        },
+
+        padding: {
+            left: 20,
+            right: 20,
+            bottom: 20,
+            top: 10
         }
     });
 
