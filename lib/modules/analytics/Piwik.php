bb86ff4ddbbb20b4cee9a63791765dba8ddde3db
Gregor Aisch <contact@vis4.net>
2013-02-05 09:07:06 +0100

    optimized tracking (custom url, custom variables)

--- lib/modules/analytics/Piwik.php
+++ lib/modules/analytics/Piwik.php
@@ -24,7 +24,12 @@ _paq.push(["setSiteId", ' . $idSite . ']);
 _paq.push(["setTrackerUrl", u+"piwik.php"]);
 _paq.push(["trackPageView"]);
 _paq.push(["enableLinkTracking"]);
-var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript"; g.defer=true; g.async=true; g.src=u+"piwik.js";
+' . ($chart != null ? '_paq.push(["setCustomUrl", "/chart/'.$chart->getId().'"]);'."\n"
+    .'_paq.push(["setCustomVariable", "1", "Layout", "'.$chart->getTheme().'", "page"]);'."\n"
+    .'_paq.push(["setCustomVariable", "2", "Author", "'.$chart->getUser()->getEmail().'", "page"]);'."\n"
+    .'_paq.push(["setCustomVariable", "3", "Visualization", "'.$chart->getType().'", "page"]);'."\n"
+ : '') .
+'var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript"; g.defer=true; g.async=true; g.src=u+"piwik.js";
 s.parentNode.insertBefore(g,s); })();
  </script>
 <!-- End Piwik Code -->';
