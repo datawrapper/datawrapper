8aced55826835c288fd780e00524a3930ffa1b42
Gregor Aisch <contact@vis4.net>
2013-02-21 16:45:59 +0100

    fixed utils/themes.php

--- lib/utils/themes.php
+++ lib/utils/themes.php
@@ -7,10 +7,10 @@ function get_themes_meta($path = '') {
     $user = DatawrapperSession::getInstance()->getUser();
     $email = $user->getEmail();
     $domain = substr($email, strpos($email, '@'));
-    $files = glob($path . 'static/themes/*/meta.json');
+    $files = glob(ROOT_PATH . 'www/static/themes/*/meta.json');
     if (count($files) > 0) {
         foreach ($files as $file) {
-            $id = substr($file, 14, -10);
+            $id = substr($file, strlen(ROOT_PATH) + 18, -10);
             $meta = get_theme_meta($id, $path);
             if (!$meta) continue;
             if (!isset($meta['restrict'])  // no restriction at all
@@ -24,12 +24,12 @@ function get_themes_meta($path = '') {
 }
 
 function get_theme_meta($id, $path = '') {
-    $theme_meta = $path . 'static/themes/' . $id . '/meta.json';
+    $theme_meta = ROOT_PATH . 'www/static/themes/' . $id . '/meta.json';
     if (file_exists($theme_meta)) {
         $meta = json_decode(file_get_contents($theme_meta), true);
         $meta['id'] = $id;
-        $meta['hasStyles'] = file_exists($path . 'static/themes/' . $id . '/theme.css');
-        $meta['hasTemplate'] = file_exists('../templates/themes/' . $id . '.twig');
+        $meta['hasStyles'] = file_exists(ROOT_PATH . 'www/static/themes/' . $id . '/theme.css');
+        $meta['hasTemplate'] = file_exists(ROOT_PATH . 'templates/themes/' . $id . '.twig');
 
         if (!empty($meta['locale'])) {
             $localeJS = 'static/vendor/globalize/cultures/globalize.culture.' . str_replace('_', '-', $meta['locale']) . '.js';
