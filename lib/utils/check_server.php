482edec98e24eca06ed0f857c5b7a1c77f2bfee4
Gregor Aisch <contact@vis4.net>
2013-02-27 19:14:54 +0100

    added check for config.yaml

--- lib/utils/check_server.php
+++ lib/utils/check_server.php
@@ -14,34 +14,51 @@ function check_path_permissions() {
     foreach ($paths as $path) {
         if (!is_writable($rel . $path)) $err[] = $path;
     }
-    return $err;
+    if (count($err) > 0) {
+        $msg = '<h2>The following folders on your server need to be writable:</h2><ul>';
+        foreach ($paths as $path) {
+            # code...
+            $msg .= '<li><code>'.dirname(dirname(dirname(__FILE__))).$path.'</code></li>';
+        }
+        $msg .= '</ul>';
+        $msg .= 'Read more about <a href="http://codex.wordpress.org/Changing_File_Permissions#Using_an_FTP_Client">how to change file permissions</a>';
+        return $msg;
+    }
+    return '';
+}
+
+function check_config() {
+    if (!file_exists('../config.yaml')) {
+        $error = true;
+        $info['config_missing'] = true;
+        return '<h2>Could not find <b>config.yaml</b></h2>'
+          . '<p>Please copy the template configuration from <code>config.template.yaml</code> to <code>config.yaml</code> and change the configuration according to your server.</p>';
+    }
+    return '';
 }
 
 function check_database() {
     
 }
 
+
 function check_server() {
-    $error = false;
-    $paths = check_path_permissions();
-    if (count($paths) > 0) $error = true;
+    $check = array();
+    $check[] = 'check_config';
+    $check[] = 'check_path_permissions';
+    $check[] = 'check_database';
 
-    if ($error) {
-        print '<html><head><title>Datawrapper</title></head><body>';
-        print '<div style="border-radius:20px;background:#ffc; border:1px solid #eea; padding: 30px;width:700px;margin:30px auto;font-size:18px;font-family:Helvetica Neue;font-weight:300">';
-        print '<style>h2 { font-weight: 400; font-size: 20px; color: #b20 } ul li { font-size: 18px }</style>';
-        print '<h1 style="margin:0 0 30px;font-size:32px;line-height:30px;letter-spacing:-1px;color:#531">Whoops! There\'s something wrong with your Datawrapper instance!</h1>';
-        if (count($paths) > 0) {
-            print '<h2>The following folders on your server need to be writable:</h2><ul>';
-            foreach ($paths as $path) {
-                # code...
-                print '<li><code>'.dirname(dirname(dirname(__FILE__))).$path.'</code></li>';
-            }
-            print '</ul>';
-            print 'Read more about <a href="http://codex.wordpress.org/Changing_File_Permissions#Using_an_FTP_Client">how to change file permissions</a>';
+    foreach ($check as $func) {
+        $msg = call_user_func($func);
+        if (!empty($msg)) {
+            print '<html><head><title>Datawrapper</title></head><body>';
+            print '<div style="border-radius:20px;background:#ffc; border:1px solid #eea; padding: 30px;width:700px;margin:30px auto;font-size:18px;font-family:Helvetica Neue;font-weight:300">';
+            print '<style>h2 { font-weight: 400; font-size: 20px; color: #b20 } ul li { font-size: 18px }</style>';
+            print '<h1 style="margin:0 0 30px;font-size:32px;line-height:30px;letter-spacing:-1px;color:#531">Whoops! Something is wrong with your Datawrapper instance!</h1>';
+
+            print $msg;
+            print '</div></body></html>';
+            exit();
         }
-
-        print '</div></body></html>';
-        exit();
-    }
+     }
 }
