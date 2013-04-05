429b26be5a9afaaa19cf7ea818ee8a703ada3f1a
Gregor Aisch <contact@vis4.net>
2013-04-05 21:16:05 +0200

    updated to requests 1.0

--- test/api.test.py
+++ test/api.test.py
@@ -31,16 +31,16 @@ ns = {
 class TestDatawrapperAPI(unittest.TestCase):
 
     def checkRes(self, r):
-        self.assertIsInstance(r.json, dict)
-        self.assertEqual(r.json['status'], 'ok')
-        if r.json['status'] == 'error':
-            print r.json['message']
+        self.assertIsInstance(r.json(), dict)
+        self.assertEqual(r.json()['status'], 'ok')
+        if r.json()['status'] == 'error':
+            print r.json()['message']
 
     def test_01_create_new_chart(self):
         global ns
         r = ns['session'].post(endpoint + 'charts')
         self.checkRes(r)
-        ns['chartId'] = r.json['data'][0]['id']
+        ns['chartId'] = r.json()['data'][0]['id']
 
     def test_02_set_chart_data(self):
         data = 'some,data,to,send\nanother,row,to,send\n'
@@ -52,7 +52,8 @@ class TestDatawrapperAPI(unittest.TestCase):
         self.assertEqual(r.text, data)
 
     def test_03_upload_chart_data(self):
-        files = {'qqfile': ('report.csv', 'other,data,to,send\nanother,row,to,send\n')}
+        files = {'qqfile': (
+            'report.csv', 'other,data,to,send\nanother,row,to,send\n')}
         url = endpoint + 'charts/%s/data' % ns['chartId']
         r = ns['session'].post(url, files=files)
         self.checkRes(r)
@@ -64,19 +65,19 @@ class TestDatawrapperAPI(unittest.TestCase):
         url = endpoint + 'charts/%s' % ns['chartId']
         r = ns['session'].get(url)
         self.checkRes(r)
-        self.assertEqual(r.json['data']['showInGallery'], False)
+        self.assertEqual(r.json()['data']['showInGallery'], False)
 
     def test_05_saveMetadata(self):
         url = endpoint + 'charts/%s' % ns['chartId']
         r = ns['session'].get(url)
         self.checkRes(r)
-        data = r.json['data']
+        data = r.json()['data']
         data['title'] = 'My cool new chart'
         data['metadata']['describe']['source-name'] = 'Example Data Source'
         data['metadata']['describe']['source-url'] = 'http://example.org'
         r = ns['session'].put(url, data=json.dumps(data))
         self.checkRes(r)
-        #self.assertEqual(r.json['data']['showInGallery'], False)
+        # self.assertEqual(r.json()['data']['showInGallery'], False)
 
     def test_06_gallery(self):
         url = endpoint + 'gallery'
@@ -87,20 +88,20 @@ class TestDatawrapperAPI(unittest.TestCase):
         url = endpoint + 'visualizations'
         r = ns['session'].get(url)
         self.checkRes(r)
-        self.assertIsInstance(r.json['data'], list)
+        self.assertIsInstance(r.json()['data'], list)
 
     def test_07_bar_chart(self):
         url = endpoint + 'visualizations/bar-chart'
         r = ns['session'].get(url)
         self.checkRes(r)
-        self.assertIsInstance(r.json['data'], dict)
+        self.assertIsInstance(r.json()['data'], dict)
 
     def test_08_account(self):
         url = endpoint + 'account'
         r = ns['session'].get(url)
         self.checkRes(r)
-        self.assertIn('user', r.json['data'])
-        self.assertIsInstance(r.json['data']['user'], dict)
+        self.assertIn('user', r.json()['data'])
+        self.assertIsInstance(r.json()['data']['user'], dict)
 
     def test_09_set_lang_to_fr(self):
         url = endpoint + 'account/lang'
@@ -111,19 +112,19 @@ class TestDatawrapperAPI(unittest.TestCase):
         url = endpoint + 'account/lang'
         r = ns['session'].get(url)
         self.checkRes(r)
-        self.assertEqual(r.json['data'], 'fr')
+        self.assertEqual(r.json()['data'], 'fr')
 
     def test_11_charts(self):
         url = endpoint + 'charts'
         r = ns['session'].get(url)
         self.checkRes(r)
-        self.assertEqual(len(r.json['data']), 1)
+        self.assertEqual(len(r.json()['data']), 1)
 
     def test_11a_charts_sorted(self):
         url = endpoint + 'charts?order=theme'
         r = ns['session'].get(url)
         self.checkRes(r)
-        self.assertEqual(len(r.json['data']), 1)
+        self.assertEqual(len(r.json()['data']), 1)
 
     def test_12_estimate_job(self):
         url = endpoint + 'jobs/export/estimate'
@@ -134,7 +135,7 @@ class TestDatawrapperAPI(unittest.TestCase):
         url = endpoint + '/users'
         password = '1234'
         body = dict(pwd=password, pwd2=password,
-            email=('test-%d@' + config['domain']) % randint(10000, 99999))
+                    email=('test-%d@' + config['domain']) % randint(10000, 99999))
         r = ns['session'].post(url, data=json.dumps(body))
         self.checkRes(r)
 
