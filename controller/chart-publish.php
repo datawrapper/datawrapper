3fe387f327ad5f0405f970d81b09908118065df4
Gregor Aisch <contact@vis4.net>
2013-04-11 15:13:47 +0200

    added 'index.html' to local chart url

--- controller/chart-publish.php
+++ controller/chart-publish.php
@@ -16,7 +16,7 @@ $app->get('/chart/:id/publish', function ($id) use ($app) {
     check_chart_writable($id, function($user, $chart) use ($app) {
 
         $cfg = $GLOBALS['dw_config'];
-        $local_url = 'http://' . $cfg['chart_domain'] . '/' . $chart->getID() . '/';
+        $local_url = 'http://' . $cfg['chart_domain'] . '/' . $chart->getID() . '/index.html';
         $public_url = $chart->getPublicUrl();
 
         if (empty($public_url)) $public_url = $local_url;
