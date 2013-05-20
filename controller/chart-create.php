0198aae895b7c382029eb876fd8edee17070816e
Gregor Aisch <contact@vis4.net>
2013-05-21 01:40:16 +0200

    ignoring prevent_guest_chart if not set

--- controller/chart-create.php
+++ controller/chart-create.php
@@ -3,8 +3,10 @@
 $app->get('/chart/create', function() use ($app) {
     disable_cache($app);
 
+    $cfg = $GLOBALS['dw_config'];
+
     $user = DatawrapperSession::getUser();
-    if (!$user->isLoggedIn() && $GLOBALS['dw_config']['prevent_guest_charts']) {
+    if (!$user->isLoggedIn() && isset($cfg['prevent_guest_charts']) && $cfg['prevent_guest_charts']) {
         error_access_denied();
     } else {
         $chart = ChartQuery::create()->createEmptyChart($user);
