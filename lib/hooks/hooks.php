e2208bfa6b81d5ee6ce8622426f750bfebe9f4f0
Pierre Bellon <bellon.pierre@gmail.com>
2013-06-08 18:27:47 +0200

    Created datawrapper hook system files

---
+++ lib/hooks/hooks.php
@@ -0,0 +1,10 @@
+<?php
+/** 
+ * This file is basicly a declaration of all hooks handled by datawrapper,
+ * plugin may have their own hook that will be called by other plugins. This 
+ * class doesn't cover that. 
+ */
+class Hooks 
+{
+    const TEST = 'test';
+}
