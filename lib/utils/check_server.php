08862bc9d7a4fb21742d8639a869a0c34b1ccd20
Gregor Aisch <contact@vis4.net>
2013-06-09 03:10:42 +0200

    initializing array properly

--- lib/utils/check_server.php
+++ lib/utils/check_server.php
@@ -74,6 +74,7 @@ function check_database() {
 
 function check_plugins() {
     $res = mysql_query('SELECT id FROM plugin WHERE enabled = 1');
+    $missing = array();
     while ($row = mysql_fetch_assoc($res)) {
         if (!file_exists(ROOT_PATH . 'plugins/' . $row['id'] . '/plugin.php')) {
             $missing[] = $row['id'];
