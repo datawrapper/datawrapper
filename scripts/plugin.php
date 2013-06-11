73a1808172f95525038f2d8d67ab279dd60bf271
Gregor Aisch <contact@vis4.net>
2013-06-11 10:06:45 +0200

    plugin installer can now handle wildcards

--- scripts/plugin.php
+++ scripts/plugin.php
@@ -17,37 +17,50 @@ date_default_timezone_set('Europe/Berlin');
 require_once ROOT_PATH . 'lib/bootstrap.php';
 
 $cmd = $argv[1];
-$plugin_id = $argv[2];
+$pattern = $argv[2];
 
+$plugin_ids = array();
 
-$plugin = new Plugin();
-$plugin->setId($plugin_id);
+foreach (glob(ROOT_PATH . "plugins/" . $pattern . "/package.json") as $filename) {
+    $d = dirname($filename);
+    $d = substr($d, strrpos($d, DIRECTORY_SEPARATOR)+1);
+    $plugin_ids[] = $d;
+}
 
-if (file_exists($plugin->getPath())) {
-    if (file_exists($plugin->getPath() . 'plugin.php')) {
-        require_once $plugin->getPath() . 'plugin.php';
-        $className = $plugin->getClassName();
-        $pluginClass = new $className();
-    } else {
-        // no plugin.php
-        $pluginClass = new DatawrapperPlugin($plugin->getName());
-    }
-
-    switch ($cmd) {
-        case 'enable':
-        case 'install':
-            print "Installing " . $pluginClass->getName() . "\n";
-            $pluginClass->install();
-            break;
-        case 'disable':
-        case 'uninstall':
-            print "Uninstalling " . $pluginClass->getName() . "\n";
-            $pluginClass->uninstall();
-            break;
-    }
-
-} else {
-    print 'Warning: Plugin not found: '.$plugin_id."\n";
+if (empty($plugin_ids)) {
+    print "No matching plugin found.\n";
     exit(1);
 }
 
+foreach ($plugin_ids as $plugin_id) {
+    $plugin = new Plugin();
+    $plugin->setId($plugin_id);
+
+    if (file_exists($plugin->getPath())) {
+        if (file_exists($plugin->getPath() . 'plugin.php')) {
+            require_once $plugin->getPath() . 'plugin.php';
+            $className = $plugin->getClassName();
+            $pluginClass = new $className();
+        } else {
+            // no plugin.php
+            $pluginClass = new DatawrapperPlugin($plugin->getName());
+        }
+
+        switch ($cmd) {
+            case 'enable':
+            case 'install':
+                print "Installing " . $pluginClass->getName() . "\n";
+                $pluginClass->install();
+                break;
+            case 'disable':
+            case 'uninstall':
+                print "Uninstalling " . $pluginClass->getName() . "\n";
+                $pluginClass->uninstall();
+                break;
+        }
+
+    } else {
+        print 'Warning: Plugin not found: '.$plugin_id."\n";
+        exit(1);
+    }
+}
