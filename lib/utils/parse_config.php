82a713ce16a7bb15606a6f890e47e2b1626fbf19
Gregor Aisch <contact@vis4.net>
2013-06-12 14:26:44 +0200

    added adress for error mails

--- lib/utils/parse_config.php
+++ lib/utils/parse_config.php
@@ -10,16 +10,24 @@ function parse_config() {
     // check that email adresses are set
     if (!isset($cfg['email']))
         $cfg['email'] = array();
+
     if (!isset($cfg['email']['admin']))
         $cfg['email']['admin'] = 'admin@' . $cfg['domain'];
+
     if (!isset($cfg['email']['log']))
         $cfg['email']['log'] = 'admin@' . $cfg['domain'];
+
     if (!isset($cfg['email']['pwd_reset']))
         $cfg['email']['pwd_reset'] = 'password-reset@' . $cfg['domain'];
+
     if (!isset($cfg['email']['activate']))
         $cfg['email']['activate'] = 'activate@' . $cfg['domain'];
+
     if (!isset($cfg['email']['invite']))
         $cfg['email']['invite'] = 'invite@' . $cfg['domain'];
+
+    if (!isset($cfg['email']['error']))
+        $cfg['email']['error'] = 'error@' . $cfg['domain'];
 
     $GLOBALS['dw_config'] = $cfg;
 }
