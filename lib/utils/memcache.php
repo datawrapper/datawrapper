b025bf3b5d3201e45bc34951fb30269c28190413
Gregor Aisch <contact@vis4.net>
2013-06-13 14:02:21 +0200

    no global required here

--- lib/utils/memcache.php
+++ lib/utils/memcache.php
@@ -2,7 +2,6 @@
 
 if (isset($_GLOBALS['dw-config']['memcache'])) {
     $memcfg = $_GLOBALS['dw-config']['memcache'];
-    global $memcache;
-    $memcache = new Memcache;
+    $memcache = new Memcache();
     $memcache->connect($memcfg['host'], $memcfg['port']) or die ("Could not connect");
 }
