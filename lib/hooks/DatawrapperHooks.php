83f4a6c2ade68ff8dcbac97df558884a5a957fe3
Gregor Aisch <contact@vis4.net>
2013-06-13 09:49:58 +0200

    added hookRegistered()

--- lib/hooks/DatawrapperHooks.php
+++ lib/hooks/DatawrapperHooks.php
@@ -70,6 +70,16 @@ class DatawrapperHooks {
         return $results;
     }
 
+    /*
+     * Checks whether a hook has been registered or if executing
+     * that hook would lead to no action.
+     * @param $hookName - the name of hook to register (see Core::Hooks)
+     */
+    public static function hookRegistered($hookName) {
+        $me = self::getInstance();
+        return isset($me->hooks[$hookName]);
+    }
+
     // print something below a charts HTML body
     const AFTER_CHART_BODY = 'after_chart_body';
 
