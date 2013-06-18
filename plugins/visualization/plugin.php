3e96dcd671c33d50f7a341b3bde08f1820517d1c
Bellon Pierre <bellon.pierre@gmail.com>
2013-06-18 16:20:55 +0200

    Registered GET_DEMO_DATASETS into visualization plugin, added getDemoDataSets function

--- plugins/visualization/plugin.php
+++ plugins/visualization/plugin.php
@@ -4,11 +4,31 @@ class DatawrapperPlugin_Visualization extends DatawrapperPlugin {
 
     public function init() {
         $meta = $this->getMeta();
+        $datasets = $this->getDemoDataSets();
+        $demo_hook = DatawrapperHooks::GET_DEMO_DATASETS;
         if (!empty($meta)) DatawrapperVisualization::register($this, $meta);
+        if (!empty($datasets)){
+        	$first_element =  reset($datasets);
+        	if(is_array($first_element)){
+        		foreach ($datasets as $key => $dataset) {
+        			DatawrapperHooks::register($demo_hook, function() use ($dataset){
+        				return $dataset;
+        			});
+        		}
+        	} else {
+        		DatawrapperHooks::register($demo_hook, function() use ($datasets){
+        			return $datasets;
+        		});
+        	}
+        }
     }
 
     public function getMeta() {
         return array();
+    }
+
+    public function getDemoDataSets() {
+    	return array();
     }
 
 }
