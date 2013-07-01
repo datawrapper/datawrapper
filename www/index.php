3996d61725e1a401b07f6f94c5be3d8c544cfa64
Gregor Aisch <contact@vis4.net>
2013-07-01 17:24:28 +0200

    bumped version number

--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@
 
 
 
-define('DATAWRAPPER_VERSION', '1.4.2');  // must be the same as in package.json
+define('DATAWRAPPER_VERSION', '1.4.3');  // must be the same as in package.json
 
 define('ROOT_PATH', '../');
 
