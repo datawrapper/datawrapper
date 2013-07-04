59579bc6d9f5ed3fcb86e7f2585e807f419da19d
Gregor Aisch <contact@vis4.net>
2013-07-04 09:57:45 +0200

    renamed hooks

--- plugins/analytics-piwik/plugin.php
+++ plugins/analytics-piwik/plugin.php
@@ -8,8 +8,8 @@
 class DatawrapperPlugin_AnalyticsPiwik extends DatawrapperPlugin {
 
     public function init() {
-        DatawrapperHooks::register(DatawrapperHooks::AFTER_CHART_BODY, array($this, 'getTrackingCode'));
-        DatawrapperHooks::register(DatawrapperHooks::AFTER_CORE_BODY, array($this, 'getTrackingCode'));
+        DatawrapperHooks::register(DatawrapperHooks::CHART_AFTER_BODY, array($this, 'getTrackingCode'));
+        DatawrapperHooks::register(DatawrapperHooks::CORE_AFTER_BODY, array($this, 'getTrackingCode'));
     }
 
   	public function getTrackingCode($chart = null) {
