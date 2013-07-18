40e79e48ba77ba876ac908892b9fb9247e888b98
Gregor Aisch <contact@vis4.net>
2013-07-18 15:52:11 +0200

    build

--- dw.js/dw-2.0.js
+++ dw.js/dw-2.0.js
@@ -1283,13 +1283,37 @@ dw.utils = {
 
     name: function(obj) {
         return _.isFunction(obj.name) ? obj.name() : _.isString(obj.name) ? obj.name : obj;
+    },
+
+    getMaxChartHeight: function(el) {
+        function margin(el, type) {
+            if ($(el).css('margin-' + type) == 'auto') return 0;
+            return +$(el).css('margin-' + type).replace('px', '');
+        }
+        var ch = 0, bottom = 0; // summed height of children, 10px for top & bottom margin
+        $('body > *').each(function(i, el) {
+            var t = el.tagName.toLowerCase();
+            if (t != 'script' && el.id != 'chart' && !$(el).hasClass('tooltip') && !$(el).hasClass('container')) {
+                ch += $(el).outerHeight(false); // element height
+            }
+            ch += Math.max(margin(el, 'top'), bottom);
+            bottom = margin(el, 'bottom');
+        });
+        ch += bottom;
+        // subtract body padding
+        //ch += $('body').innerHeight() - $('body').height();
+        var mt = $('#chart').css('margin-top').replace('px', ''),
+            mb = $('#chart').css('margin-bottom').replace('px', ''),
+            maxH = $(window).height() - ch - 2;
+        // IE Fix
+        if (!$.support.leadingWhitespace) maxH -= 15;
+        maxH -= $('body').css('padding-top').replace('px', '');
+        maxH -= $('body').css('padding-bottom').replace('px', '');
+        return maxH;
     }
 
 };
 
-function getTimeslider(width) {
-
-}
 
 
 
@@ -1531,4 +1555,184 @@ dw.utils.filter = function (column, active, type, format) {
     return filter;
 };
 
+/*
+ * DataColumn abstracts the functionality of each column
+ * of a dataset. A column has a type (text|number|date).
+ *
+ * API:
+ *
+ * column.name() ... returns the name (string)
+ * column.type() ... return column type (string)
+ * column.length ... number of rows (number)
+ * column.val(i) ... parsed value in row i
+ * column.each(func) ... apply function to each value
+ * column.raw() ... access raw, unparsed values
+ *
+ */
+dw.chart = function(attributes) {
+
+    // private methods and properties
+    var dataset,
+        theme,
+        visualization,
+        metric_prefix,
+        load_callbacks = [],
+        change_callbacks = [],
+        locale;
+
+    // public interface
+    var chart = {
+        // returns an attribute
+        get: function(key, _default) {
+            var keys = key.split('.'),
+                pt = attributes;
+
+            _.each(keys, function(key) {
+                if (pt === undefined) {
+                    return _default;
+                }
+                pt = pt[key];
+            });
+            return _.isUndefined(pt) || _.isNull(pt) ? _default : pt;
+        },
+
+        set: function(key, value) {
+            var keys = key.split('.'),
+                lastKey = keys.pop(),
+                pt = attributes;
+
+            // resolve property until the parent dict
+            _.each(keys, function(key) {
+                if (_.isArray(pt[key]) && pt[key].length === 0) {
+                    pt[key] = {};
+                }
+                pt = pt[key];
+            });
+
+            // check if new value is set
+            if (!_.isEqual(pt[lastKey], value)) {
+                pt[lastKey] = value;
+                _.each(change_callbacks, function(cb) {
+                    if (_.isFunction(cb)) cb(chart, key, value);
+                });
+            }
+            return this;
+        },
+
+        // loads the dataset and returns a deferred
+        load: function() {
+            var datasource, me = this;
+
+            datasource = dw.datasource.delimited({
+                url: 'data',
+                transpose: chart.get('metadata.data.transpose', false)
+            });
+
+            return datasource.dataset().done(function(ds) {
+                dataset = ds;
+                _.each(load_callbacks, function(db) {
+                    if (_.isFunction(cb)) cb(chart);
+                });
+                load_callbacks = [];
+            });
+        },
+
+        loaded: function(callback) {
+            var me = this;
+            if (dataset) {
+                // run now
+                callback(me);
+            } else {
+                load_callbacks.push(callback);
+            }
+        },
+
+        // returns the dataset
+        dataset: function() {
+            return dataset;
+        },
+
+        // sets or gets the theme
+        theme: function(_theme) {
+            if (arguments.length) {
+                theme = _theme;
+                return chart;
+            }
+            return theme;
+        },
+
+        // sets or gets the visualization
+        vis: function(_vis) {
+            if (arguments.length) {
+                vis = _vis;
+                return chart;
+            }
+            return vis;
+        },
+
+        // returns true if the user has set any highlights
+        hasHighlight: function() {
+            var hl = chart.get('metadata.visualize.highlighted-series');
+            return _.isArray(hl) && hl.length > 0;
+        },
+
+        isHighlighted: function(obj) {
+            if (_.isUndefined(obj) === undefined) return false;
+            var hl = this.get('metadata.visualize.highlighted-series'),
+                obj_name = dw.utils.name(obj);
+            return !_.isArray(hl) || hl.length === 0 || _.indexOf(hl, obj_name) >= 0;
+        },
+
+        locale: function(_locale) {
+            if (arguments.length) {
+                locale = _locale;
+                Globalize.culture(locale);
+                return chart;
+            }
+            return locale;
+        },
+
+        metricPrefix: function(_metric_prefix) {
+            if (arguments.length) {
+                metric_prefix = _metric_prefix;
+                return chart;
+            }
+            return metric_prefix;
+        },
+
+        formatValue: function(val, full, round) {
+            var format = chart.get('metadata.describe.number-format'),
+                div = Number(chart.get('metadata.describe.number-divisor')),
+                append = chart.get('metadata.describe.number-append', '').replace(' ', '&nbsp;'),
+                prepend = chart.get('metadata.describe.number-prepend', '').replace(' ', '&nbsp;');
+
+            if (div !== 0) val = Number(val) / Math.pow(10, div);
+            if (format != '-') {
+                if (round || val == Math.round(val)) format = format.substr(0,1)+'0';
+                val = Globalize.format(val, format);
+            } else if (div !== 0) {
+                val = val.toFixed(1);
+            }
+            return full ? prepend + val + append : val;
+        },
+
+        render: function(container) {
+            if (!vis || !theme || !dataset) {
+                throw 'cannot render the chart!';
+            }
+            vis.setChart(chart);
+            vis.render(container);
+        },
+
+        attributes: function() {
+            return attributes;
+        },
+
+        onChange: function(cb) {
+            change_callbacks.push(cb);
+        }
+    };
+
+    return chart;
+};
 }).call(this);
--- www/static/js/datawrapper.js
+++ www/static/js/datawrapper.js
@@ -47,229 +47,6 @@
 
 }).call(this);(function(){
 
-    // Datawrapper.Chart
-    // -----------------
-
-    //
-    var Chart = Datawrapper.Chart = function(attributes) {
-        this.__attributes = attributes;
-    };
-
-    _.extend(Chart.prototype, {
-
-        get: function(key, _default) {
-            var keys = key.split('.'),
-                pt = this.__attributes;
-
-            _.each(keys, function(key) {
-                if (pt === undefined) {
-                    return _default;
-                }
-                pt = pt[key];
-            });
-            return _.isUndefined(pt) || _.isNull(pt) ? _default : pt;
-        },
-
-        // loads the dataset of this chart
-        dataset: function(callback, ignoreTranspose) {
-            var datasource, me = this;
-
-            datasource = dw.datasource.delimited({
-                url: 'data',
-                transpose: ignoreTranspose ? false : this.get('metadata.data.transpose', false)
-            });
-
-            return datasource.dataset().done(function(ds) {
-                me.__dataset = ds;
-                if ($.isFunction(callback)) callback(ds);
-                if (me.__datasetLoadedCallbacks) {
-                    $.each(me.__datasetLoadedCallbacks, function(i, f) {
-                        if ($.isFunction(f)) f(me);
-                    });
-                }
-            });
-
-            /***
-
-            var me = this, ds, dsOpts = {
-                delimiter: 'auto',
-                url: 'data',
-                transpose: ignoreTranspose ? false : this.get('metadata.data.transpose', false),
-                firstRowIsHeader: this.get('metadata.data.horizontal-header', true),
-                firstColumnIsHeader: this.get('metadata.data.vertical-header', true)
-            };
-            me.__dataset = ds = new Datawrapper.Dataset(dsOpts);
-            ds.fetch({
-                success: function() {
-                    callback(ds);
-                    if (me.__datasetLoadedCallbacks) {
-                        for (var i=0; i<me.__datasetLoadedCallbacks.length; i++) {
-                            me.__datasetLoadedCallbacks[i](me);
-                        }
-                    }
-                }
-            });
-            return ds;
-
-            ***/
-        },
-
-        rawData: function(rawData) {
-            var me = this,
-                dsOpts = {
-                    rawData: rawData,
-                    delimiter: 'auto',
-                    transpose: this.get('metadata.data.transpose', false),
-                    firstRowIsHeader: this.get('metadata.data.horizontal-header', true),
-                    firstColumnIsHeader: this.get('metadata.data.vertical-header', true)
-                };
-            me.__dataset = ds = new Datawrapper.Dataset(dsOpts);
-            ds.fetchRaw();
-        },
-
-        datasetLoaded: function(callback) {
-            var me = this;
-            if (me.__dataset && me.__dataset.__loaded) {
-                // run now
-                callback(me);
-            } else {
-                if (!me.__datasetLoadedCallbacks) me.__datasetLoadedCallbacks = [];
-                me.__datasetLoadedCallbacks.push(callback);
-            }
-        },
-
-        dataSeries: function(sortByFirstValue, reverseOrder) {
-            var me = this;
-            ds = [];
-            me.__dataset.eachSeries(function(series, i) {
-                ds.push(series);
-            });
-            if (sortByFirstValue === true) {
-                ds = ds.sort(function(a,b) {
-                    return b.val(0) > a.val(0) ? 1 : -1;
-                });
-            } else if ($.type(sortByFirstValue) == "number") {
-                var row = sortByFirstValue;
-                ds = ds.sort(function(a,b) {
-                    return b.val(row, true) > a.val(row, true) ? 1 : -1;
-                });
-            }
-            if (reverseOrder) ds.reverse();
-            return ds;
-        },
-
-        seriesByName: function(name) {
-            return this.__dataset.column(name);
-        },
-
-        numRows: function() {
-            return this.__dataset.numRows();
-        },
-
-        // column header is the first value of each data series
-        hasColHeader:  function(invert) {
-            var t = this.get('metadata.data.transpose');
-            if (invert ? !t : t) {
-                return this.get('metadata.data.vertical-header');
-            } else {
-                return this.get('metadata.data.horizontal-header');
-            }
-        },
-
-        // row header is the first data series
-        hasRowHeader: function() {
-            return this.hasColHeader(true);
-        },
-
-        rowHeader: function() {
-            var ds = this.__dataset;
-            return this.hasRowHeader() ? { data: ds.rowNames() } : false;
-        },
-
-        rowLabels: function() {
-            //console.warn('chart.rowLabels() is marked deprecated. Use chart.dataset().rowNames() instead');
-            if (this.hasRowHeader()) {
-                return this.rowHeader().data;
-            } else {
-                var rh = [];
-                for (var i=0; i<this.numRows(); i++) rh.push('Row '+(i+1));
-                return rh;
-            }
-        },
-
-        rowLabel: function(r) {
-            if (this.hasRowHeader()) {
-                return this.rowHeader().data[r];
-            } else {
-                return r;
-            }
-        },
-
-        hasHighlight: function() {
-            var hl = this.get('metadata.visualize.highlighted-series');
-            return _.isArray(hl) && hl.length > 0;
-        },
-
-        isHighlighted: function(col) {
-            if (col === undefined) return false;
-            var hl = this.get('metadata.visualize.highlighted-series'),
-                col_name = dw.utils.name(col);
-            return !_.isArray(hl) || hl.length === 0 || _.indexOf(hl, col_name) >= 0;
-        },
-
-        setLocale: function(locale, metric_prefix) {
-            Globalize.culture(locale);
-            this.locale = locale;
-            this.metric_prefix = metric_prefix;
-        },
-
-        formatValue: function(val, full, round) {
-            var me = this,
-                format = me.get('metadata.describe.number-format'),
-                div = Number(me.get('metadata.describe.number-divisor')),
-                append = me.get('metadata.describe.number-append', '').replace(' ', '&nbsp;'),
-                prepend = me.get('metadata.describe.number-prepend', '').replace(' ', '&nbsp;');
-
-            if (div !== 0) val = Number(val) / Math.pow(10, div);
-            if (format != '-') {
-                if (round || val == Math.round(val)) format = format.substr(0,1)+'0';
-                val = Globalize.format(val, format);
-            } else if (div !== 0) {
-                val = val.toFixed(1);
-            }
-
-            return full ? prepend + val + append : val;
-        },
-
-        /*
-         * filter to a single row in the dataset
-         */
-        filterRow: function(row) {
-            this.__dataset.filterRows([row]);
-        },
-
-        filterRows: function(rows) {
-            this.__dataset.filterRows(rows);
-        },
-
-        hasMissingValues: function() {
-            var missValues = false;
-            _.each(this.dataSeries(), function(ds) {
-                _.each(ds.data, function(val) {
-                    if (val != Number(val)) {
-                        missValues = true;
-                        return false;
-                    }
-                });
-                if (missValues) return false;
-            });
-            return missValues;
-        }
-
-    });
-
-}).call(this);(function(){
-
     // Datawrapper.Theme
     // -----------------
 
@@ -471,17 +248,6 @@
             return me;
         },
 
-        load: function(chart, callback) {
-            var me = this;
-            me.chart = chart;
-            chart.vis = me;
-            return chart.dataset().done(function(ds) {
-                me.dataset = ds;
-                me.dataset.filterSeries(chart.get('metadata.data.ignore-series', {}));
-                callback.call(me, me);
-            });
-        },
-
         /**
          * short-cut for this.chart.get('metadata.visualize.*')
          */
@@ -526,6 +292,15 @@
 
         checkBrowserCompatibility: function(){
             return true;
+        },
+
+        setChart: function(chart) {
+            console.log('vis::init', chart, chart.dataset());
+            var me = this;
+            me.dataset = chart.dataset();
+            me.theme = chart.theme();
+            me.chart = chart;
+            me.dataset.filterSeries(chart.get('metadata.data.ignore-series', {}));
         },
 
         axes: function() {
