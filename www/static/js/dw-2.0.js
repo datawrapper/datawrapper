da0b807610f2a9388601a28a806dc4d24dec186d
Gregor Aisch <contact@vis4.net>
2013-08-21 19:44:18 +0200

    removed false comment

--- www/static/js/dw-2.0.js
+++ www/static/js/dw-2.0.js
@@ -1173,19 +1173,9 @@ dw.utils.filter = function (column, active, type, format) {
 };
 
 /*
- * DataColumn abstracts the functionality of each column
- * of a dataset. A column has a type (text|number|date).
- *
- * API:
- *
- * column.name() ... returns the name (string)
- * column.type() ... return column type (string)
- * column.length ... number of rows (number)
- * column.val(i) ... parsed value in row i
- * column.each(func) ... apply function to each value
- * column.raw() ... access raw, unparsed values
  *
  */
+
 dw.chart = function(attributes) {
 
     // private methods and properties
