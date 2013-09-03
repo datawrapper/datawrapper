f1b537fa17aad4fdeb72b906bed1aa6c233a8bf0
Gregor Aisch <contact@vis4.net>
2013-09-03 23:26:27 +0200

    using current location.protocol

--- www/static/js/dw-backend-2.0.js
+++ www/static/js/dw-backend-2.0.js
@@ -368,8 +368,8 @@ var dw = dw || {};
         $.getJSON('/api/charts/'+id, function(res) {
             if (res.status == "ok") {
                 var chart = res.data,
-                    chartUrl = preview ? 'http://' + dw.backend.__domain + '/chart/' + chart.id + '/preview' :
-                        'http://' + dw.backend.__chartCacheDomain + '/' + chart.id + '/index.html';
+                    chartUrl = preview ? location.protocol + '//' + dw.backend.__domain + '/chart/' + chart.id + '/preview' :
+                        location.protocol + '//' + dw.backend.__chartCacheDomain + '/' + chart.id + '/index.html';
                     chartIframe = $('<iframe src="'+chartUrl+'" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>'),
                     wrapper = $('<div></div>'),
                     overlay = wrapper.overlay({
