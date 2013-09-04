15bcbb28e91e95fba250dcb4b7d5283411c50ad8
Gregor Aisch <contact@vis4.net>
2013-09-04 12:14:34 +0200

    added dw.utils.smartRound

--- dw.js/dw-2.0.js
+++ dw.js/dw-2.0.js
@@ -1019,7 +1019,25 @@ dw.utils = {
         return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
             return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
         });
-    }
+    },
+
+    /*
+     * Rounds a set of unique numbers to the lowest
+     * precision where the values remain unique
+     */
+    smartRound: function(values, add_precision) {
+            var result = [], precision = 0, nonEqual = true;
+            do {
+                result = _.map(values, round);
+                precision++;
+            } while (_.uniq(result, true).length < values.length);
+            if (add_precision) {
+                precision += add_precision - 1;
+                result = _.map(values, round);
+            }
+            function round(b) { return +(b.toFixed(precision)); }
+            return result;
+        }
 
 };
 
