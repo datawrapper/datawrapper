6a79597de6feca83dd00dff9472f12ef7bedb8fe
Gregor Aisch <contact@vis4.net>
2013-09-06 14:50:26 +0200

    updated chroma.js

--- plugins/visualization-raphael-chart/plugin.php
+++ plugins/visualization-raphael-chart/plugin.php
@@ -12,7 +12,7 @@ class DatawrapperPlugin_VisualizationRaphaelChart extends DatawrapperPlugin_Visu
                 ),
                 array(
                     "local" => "vendor/chroma.min.js",
-                    "cdn" => "//assets-datawrapper.s3.amazonaws.com/vendor/chroma-js/0.4.14/chroma.min.js"
+                    "cdn" => "//assets-datawrapper.s3.amazonaws.com/vendor/chroma-js/0.5.1/chroma.min.js"
                 ),
                 array(
                     "local" => "vendor/raphael-2.1.0.min.js",
