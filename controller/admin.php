8742b8a9e354a72fbdcc6d9c0006ee47d591c981
Gregor Aisch <contact@vis4.net>
2013-09-10 16:29:45 +0200

    added more charts to dashboard

--- controller/admin.php
+++ controller/admin.php
@@ -58,16 +58,35 @@ $app->get('/admin/?', function() use ($app) {
 
         $user_signups_sql = 'SELECT DATE_FORMAT(created_at, \'%Y-%m-%d\') create_date, COUNT(*) user_count FROM `user` GROUP BY create_date ORDER BY `create_date` DESC LIMIT 90';
 
+        $numUsers = UserQuery::create()->filterByDeleted(false)->count();
+        $numUsersPending = UserQuery::create()->filterByDeleted(false)->filterByRole(UserPeer::ROLE_PENDING)->count();
+        $numUsersActivated = UserQuery::create()->filterByDeleted(false)->filterByRole(UserPeer::ROLE_EDITOR)->count();
+        $numUsersDeleted = UserQuery::create()->filterByDeleted(true)->count();
+        $users_csv = "Type;Count\\nPending;$numUsersPending\\nActivated;$numUsersActivated\\nDeleted;$numUsersDeleted";
+
+        $numCharts = ChartQuery::create()->filterByDeleted(false)->count();
+        $numChartsUpload = ChartQuery::create()->filterByLastEditStep(1)->filterByDeleted(false)->count();
+        $numChartsDescribe = ChartQuery::create()->filterByLastEditStep(2)->filterByDeleted(false)->count();
+        $numChartsVisualize = ChartQuery::create()->filterByLastEditStep(3)->filterByDeleted(false)->count();
+        $numChartsPublished = ChartQuery::create()->filterByLastEditStep(array('min' => 4))->filterByDeleted(false)->count();
+        $charts_csv = "LastEditStep;Count\\nUpload;$numChartsUpload\\nDescribe;$numChartsDescribe\\nVisualize;$numChartsVisualize\\nPublish;$numChartsPublished\\n";
+
+        $charts_by_type_csv = res2csv($con->query('SELECT type, COUNT(*) FROM chart GROUP BY type;'));
+
         $page = array(
             'title' => 'Dashboard',
-            'num_users' => UserQuery::create()->filterByDeleted(false)->count(),
-            'num_users_activated' => UserQuery::create()->filterByDeleted(false)->filterByRole(UserPeer::ROLE_EDITOR)->count(),
-            'num_charts' => ChartQuery::create()->filterByDeleted(false)->count(),
-            'num_charts_published' => ChartQuery::create()->filterByLastEditStep(5)->filterByDeleted(false)->count(),
+            'num_users' => $numUsers,
+            'num_users_activated' => $numUsersActivated,
+            'num_charts' => $numCharts,
+            'num_charts_published' => $numChartsPublished,
             'published_csv' => res2csv($con->query($publised_sql)),
+            'users_csv' => $users_csv,
+            'charts_edit_step_csv' => $charts_csv,
+            'charts_by_type_csv' => $charts_by_type_csv,
             'created_csv' => res2csv($con->query($created_sql)),
             'user_signups_csv' => res2csv($con->query($user_signups_sql)),
             'linechart' => DatawrapperVisualization::get('line-chart'),
+            'donutchart' => DatawrapperVisualization::get('donut-chart'),
             'chartLocale' => 'en-US'
         );
         add_header_vars($page, 'admin');
