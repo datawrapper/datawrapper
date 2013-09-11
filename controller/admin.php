8e8bab873fd34ef4541286d1507e726ab3328a01
Gregor Aisch <contact@vis4.net>
2013-09-12 00:49:31 +0200

    reverted last commit, checking if admin pages are registered at all

--- controller/admin.php
+++ controller/admin.php
@@ -1,37 +1,36 @@
 <?php
 
-// pull admin pages from plugins
-$__dw_admin_pages = DatawrapperHooks::execute(DatawrapperHooks::GET_ADMIN_PAGES);
+if (DatawrapperHooks::hookRegistered(DatawrapperHooks::GET_ADMIN_PAGES)) {
+    // pull admin pages from plugins
+    $__dw_admin_pages = DatawrapperHooks::execute(DatawrapperHooks::GET_ADMIN_PAGES);
 
-function __dw_admin_pages_sort($a, $b) {
-    return (isset($a['order']) ? $a['order'] : 9999) - (isset($b['order']) ? $b['order'] : 9999);
-}
-
-// order admin pages by index "order"
-usort($__dw_admin_pages, '__dw_admin_pages_sort');
-
-
-foreach ($__dw_admin_pages as $admin_page) {
-
-    $app->get('/admin' . $admin_page['url'], function() use ($app, $admin_page, $__dw_admin_pages) {
-        disable_cache($app);
-
-        $user = DatawrapperSession::getUser();
-        if ($user->isAdmin()) {
-            $page_vars = array(
-                'title' => $admin_page['title'],
-                'adminmenu' => array(),
-                'adminactive' => $admin_page['url']
-            );
-            // add admin pages to menu
-            foreach ($__dw_admin_pages as $adm_pg) {
-                $page_vars['adminmenu'][$adm_pg['url']] = $adm_pg['title'];
-            }
-            add_header_vars($page_vars, 'admin');
-            call_user_func_array($admin_page['controller'], array($app, $page_vars));
-        } else {
-            $app->notFound();
-        }
+    // order admin pages by index "order"
+    usort($__dw_admin_pages, function($a, $b) {
+        return (isset($a['order']) ? $a['order'] : 9999) - (isset($b['order']) ? $b['order'] : 9999);
     });
-}
 
+
+    foreach ($__dw_admin_pages as $admin_page) {
+
+        $app->get('/admin' . $admin_page['url'], function() use ($app, $admin_page, $__dw_admin_pages) {
+            disable_cache($app);
+
+            $user = DatawrapperSession::getUser();
+            if ($user->isAdmin()) {
+                $page_vars = array(
+                    'title' => $admin_page['title'],
+                    'adminmenu' => array(),
+                    'adminactive' => $admin_page['url']
+                );
+                // add admin pages to menu
+                foreach ($__dw_admin_pages as $adm_pg) {
+                    $page_vars['adminmenu'][$adm_pg['url']] = $adm_pg['title'];
+                }
+                add_header_vars($page_vars, 'admin');
+                call_user_func_array($admin_page['controller'], array($app, $page_vars));
+            } else {
+                $app->notFound();
+            }
+        });
+    }
+}
