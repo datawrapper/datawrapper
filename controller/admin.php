0243f35bb04a743d22a3fea6c67f691ff6488f69
Gregor Aisch <contact@vis4.net>
2013-09-11 03:14:45 +0200

    showing published charts per week

--- controller/admin.php
+++ controller/admin.php
@@ -54,6 +54,8 @@ $app->get('/admin/?', function() use ($app) {
 
         $publised_sql = 'SELECT DATE_FORMAT(published_at, \'%Y-%m-%d\') pub_date, COUNT(*) pub_count FROM `chart` WHERE last_edit_step = 5 GROUP BY pub_date ORDER BY `pub_date` DESC LIMIT 1,90';
 
+        $publised_week_sql = 'SELECT DATE_FORMAT(published_at, \'%Y-w%u\') pub_date, COUNT(*) pub_count FROM `chart` WHERE last_edit_step = 5 GROUP BY pub_date ORDER BY `pub_date` DESC LIMIT 1,26';
+
         $user_signups_sql = 'SELECT DATE_FORMAT(created_at, \'%Y-%m-%d\') create_date, COUNT(*) user_count FROM `user` GROUP BY create_date ORDER BY `create_date` DESC LIMIT 1,90';
 
         $numUsers = UserQuery::create()->filterByDeleted(false)->count();
@@ -79,6 +81,7 @@ $app->get('/admin/?', function() use ($app) {
             'num_charts' => $numCharts,
             'num_charts_published' => $numChartsPublished,
             'published_csv' => res2csv($con->query($publised_sql)),
+            'published_week_csv' => res2csv($con->query($publised_week_sql)),
             'users_csv' => $users_csv,
             'charts_edit_step_csv' => $charts_csv,
             'charts_by_type_csv' => $charts_by_type_csv,
