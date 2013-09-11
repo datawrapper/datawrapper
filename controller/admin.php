4f133be3a751d206cdef41b79a25fb1e59e5d9ad
Gregor Aisch <contact@vis4.net>
2013-09-12 00:37:47 +0200

    simplified admin controller code

--- controller/admin.php
+++ controller/admin.php
@@ -1,39 +1,31 @@
 <?php
 
-
-function add_adminpage_vars(&$page, $active) {
-    $page['adminmenu'] = array();
-    global $__dw_admin_pages;
-    foreach ($__dw_admin_pages as $admin_page) {
-        $page['adminmenu'][$admin_page['url']] = $admin_page['title'];
-    }
-    /*$page['adminmenu'] = array(
-        '/admin' => 'Dashboard',
-        '/admin/users' => 'Users',
-        '/admin/themes' => 'Themes',
-        '/admin/jobs' => 'Jobs',
-    );*/
-    /*$q = JobQuery::create()->filterByStatus('queued')->count();
-    if ($q > 0) $page['adminmenu']['/admin/jobs'] .= ' <span class="badge badge-info">'.$q.'</span>';
-    $f = JobQuery::create()->filterByStatus('failed')->count();
-    if ($f > 0) $page['adminmenu']['/admin/jobs'] .= ' <span class="badge badge-important">'.$f.'</span>';*/
-    $page['adminactive'] = $active;
-}
-
+// pull admin pages from plugins
 $__dw_admin_pages = DatawrapperHooks::execute(DatawrapperHooks::GET_ADMIN_PAGES);
-  // order by index "order"
+
+// order admin pages by index "order"
 usort($__dw_admin_pages, function($a, $b) {
     return (isset($a['order']) ? $a['order'] : 9999) - (isset($b['order']) ? $b['order'] : 9999);
 });
 
+
 foreach ($__dw_admin_pages as $admin_page) {
-    $app->get('/admin' . $admin_page['url'], function() use ($app, $admin_page) {
+
+    $app->get('/admin' . $admin_page['url'], function() use ($app, $admin_page, $__dw_admin_pages) {
         disable_cache($app);
+
         $user = DatawrapperSession::getUser();
         if ($user->isAdmin()) {
-            $page_vars = array('title' => $admin_page['title']);
+            $page_vars = array(
+                'title' => $admin_page['title'],
+                'adminmenu' => array(),
+                'adminactive' => $admin_page['url']
+            );
+            // add admin pages to menu
+            foreach ($__dw_admin_pages as $adm_pg) {
+                $page_vars['adminmenu'][$adm_pg['url']] = $adm_pg['title'];
+            }
             add_header_vars($page_vars, 'admin');
-            add_adminpage_vars($page_vars, $admin_page['url']);
             call_user_func_array($admin_page['controller'], array($app, $page_vars));
         } else {
             $app->notFound();
