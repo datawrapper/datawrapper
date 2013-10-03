95b318d65d393b01006fd48f87fadb5f73dfc721
Gregor Aisch <contact@vis4.net>
2013-10-03 02:11:20 +0200

    separated publish js+css from twig

---
+++ www/static/js/dw/chart/publish.js
@@ -0,0 +1,106 @@
+
+define(function() {
+
+    var chart = dw.backend.currentChart;
+
+    function init(publish, republish) {
+        chart.sync('#embed-width', 'metadata.publish.embed-width');
+        chart.sync('#embed-height', 'metadata.publish.embed-height');
+
+        $('#embed-width, #embed-height').change(function() {
+            var w = $('#embed-width').val(), h = $('#embed-height').val(),
+                embedCode = $('#iframe-code').html();
+            embedCode = embedCode.replace(/width="\d+"/, 'width="' + w + '"');
+            embedCode = embedCode.replace(/height="\d+"/, 'height="' + h + '"');
+            $('#iframe-code').html(embedCode);
+
+            resizeIFrame();
+        });
+
+        $('a.duplicate').click(triggerDuplicate);
+        resizeIFrame();
+
+        // send ajax request for re-sending activation email
+        $('a[href=#resend]').click(function(e) {
+            e.preventDefault();
+            dw.backend.resendActivationMail($(e.target).parent().parent().parent());
+        });
+
+        // automatically select embed code
+        $("textarea").click(function() { $(this).focus().select(); } );
+
+        if (publish) {
+            $('.published').hide();
+
+            var checkStatus;
+            $.ajax({
+                url: '/api/charts/{{ chart.id }}/publish',
+                type: 'post',
+                dataType: 'json',
+                success: function(res) {
+                    clearInterval(checkStatus);
+                    if (res.status == "ok") {
+                        $('.publishing').hide();
+                        $('.published').fadeIn();
+                        $('#iframe-vis').attr('src', '{{ chartUrl }}');
+                        window.history.pushState("post_republish", "Datawrapper", "/chart/{{ chart.id }}/publish");
+                    } else {
+                        console.warn(res);
+                    }
+                }
+            });
+            var status = 0;
+            checkStatus = setInterval(function() {
+                $.get('/api/charts/{{ chart.id }}/publish/status', function(res) {
+                    status = Math.max(status, Number(res));
+                    $('.publishing .bar').width(status+'%');
+                });
+            }, 1000);
+
+            if (republish) {
+                setTimeout(function() {
+                    $('#chart-url-change-warning').fadeIn();
+                }, 1000);
+            }
+        }
+
+    }
+
+    function triggerDuplicate(e) {
+        e.preventDefault();
+        var id = chart.get('id');
+        $.ajax({
+            url: '/api/charts/'+id+'/copy',
+            type: 'POST',
+            success: function(data) {
+                if (data.status == "ok") {
+                    // redirect to copied chart
+                    location.href = '/chart/'+data.data.id+'/visualize';
+                } else {
+                    console.warn(data);
+                }
+            }
+        });
+    }
+
+    function resizeIFrame() {
+        var iframe = $('#iframe-vis'),
+            w = $('#embed-width').val(),
+            h = $('#embed-height').val(),
+            ow = iframe.parent().width();
+            oh = $('div.span4').height();
+
+        if (!w) w = 600;
+        if (!h) h = 400;
+
+        iframe.width(w);
+        iframe.height(h);
+        iframe.css({ 'margin-left': Math.max(0, (ow - w) * 0.5) + 'px' });
+        iframe.css({ 'margin-top':  '10px' });
+    }
+
+    return {
+        init: init
+    };
+
+});
