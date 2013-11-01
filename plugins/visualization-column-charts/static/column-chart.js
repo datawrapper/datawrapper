59e083399e6849b02d10c80bc7193a279c96eff2
Gregor Aisch <contact@vis4.net>
2013-11-01 10:17:30 +0100

    fixed column chart labeling

--- plugins/visualization-column-charts/static/column-chart.js
+++ plugins/visualization-column-charts/static/column-chart.js
@@ -445,7 +445,7 @@
             var me = this,
                 theme = me.theme();
             _.each(me.getBarColumns(), function(col) {
-                _.each(me.__seriesLabels[col.name()], function(lbl) {
+                _.each(me.__labels[col.name()], function(lbl) {
                     if (column !== undefined && col.name() == column.name()) {
                         lbl.addClass('hover');
                         if (lbl.hasClass('showOnHover')) lbl.show(0.5);
