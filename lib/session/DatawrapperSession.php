6b6168e62ede11b82727a8a7dcfc4ceafef50504
Gregor Aisch <contact@vis4.net>
2014-02-23 15:17:09 -0500

    bugfix: locales are now stored in json files, not directories

--- lib/session/DatawrapperSession.php
+++ lib/session/DatawrapperSession.php
@@ -94,8 +94,8 @@ class DatawrapperSession {
     public static function getBrowserLocale() {
         // get list of available locales
         $available_locales = array('en_US');
-        foreach (glob('../locale/*', GLOB_ONLYDIR) as $l) {
-            $available_locales[] = substr($l, 10);
+        foreach (glob('../locale/*_*.json') as $l) {
+            $available_locales[] = substr($l, 10, 5);
         }
         // filter out locales that are not defined in
         // config.languages
