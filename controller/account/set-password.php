11b2e8b8d82a2961bd31da8c77f9608cea187278
Gregor Aisch <contact@vis4.net>
2014-06-12 00:20:22 -0400

    fixed set-password controller

--- controller/account/set-password.php
+++ controller/account/set-password.php
@@ -1,21 +1,24 @@
 <?php
 
-//GET route
+/*
+ * this page shows up if an user has been invited to
+ * datawrapper and therefor only needs to pick a password
+ * to complete the registration process.
+ */
 $app->get('/account/set-password/:token', function ($token) use ($app) {
     disable_cache($app);
-    $page = array();
-    add_header_vars($page, 'about');
     if (!empty($token)) {
         $users = UserQuery::create()
           ->filterByActivateToken($token)
           ->find();
-        $page['salt'] = DW_AUTH_SALT;
+
         if (count($users) != 1) {
-            $page['alert'] = array(
-                'type' => 'error',
-                'message' => __('This activation token is invalid. Your email address is probably already activated.')
-            );
+            $app->redirect('/?t=e&m='.__('This activation token is invalid. Your email address is probably already activated.'));
+        } else {
+            $page = array();
+            add_header_vars($page, 'about');
+            $page['salt'] = DW_AUTH_SALT;
+            $app->render('account/set-password.twig', $page);
         }
-    }
-    $app->render('account/set-password.twig', $page);
+    } else $app->notFound();
 });
