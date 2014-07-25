190ae01548e98393b1d8763f4b005fd086dc1d90
Christian Zozmann <zozi@gabriel.office.webvariants.de>
2014-07-25 13:27:31 +0200

    allow admins to get list of organizations

--- lib/api/organizations.php
+++ lib/api/organizations.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+ * get list of all organizations
+ */
+$app->get('/organizations', function() use ($app) {
+    disable_cache($app);
+	if_is_admin(function() use ($app) {
+		try {
+			$organizations = OrganizationQuery::create()->filterByDeleted(false)->find();
+			$res           = array();
+			foreach ($organizations as $organization) {
+				$res[] = $organization->toArray();
+			}
+			ok($res);
+		} catch (Exception $e) {
+			error('io-error', $e->getMessage());
+		}
+	});
+});
 
 /*
  * creates new organization
