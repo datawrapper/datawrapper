80ce58bacde93c11c238544e8c41ecfb1ea60e90
zozi <zozi@gabriel.office.webvariants.de>
2014-07-25 11:24:49 +0200

    extend product rest api

--- lib/api/products.php
+++ lib/api/products.php
@@ -134,21 +134,74 @@ $app->post('/products/:id/users', function($id) use ($app) {
 		$product = ProductQuery::create()->findPk($id);
 		if ($product) {
 			$data = json_decode($app->request()->getBody(), true);
-			foreach ($data as $u_id => $expires) {
-				$user = UserQuery::create()->findPk($u_id);
+			foreach ($data as $newRelation) {
+				$user = UserQuery::create()->findPk($newRelation['id']);
 				if ($user) {
 					$up = new UserProduct();
 					$up->setUser($user);
 
-					if ($expires) {
-						$up->setExpires($expires);
+					if ($newRelation['expires']) {
+						$up->setExpires($newRelation['expires']);
 					}
 
 					$product->addUserProduct($up);
 				}
 			}
-			$product->save();
-			ok();
+			try {
+                $product->save();
+                ok($up);
+            } catch (Exception $e) {
+                error('io-error', $e->getMessage());
+            }
+		} else {
+			return error('unknown-product', 'Product not found');
+		}
+	});
+});
+
+$app->put('/products/:id/users', function($id) use ($app) {
+	if_is_admin(function() use ($app, $id) {
+		$product = ProductQuery::create()->findPk($id);
+		if ($product) {
+			$data = json_decode($app->request()->getBody(), true);
+            $res  = array();
+
+			try {
+                foreach ($data as $relation) {
+                    $op = UserProductQuery::create()
+                            ->filterByProductId($id)
+                            ->filterByUserId($relation['id'])
+                            ->findOne();
+                    $op->setExpires($relation['expires']);
+                    $op->save();
+                }
+                ok($res);
+            } catch (Exception $e) {
+                error('io-error', $e->getMessage());
+            }
+		} else {
+			return error('unknown-product', 'Product not found');
+		}
+	});
+});
+
+$app->delete('/products/:id/users', function($id) use ($app) {
+	if_is_admin(function() use ($app, $id) {
+		$product = ProductQuery::create()->findPk($id);
+		if ($product) {
+			$data = json_decode($app->request()->getBody(), true);
+			foreach ($data as $userid) {
+				$org = UserQuery::create()->findPk($userid);
+				if ($org) {
+					$product->removeUser($org);
+				}
+			}
+			try {
+                $product->save();
+                ok();
+            } catch (Exception $e) {
+                error('io-error', $e->getMessage());
+            }
 		} else {
 			return error('unknown-product', 'Product not found');
 		}
@@ -160,21 +213,76 @@ $app->post('/products/:id/organizations', function($id) use ($app) {
 		$product = ProductQuery::create()->findPk($id);
 		if ($product) {
 			$data = json_decode($app->request()->getBody(), true);
-			foreach ($data as $o_id => $expires) {
-				$org = OrganizationQuery::create()->findPk($o_id);
+            $res  = array();
+			foreach ($data as $newRelation) {
+				$org = OrganizationQuery::create()->findPk($newRelation['id']);
 				if ($org) {
 					$op = new OrganizationProduct();
 					$op->setOrganization($org);
 
-					if ($expires) {
-						$op->setExpires($expires);
+					if ($newRelation['expires']) {
+						$op->setExpires($newRelation['expires']);
 					}
 
 					$product->addOrganizationProduct($op);
 				}
+                $res[] = $op;
 			}
-			$product->save();
-			ok();
+            try {
+                $product->save();
+                ok($res);
+            } catch (Exception $e) {
+                error('io-error', $e->getMessage());
+            }
+		} else {
+			return error('unknown-product', 'Product not found');
+		}
+	});
+});
+
+$app->put('/products/:id/organizations', function($id) use ($app) {
+	if_is_admin(function() use ($app, $id) {
+		$product = ProductQuery::create()->findPk($id);
+		if ($product) {
+			$data = json_decode($app->request()->getBody(), true);
+            $res  = array();
+
+			try {
+                foreach ($data as $relation) {
+                    $op = OrganizationProductQuery::create()
+                            ->filterByProductId($id)
+                            ->filterByOrganizationId($relation['id'])
+                            ->findOne();
+                    $op->setExpires($relation['expires']);
+                    $op->save();
+                }
+                ok($res);
+            } catch (Exception $e) {
+                error('io-error', $e->getMessage());
+            }
+		} else {
+			return error('unknown-product', 'Product not found');
+		}
+	});
+});
+
+$app->delete('/products/:id/organizations', function($id) use ($app) {
+	if_is_admin(function() use ($app, $id) {
+		$product = ProductQuery::create()->findPk($id);
+		if ($product) {
+			$data = json_decode($app->request()->getBody(), true);
+			foreach ($data as $orgid) {
+				$org = OrganizationQuery::create()->findPk($orgid);
+				if ($org) {
+					$product->removeOrganization($org);
+				}
+			}
+			try {
+                $product->save();
+                ok();
+            } catch (Exception $e) {
+                error('io-error', $e->getMessage());
+            }
 		} else {
 			return error('unknown-product', 'Product not found');
 		}
