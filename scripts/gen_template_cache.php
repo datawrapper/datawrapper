325f804b227f06f78a0c5137d00a67068c998ef1
Gregor Aisch <gregor.aisch@nytimes.com>
2014-08-31 23:38:06 -0400

    fixed template generation

--- scripts/gen_template_cache.php
+++ scripts/gen_template_cache.php
@@ -9,29 +9,27 @@
 define('ROOT_PATH', dirname(dirname(__FILE__)) . '/');
 define('NO_SLIM', 1);
 
+require_once ROOT_PATH . 'vendor/autoload.php';
 require_once ROOT_PATH . 'lib/bootstrap.php';
-
-date_default_timezone_set('Europe/Berlin');
-
-// taken from http://twig.sensiolabs.org/doc/extensions/i18n.html#extracting-template-strings
-
-Twig_Autoloader::register();
 
 $tplDir = ROOT_PATH . 'templates';
 $tmpDir = ROOT_PATH . 'scripts/tmpl_cache/';
 $tmpDirPlugins = ROOT_PATH . 'scripts/tmpl_cache/plugins/';
 $loader = new Twig_Loader_Filesystem($tplDir);
 
+TwigView::$twigDirectory = ROOT_PATH . 'vendor/Twig';
+
 // force auto-reload to always have the latest version of the template
-$twig = new Twig_Environment($loader, array(
-    'cache' => $tmpDir,
-    'auto_reload' => true
-));
+$twig = new Twig_Environment($loader);
+
+dwInitTwigEnvironment($twig);
+
+$twig->setCache($tmpDir);
+$twig->enableAutoReload();
+
+date_default_timezone_set('Europe/Berlin');
 
 if (!file_exists($tmpDirPlugins)) mkdir($tmpDirPlugins);
-
-require_once ROOT_PATH . 'lib/utils/twig-init.php';
-
 
 // iterate over all your templates
 foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($tplDir, RecursiveDirectoryIterator::FOLLOW_SYMLINKS), RecursiveIteratorIterator::LEAVES_ONLY) as $file)
