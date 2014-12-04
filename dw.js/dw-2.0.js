b060eef3fa9413d959ffb6f7151ba7db21da35fa
Gregor Aisch <gregor.aisch@nytimes.com>
2014-12-03 21:21:26 -0500

    build

--- dw.js/dw-2.0.js
+++ dw.js/dw-2.0.js
@@ -2297,7 +2297,7 @@ dw.theme.base = {
      * some chart types (line chart) go into a 'compact'
      * mode if the chart width is below this value
      */
-    minWidth: 400,
+    minWidth: 100,
 
     /*
      * theme locale, probably unused
