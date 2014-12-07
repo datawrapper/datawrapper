03ebd161a4cbbb97c9b7469e976d34947211fea3
Gregor Aisch <gregor.aisch@nytimes.com>
2014-12-07 12:13:35 -0500

    a user can always see his own chart

--- lib/utils/check_chart.php
+++ lib/utils/check_chart.php
@@ -34,10 +34,11 @@ function check_chart_writable($id, $callback) {
 
 function check_chart_public($id, $callback) {
     $chart = ChartQuery::create()->findPK($id);
+    $loggedUser = DatawrapperSession::getUser();
     if ($chart) {
         $user = $chart->getUser();
         if ($user->isAbleToPublish()) {
-            if ($chart->isPublic()) {
+            if ($chart->isPublic() || $user == $loggedUser) {
                 call_user_func($callback, $user, $chart);
             } else if ($chart->_isDeleted()) {
                 error_chart_deleted();
