datawrapper/datawrapper

commit d4b2c60be00e80808166b25924b4210c371e4730

load js libs in reverse order of vis inheritance
if visualization V1 requires the javascript library A and B, and
inherits from another visualization V2 which requires the js libraries
C and D, the js libraries are loaded in order C, D, A, B.

lib/utils/chart_content.php
@@ -75,7 +75,7 @@ function get_chart_content($chart, $user, $published = false, $debug = false) {
         $vis = DatawrapperVisualization::get($next_vis_id);
         $vjs = array();
         if (!empty($vis['libraries'])) {
-            foreach ($vis['libraries'] as $script) {
+            foreach (array_reverse($vis['libraries']) as $script) {
                 if (!is_array($script)) {
                     $script = array("local" => $script, "cdn" => false);
                 }
@@ -128,7 +128,7 @@ function get_chart_content($chart, $user, $published = false, $debug = false) {
     if ($published) {
         $scripts = array_merge(
             $base_js,
-            $vis_libs_cdn,
+            array($vis_libs_cdn),
             array(
                 '/lib/' . $the_vis_js[0],
                 '/lib/' . $the_theme_js[0],
@@ -152,7 +152,7 @@ function get_chart_content($chart, $user, $published = false, $debug = false) {
                 array('/static/js/dw-2.0'.($debug ? '' : '.min').'.js'),
                 array_reverse($theme_js),
                 array_reverse($vis_js),
-                $vis_libs,
+                array_reverse($vis_libs),
                 array('/static/js/dw/chart.base.js')
             )
         );

commit 6f60346799a2e8fc9c327251da1df6ebea173f5f

fixed typo (introduced in d4b2c60be00e808081)
This typo lead to $scripts being something like

Array (
    [0] => //assets-datawrapper.s3.amazonaws.com/globalize.min.js
    [1] => //cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js
    [2] => //cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js
    [3] => //assets-datawrapper.s3.amazonaws.com/cultures/globalize.culture.es-ES.js
    [4] => Array (
            [0] => Array (
                    [local] => vendor/raphael-2.1.2.min.js
                    [cdn] => //assets-datawrapper.s3.amazonaws.com/vendor/raphael-js/2.1.2/raphael-min.js
                    [src] => //assets-datawrapper.s3.amazonaws.com/vendor/raphael-js/2.1.2/raphael-min.js
                )
              .....
        )
    [5] => /lib/vis/column-chart-bf5a0a39557c2bbbe290c08f08082d75.min.js
    [6] => /lib/theme/default-b0b27d4baeaba05dbf63e06e31d632aa.min.js
    [7] => /lib/chart-2a5c5c8e9f728a9b1b285cff2d9d9746.min.js
)

which confused the chart.twig.

lib/utils/chart_content.php
@@ -128,7 +128,7 @@ function get_chart_content($chart, $user, $published = false, $debug = false) {
     if ($published) {
         $scripts = array_merge(
             $base_js,
-            array($vis_libs_cdn),
+            $vis_libs_cdn,
             array(
                 '/lib/' . $the_vis_js[0],
                 '/lib/' . $the_theme_js[0],