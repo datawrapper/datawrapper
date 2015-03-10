a70713ed1d0916168ae0c162073002a6f22ad4d4
Christoph <christoph@webvariants.de>
2015-03-10 14:28:46 +0100

    version 1.9.3

--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@
 
 
 
-define('DATAWRAPPER_VERSION', '1.9.2');  // must match with package.json
+define('DATAWRAPPER_VERSION', '1.9.3');  // must match with package.json
 
 define('ROOT_PATH', '../');
 
