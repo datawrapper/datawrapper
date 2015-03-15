ae6dba6bea2372490df910afe0a1b19b0732b6c8
Gregor Aisch <gregor.aisch@nytimes.com>
2015-03-15 10:43:25 -0400

    removed custom-format from core-vis-options please install the new plugin 'options' instead

--- plugins/core-vis-options/static/sync-controls.js
+++ plugins/core-vis-options/static/sync-controls.js
@@ -121,54 +121,6 @@ $(function() {
     }
 
 
-    function syncCustomFormat(args) {
-        var curVal = args.chart.get('metadata.visualize.'+args.key),
-            formats = [],
-            colids = args.vis.axes()[args.option.axis],
-            colid = _.isArray(colids) ? colids[0] : colids,
-            axesCol = colid ? args.chart.dataset().column(colid) : null;
-
-        if (!colid) return;
-        if (axesCol.type() == 'number') {
-            formats = [
-                { l: '0', f: 'f' },
-                { l: '0.0', f: '.1f' },
-                { l: '0.00', f: '.2f' },
-                { l: '0.000', f: '.3f' },
-                { l: '1 sign. digit', f: '.1r' },
-                { l: '2 sign. digits', f: '.2r' },
-                { l: '3 sign. digits', f: '.3r' },
-                { l: '0%', f: '%' },
-                { l: '0k', f: '.1s' },
-                { l: '0.0k', f: '.2s' },
-                { l: '0.00k', f: '.3s' },
-            ];
-        } else if (axesCol.type() == 'date') {
-            formats = [
-                { l: '2015', f: '%Y' },
-                { l: '’15', f: '’%y' },
-                { l: 'April', f: '%B' },
-                { l: 'Apr', f: '%b' },
-                { l: 'Apr ’15', f: '%b ’%y' }
-            ];
-        }
-
-        var select = d3.select('#'+args.key).html('');
-
-        select.selectAll('option')
-            .data(formats)
-            .enter()
-            .append('option')
-            .attr('value', function(d) { return d.f; })
-            .text(function(d) { return d.l; });
-
-        select.on('change', function(d) {
-            args.chart.set('metadata.visualize.'+args.key, select.node().value);
-        });
-
-        select.node().value = curVal || formats.length ? formats[0].f : '';
-    }
-
     dw.backend.on('sync-option:select', syncValue);
     dw.backend.on('sync-option:text', syncValue);
     dw.backend.on('sync-option:textarea', syncValue);
@@ -176,7 +128,6 @@ $(function() {
     dw.backend.on('sync-option:radio', syncRadio);
     dw.backend.on('sync-option:radio-left', syncRadio);
     dw.backend.on('sync-option:custom-range', syncCustomRange);
-    dw.backend.on('sync-option:custom-format', syncCustomFormat);
 
     // column select
     dw.backend.on('sync-option:select-axis-column', syncSelectAxisColumn);
