70712c9e9ec436415f76b41f548241b71b465ff4
Gregor Aisch <gregor.aisch@nytimes.com>
2015-04-21 08:37:30 -0400

    static route to emulate published vis files

--- controller/chart/preview.php
+++ controller/chart/preview.php
@@ -3,7 +3,7 @@
 /*
  * Shows a preview of a chart for display in an iFrame
  */
-$app->get('/chart/:id/preview/?', function ($id) use ($app) {
+$app->get('/chart/:id/preview', function ($id) use ($app) {
     disable_cache($app);
 
     check_chart_readable($id, function($user, $chart) use ($app) {
@@ -29,4 +29,22 @@ $app->get('/chart/:id/preview/?', function ($id) use ($app) {
 
 $app->get('/chart/:id/nojs.png', function ($id) use ($app) {
     $app->redirect('/static/img/nojs.png');
+});
+
+// always redirect to url without trailing slash
+$app->get('/chart/:id/preview/', function ($id) use ($app) {
+    $app->redirect("/chart/$id/preview");
+});
+
+// static route to emulate published vis files
+$app->get('/chart/:id/_static/:file+', function($id, $parts) use ($app) {
+    check_chart_readable($id, function($user, $chart) use ($app, $parts) {
+        $fn = implode('/', $parts);
+        $vis = DatawrapperVisualization::get($chart->getType());
+        if (file_exists(ROOT_PATH . 'www/' . $vis['__static_path'] . $fn)) {
+            $app->redirect($vis['__static_path'] . $fn);
+        } else {
+            $app->notFound();
+        }
+    });
 });
