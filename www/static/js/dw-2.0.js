b85d79e539a019c56986769fb45a58be94d44787
Gregor Aisch <gregor.aisch@nytimes.com>
2015-06-21 15:07:14 -0400

    build

--- www/static/js/dw-2.0.js
+++ www/static/js/dw-2.0.js
@@ -1,4 +1,4 @@
-/*! datawrapper - v1.9.5 - 2015-06-13 *///
+/*! datawrapper - v1.9.5 - 2015-06-21 *///
 // NOTE: This file is auto-generated using /dw.js/make
 // from the source files /dw.js/src/*.js.
 //
