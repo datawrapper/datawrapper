bea1e31761c4fd6eb99df26dea30a7a04ad72d5d
Gregor Aisch <spam@vis4.net>
2015-07-16 23:11:50 -0400

    added in-array check

--- www/static/js/dw/chart/visualize/options/syncVisOptions.js
+++ www/static/js/dw/chart/visualize/options/syncVisOptions.js
@@ -96,8 +96,9 @@ define(function() {
                             key = key.match(isNullRegex)[1];
                             visible = visible && _.isNull(chart.get('metadata.visualize.'+key, null)) === val;
                         } else {
-
-                            visible = visible && chart.get('metadata.visualize.'+key) == val;
+                            visible = visible && (_.isArray(val) ?
+                                val.indexOf(chart.get('metadata.visualize.'+key)) > -1 :
+                                chart.get('metadata.visualize.'+key) == val);                            
                         }
 
                         function compare(v) {
