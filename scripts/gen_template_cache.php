a9558ee1dc9737b2cb4c6727d64d401c12d08f39
Christoph <christoph@webvariants.de>
2015-10-13 19:19:08 +0200

    make this thing work better on Windows

--- scripts/gen_template_cache.php
+++ scripts/gen_template_cache.php
@@ -6,7 +6,7 @@
  * (but PHP scripts)
  */
 
-define('ROOT_PATH', dirname(dirname(__FILE__)) . '/');
+define('ROOT_PATH', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 define('NO_SLIM', 1);
 
 require_once ROOT_PATH . 'vendor/autoload.php';
@@ -34,15 +34,15 @@ if (!file_exists($tmpDirPlugins)) mkdir($tmpDirPlugins);
 // iterate over all your templates
 foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($tplDir, RecursiveDirectoryIterator::FOLLOW_SYMLINKS), RecursiveIteratorIterator::LEAVES_ONLY) as $file)
 {
-    if (substr($file, -5) == ".twig") {
+    if (substr($file, -5) == '.twig') {
         // force compilation
-        $tmplPath = str_replace($tplDir.'/', '', $file);
+        $tmplPath = str_replace($tplDir.DIRECTORY_SEPARATOR, '', $file);
         $twig->loadTemplate($tmplPath);
         $cacheFile = $twig->getCacheFilename($tmplPath);
         $compiled = file_get_contents($cacheFile);
-        $outPath = $tmpDir . str_replace("/", "__", $tmplPath).".php";
+        $outPath = $tmpDir . str_replace(DIRECTORY_SEPARATOR, '__', $tmplPath).'.php';
         if (substr($tmplPath, 0, 8) == 'plugins/') {
-            $outPath = $tmpDirPlugins . str_replace("/", "__", substr($tmplPath, 8)).".php";
+            $outPath = $tmpDirPlugins . str_replace(DIRECTORY_SEPARATOR, '__', substr($tmplPath, 8)).'.php';
         }
         file_put_contents($outPath, $compiled);
         unlink($cacheFile);
