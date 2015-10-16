e3a01042cb80fecd1839ad0dce7c3270a17ba4ca
davidkokkelink <david.kokkelink@gmail.com>
2015-10-16 17:28:53 +0200

    remove email templates

--- lib/templates/activation-email.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/*
- * template for the Datawrapper activation email
- */
-
-$activation_mail = __("email / activation");
--- lib/templates/email-change-email.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/*
- * template for the Datawrapper activation email
- */
-
-$email_change_mail = __("email / change-email");
