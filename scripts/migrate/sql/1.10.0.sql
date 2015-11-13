dd54535328ef5096ff373aad1f24264c5ef57b13
gka <mail@driven-by-data.net>
2015-11-13 01:32:18 -0500

    added db migration query for 1.10.0

---
+++ scripts/migrate/sql/1.10.0.sql
@@ -0,0 +1,21 @@
+-- ---------------------------------------------------------------------
+-- organization_invite
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `organization_invite`;
+
+CREATE TABLE `organization_invite`
+(
+    `invite_token` VARCHAR(128) NOT NULL,
+    `organization_id` VARCHAR(128) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    PRIMARY KEY (`invite_token`),
+    INDEX `organization_invite_FI_1` (`user_id`),
+    INDEX `organization_invite_FI_2` (`organization_id`),
+    CONSTRAINT `organization_invite_FK_1`
+        FOREIGN KEY (`user_id`)
+        REFERENCES `user` (`id`),
+    CONSTRAINT `organization_invite_FK_2`
+        FOREIGN KEY (`organization_id`)
+        REFERENCES `organization` (`id`)
+) ENGINE=InnoDB;
