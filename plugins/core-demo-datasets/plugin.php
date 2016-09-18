bb540338f4c8f6cfe1cdc7b4b8e6554322cdbd42
Gregor Aisch <mail@driven-by-data.net>
2016-09-19 00:13:22 +0200

    updated core demo datasets

--- plugins/core-demo-datasets/plugin.php
+++ plugins/core-demo-datasets/plugin.php
@@ -19,7 +19,7 @@ class DatawrapperPlugin_CoreDemoDatasets extends DatawrapperPlugin {
             'title' => __('Youth Unemployment in Europe'),
             'type' => __('Line chart'),
             'presets' => array(
-                'type' => 'line-chart',
+                'type' => 'd3-lines',
                 'metadata.describe.source-name' => 'Eurostat',
                 'metadata.describe.source-url' => 'http://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-055624_QID_91D6DBE_UID_-3F171EB0&layout=TIME,C,X,0;GEO,L,Y,0;S_ADJ,L,Z,0;AGE,L,Z,1;SEX,L,Z,2;INDICATORS,C,Z,3;&zSelection=DS-055624AGE,Y_LT25;DS-055624SEX,T;DS-055624S_ADJ,SA;DS-055624INDICATORS,OBS_FLAG;&rankName1=SEX_1_2_-1_2&rankName2=AGE_1_2_-1_2&rankName3=TIME_1_0_0_0&rankName4=S-ADJ_1_2_-1_2&rankName5=INDICATORS_1_2_-1_2&rankName6=GEO_1_2_0_1&sortR=ASC_361_FIRST&pprRK=FIRST&pprSO=PROTOCOL&ppcRK=FIRST&ppcSO=ASC&sortC=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=NONE&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23',
                 'metadata.data.vertical-header' => true,
@@ -45,7 +45,7 @@ Germany\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\
             'title' => __('US Trade with United Kingdom'),
             'type' => __('Line chart'),
             'presets' => array(
-                'type' => 'line-chart',
+                'type' => 'd3-lines',
                 'metadata.describe.source-name' => 'US Census Bureau',
                 'metadata.describe.source-url' => 'http://www.census.gov/foreign-trade/balance/c4120.html',
                 'metadata.data.vertical-header' => true,
@@ -55,34 +55,38 @@ Germany\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\
                 'metadata.visualize.sort-values' => false,
                 'metadata.data.transpose' => false
             ),
-            'data' => "\tImports\tExports
-1985\t31965\t24123
-1986\t32331\t23978
-1987\t35202\t28651
-1988\t35054\t35810
-1989\t34073\t38756
-1990\t35531\t41343
-1991\t31117\t37257
-1992\t32952\t37391
-1993\t34551\t42036
-1994\t38838\t41694
-1995\t40663\t43573
-1996\t42598\t45514
-1997\t46702\t52088
-1998\t49122\t55071
-1999\t54147\t53001
-2000\t58082\t55704
-2001\t53779\t52928
-2002\t52153\t42502
-2003\t53493\t42284
-2004\t56454\t43800
-2005\t60218\t45510
-2006\t61004\t51767
-2007\t63111\t55479
-2008\t62688\t57351
-2009\t50803\t48902
-2010\t52740\t51406
-2011\t52260\t56998"
+            'data' => "Year\tExports\tImports
+1985\t11272\t14937
+1986\t11418\t15395
+1987\t14113\t17341
+1988\t18364\t17976
+1989\t20837\t18319
+1990\t23490\t20188
+1991\t22045\t18412
+1992\t22799\t20093
+1993\t26438\t21730
+1994\t26899\t25057
+1995\t28856\t26929
+1996\t30962\t28978
+1997\t36425\t32659
+1998\t39058\t34838
+1999\t38407\t39237
+2000\t41570\t43345
+2001\t40714\t41368
+2002\t33204\t40744
+2003\t33827\t42795
+2004\t35901\t46273
+2005\t38568\t51032
+2006\t45410\t53513
+2007\t49981\t56857
+2008\t53599\t58587
+2009\t45703\t47479
+2010\t48410\t49805
+2011\t56033\t51262
+2012\t54860\t55005
+2013\t47361\t52741
+2014\t53848\t54692
+2015\t56114\t57962"
         );
 
 
@@ -92,7 +96,7 @@ Germany\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\
             'chartid' => '',
             'type' => __('Line chart'),
             'presets' => array(
-                'type' => 'line-chart',
+                'type' => 'd3-lines',
                 'metadata.describe.source-name' => 'Statistisches Bundesamt',
                 'metadata.describe.source-url' => 'http://destatis.de',
                 'metadata.data.vertical-header' => true,
@@ -101,7 +105,7 @@ Germany\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\t:\
                 'metadata.data.number-divisor' => '1',
                 'metadata.data.transpose' => false
             ),
-            'data' => __('Year')."\t".__('Marriages')."\n1946\t8.1\n1947\t9.8\n1948\t10.5\n1949\t10.2\n1950\t11.0\n1951\t10.4\n1952\t9.5\n1953\t8.9\n1954\t8.7\n1955\t8.8\n1956\t8.9\n1957\t8.9\n1958\t9.1\n1959\t9.2\n1960\t9.5\n1961\t9.5\n1962\t9.4\n1963\t8.8\n1964\t8.5\n1965\t8.2\n1966\t8.0\n1967\t7.9\n1968\t7.3\n1969\t7.4\n1970\t7.4\n1971\t7.2\n1972\t7.0\n1973\t6.7\n1974\t6.5\n1975\t6.7\n1976\t6.5\n1977\t6.5\n1978\t6.0\n1979\t6.2\n1980\t6.3\n1981\t6.2\n1982\t6.2\n1983\t6.3\n1984\t6.4\n1985\t6.4\n1986\t6.6\n1987\t6.7\n1988\t6.8\n1989\t6.7\n1990\t6.5\n1991\t5.7\n1992\t5.6\n1993\t5.4\n1994\t5.4\n1995\t5.3\n1996\t5.2\n1997\t5.2\n1998\t5.1\n1999\t5.2\n2000\t5.1\n2001\t4.7\n2002\t4.7\n2003\t4.6\n2004\t4.8\n2005\t4.7\n2006\t4.5\n2007\t4.5\n2008\t4.6\n2009\t4.6\n2010\t4.7\n2011\t4.7\n2012\t4.8\n2013\t4.6\n"
+            'data' => __('Year')."\t".__('Marriages')."\n1946\t8.1\n1947\t9.8\n1948\t10.5\n1949\t10.2\n1950\t11.0\n1951\t10.4\n1952\t9.5\n1953\t8.9\n1954\t8.7\n1955\t8.8\n1956\t8.9\n1957\t8.9\n1958\t9.1\n1959\t9.2\n1960\t9.5\n1961\t9.5\n1962\t9.4\n1963\t8.8\n1964\t8.5\n1965\t8.2\n1966\t8.0\n1967\t7.9\n1968\t7.3\n1969\t7.4\n1970\t7.4\n1971\t7.2\n1972\t7.0\n1973\t6.7\n1974\t6.5\n1975\t6.7\n1976\t6.5\n1977\t6.5\n1978\t6.0\n1979\t6.2\n1980\t6.3\n1981\t6.2\n1982\t6.2\n1983\t6.3\n1984\t6.4\n1985\t6.4\n1986\t6.6\n1987\t6.7\n1988\t6.8\n1989\t6.7\n1990\t6.5\n1991\t5.7\n1992\t5.6\n1993\t5.4\n1994\t5.4\n1995\t5.3\n1996\t5.2\n1997\t5.2\n1998\t5.1\n1999\t5.2\n2000\t5.1\n2001\t4.7\n2002\t4.7\n2003\t4.6\n2004\t4.8\n2005\t4.7\n2006\t4.5\n2007\t4.5\n2008\t4.6\n2009\t4.6\n2010\t4.7\n2011\t4.7\n2012\t4.8\n2013\t4.6\n2014\t4.8\n2015\t4.9\n"
         );
 
 
