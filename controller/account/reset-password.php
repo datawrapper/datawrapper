304234d6779c0c2f4967173b22de5c8815a71edd
davidkokkelink <david.kokkelink@gmail.com>
2016-09-19 18:21:03 +0200

    controller for changing email

--- controller/account/reset-password.php
+++ controller/account/reset-password.php
@@ -30,3 +30,38 @@ $app->get('/account/reset-password/:token', function ($token) use ($app) {
     }
 });
 
+
+$app->get('/account/profile', function() use ($app) {
+    disable_cache($app);
+
+    $user = DatawrapperSession::getUser();
+
+    if ($user->getRole() == 'guest') {
+        error_settings_need_login();
+        return;
+    }
+
+    if ($app->request()->get('token')) {
+        // look for action with this token
+        $t = ActionQuery::create()
+            ->filterByUser($user)
+            ->filterByKey('email-change-request')
+            ->orderByActionTime('desc')
+            ->findOne();
+        if (!empty($t)) {
+            // check if token is valid
+            $params = json_decode($t->getDetails(), true);
+            if (!empty($params['token']) && $params['token'] == $app->request()->get('token')) {
+                // token matches
+                $user->setEmail($params['new-email']);
+                $user->save();
+                // clear token to prevent future changes
+                $params['token'] = '';
+                $t->setDetails(json_encode($params));
+                $t->save();
+            }
+        }
+    }
+
+    $app->redirect('/account');
+});
