6b420cff660e9a814ce5dfde04d92e15f93d0af9
David Kokkelink <david.kokkelink@gmail.com>
2017-06-07 16:06:15 +0200

    bugfix

--- lib/utils/parse_vis_options.php
+++ lib/utils/parse_vis_options.php
@@ -11,27 +11,29 @@ function parse_vis_options(&$vis) {
     }
     // clean vis options
     foreach ($option_parts as $options_key) {
-        foreach ($vis[$options_key] as $key => $g_option) {
-            if ($g_option['type'] != 'group') {
-                $options = [$g_option];
-            } else {
-                $options = $g_option['options'];
-            }
-            foreach ($options as $sub_key => $option) {
-                if (!empty($option['options'])) {
-                    $opts = $option['options'];
-                    if (array_keys($opts) !== range(0, count($opts) - 1)) {
-                        // associative array, convert to sequential
-                        $new_opts = [];
-                        foreach ($opts as $val => $label) {
-                            $new_opts[] = ['value' => $val, 'label' => $label];
+        if (is_array($vis[$options_key])) {
+            foreach ($vis[$options_key] as $key => $g_option) {
+                if ($g_option['type'] != 'group') {
+                    $options = [$g_option];
+                } else {
+                    $options = $g_option['options'];
+                }
+                foreach ($options as $sub_key => $option) {
+                    if (!empty($option['options'])) {
+                        $opts = $option['options'];
+                        if (array_keys($opts) !== range(0, count($opts) - 1)) {
+                            // associative array, convert to sequential
+                            $new_opts = [];
+                            foreach ($opts as $val => $label) {
+                                $new_opts[] = ['value' => $val, 'label' => $label];
+                            }
+                            if ($g_option['type'] != 'group') {
+                                $vis[$options_key][$key]['options'] = $new_opts;
+                            } else {
+                                $vis[$options_key][$key]['options'][$sub_key]['options'] = $new_opts;
+                            }
                         }
-                        if ($g_option['type'] != 'group') {
-                            $vis[$options_key][$key]['options'] = $new_opts;
-                        } else {
-                            $vis[$options_key][$key]['options'][$sub_key]['options'] = $new_opts;
-                        }
-                    } 
+                    }
                 }
             }
         }
