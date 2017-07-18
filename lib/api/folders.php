fcb34103fe3df124e1ffd47fcef67e6aac522da1
Exolon Voncor <git@exoz.net>
2017-07-18 16:42:10 +0200

    make query type switchable

--- lib/api/folders.php
+++ lib/api/folders.php
@@ -1,13 +1,30 @@
 <?php
 
-function verify_path($path, $user_id, &$parent_id) {
+function get_folder_base_query($type) {
+    switch ($type) {
+    case 'user':
+        return UserFolderQuery::create();
+    case 'organization':
+        return OrganizationFolderQuery::create();
+    default:
+        return false;
+    }
+}
+
+function verify_path($type, $path, $user_id, &$parent_id) {
     $traversed = true;
     foreach ($path as $segment) {
         if (empty($segment)) {
             // an empty segment at the end may be produced by slim
             break;
         }
-        $db_seg = UserFolderQuery::create()->filterByUserId($user_id)->filterByParentId($parent_id)->findOneByFolderName($segment);
+
+        $base_query = get_folder_base_query($type);
+        if (!$base_query) {
+            return false;
+        }
+
+        $db_seg = $base_query->filterByUserId($user_id)->filterByParentId($parent_id)->findOneByFolderName($segment);
         if (empty($db_seg)) {
             $traversed = false;
             break;
@@ -17,6 +34,7 @@ function verify_path($path, $user_id, &$parent_id) {
 
     return $traversed;
 }
+
 
 /**
  * make a chart available in a certain folder
@@ -52,19 +70,25 @@ $app->put('/folders/dir/:type/(:path+/|):dirname/?', function($type, $path, $dir
     $user = DatawrapperSession::getUser();
 
     if ($user->isLoggedIn()) {
+        $base_query = get_folder_base_query($type);
+        if (!$base_query) {
+            error('no-such-folder-type', "We don't have that type of folder(, yet?)");
+            return;
+        }
+
         $user_id = $user->getId();
-        $folders = UserFolderQuery::create()->findByUserId($user_id);
+        $folders = $base_query->findByUserId($user_id);
         // Does the user have a root folder?
         if ($folders->count() == 0) {
             $rootfolder = new UserFolder();
             $rootfolder->setUserId($user_id)->setFolderName('ROOT')->setParentId(0)->save();
         }
         // find root
-        $root_id = UserFolderQuery::create()->filterByUserId($user_id)->findOneByParentId(0)->getUfId();
+        $root_id = $base_query->filterByUserId($user_id)->findOneByParentId(0)->getUfId();
 
         // does path exists? ("" is ok, too)
-        if (verify_path($path, $user_id, $root_id)) {
-            if (empty(UserFolderQuery::create()->filterByUserId($user_id)->findOneByFolderName($dirname))) {
+        if (verify_path($type, $path, $user_id, $root_id)) {
+            if (empty($base_query->filterByUserId($user_id)->findOneByFolderName($dirname))) {
                 // Does not exist → create it!
                 $new_folder = new UserFolder();
                 // behold: $root_id was overwritten by verify_path()
