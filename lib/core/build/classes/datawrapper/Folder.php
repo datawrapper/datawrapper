bba7ac701475a93ec521a5a5c7d4f6b2992805bf
Gregor Aisch <gka@users.noreply.github.com>
2017-09-06 10:52:41 +0200

    new api

--- lib/core/build/classes/datawrapper/Folder.php
+++ lib/core/build/classes/datawrapper/Folder.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 /**
  * Skeleton subclass for representing a row from the 'folder' table.
  *
@@ -13,6 +11,74 @@
  *
  * @package    propel.generator.datawrapper
  */
-class Folder extends BaseFolder
-{
+class Folder extends BaseFolder {
+
+    /*
+     * to make our life easier
+     */
+    public function getId() {
+        return $this->getFolderId();
+    }
+
+    /*
+     * check if a given user is allowed access to this folder
+     */
+    public function isAccessibleBy($user) {
+        $type = $this->getType();
+        if ($type == 'user') return $this->getUserId() == $user->getId();
+        return OrganizationQuery::create()->findPk($this->getOrgId())->hasUser($user);
+    }
+
+    /*
+     * creates a more JSON friendly representation of this folder
+     */
+    public function serialize() {
+        return [
+            'id' => $this->getFolderId(),
+            'name' => $this->getFolderName(),
+            'parent' => $this->getParentId(),
+            'user' => $this->getUserId(),
+            'organization' => $this->getOrgId(),
+            'type' => $this->getType()
+        ];
+    }
+
+    /*
+     * returns either "user" or "organization"
+     */
+    public function getType() {
+        if (!empty($this->getUserId())) return 'user';
+        if (!empty($this->getOrgId())) return 'organization';
+        throw new Error('invalid folder type');
+    }
+
+    /*
+     * test if this folder would be a valid parent for
+     * a new folder created by this user in this org
+     */
+    public function isValidParent($user, $org_id) {
+        $type = $this->getType();
+        if ($type == 'user') {
+            // check if user id matches
+            return $user->getId() == $this->getUserId();
+        } else {
+            // check if it's the same organization
+            return $org_id == $this->getOrgId();
+        }
+    }
+
+    public function moveChartsToParent() {
+        $pdo = Propel::getConnection();
+        $sql = 'UPDATE chart SET folder = :new WHERE folder = :old';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            ':new' => $this->getParentId(),
+            ':old' => $this->getId()
+        ]);
+    }
+
+    public function moveFolder() {
+
+    }
+
 }
