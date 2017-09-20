39feb09ab92d09bbf561772afdcc8822faacdaeb
knvns <knvns@users.noreply.github.com>
2017-09-20 17:47:32 +0200

    put JS here

---
+++ www/static/js/dw/mycharts.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+	var $ = require('jquery');
+
+	// load util functions
+	var utilsFoo = require('./mycharts/util-foo');
+
+	// mychart code goes here
+	console.log('WORKS');
+	utilsFoo();
+
+});
