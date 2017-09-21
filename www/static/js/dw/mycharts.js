c911c0d38131f13ee4f36955016099b94f72f0b7
Exolon Voncor <git@exoz.net>
2017-09-21 16:51:37 +0200

    externalize generic chart functions

--- www/static/js/dw/mycharts.js
+++ www/static/js/dw/mycharts.js
@@ -1,60 +1,11 @@
-define(function(require, exports) {
+define(function(require) {
 
     var $ = require('jquery'),
-        utilsFoo = require('./mycharts/util-foo'),
+        chart_functions = require('./mycharts/generic-chart-functions'),
         twig_globals;
 
-    utilsFoo();
-
     function do_it() {
-        if (twig_globals.user2) {
-            $('.thumbnail > a').click(function(e) {
-                e.preventDefault();
-                dw.backend.popupChart($(e.target).parents('.chart').data('id'), true);
-            });
-
-            $('a.edit').click(function(e) {
-                e.preventDefault();
-                var chart = $(e.target).parents('.chart');
-                location.href = $('.thumbnail > a', chart).attr('href');
-            });
-        }
-
-        $('a.delete').click(function(e) {
-            e.preventDefault();
-            var chart = $(e.target).parents('.chart'),
-                id = chart.data('id');
-            if (window.confirm(twig_globals.strings.confirm_chart_delete)) {
-                 $.ajax({
-                    url: '/api/charts/'+id,
-                    type: 'DELETE',
-                    success: function(data) {
-                        chart.remove();
-                    }
-                });
-            };
-        });
-
-        $('.chart a.duplicate').click(function(e) {
-            e.preventDefault();
-            var id = $(e.target).parents('.chart').data('id');
-            $.ajax({
-                url: '/api/charts/'+id+'/copy',
-                type: 'POST',
-                success: function(data) {
-                    try {
-                        if (data.status == "ok") {
-                            // redirect to copied chart
-                            location.href = '/chart/'+data.data.id+'/visualize';
-                        } else {
-                            console.warn(data);
-                        }
-                    } catch (e) {
-                        console.warn(data);
-                    }
-                }
-            });
-        });
+        chart_functions(twig_globals.user2);
 
         var handler = {
             fail: function(err) {
