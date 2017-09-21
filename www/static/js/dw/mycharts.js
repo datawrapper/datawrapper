cf41a04172e8f35d6b2effb156c76c78bb94f625
Exolon Voncor <git@exoz.net>
2017-09-21 17:00:13 +0200

    treeyfy

--- www/static/js/dw/mycharts.js
+++ www/static/js/dw/mycharts.js
@@ -2,6 +2,7 @@ define(function(require) {
 
     var $ = require('jquery'),
         chart_functions = require('./mycharts/generic-chart-functions'),
+        treeyfy = require('./mycharts/treeyfy'),
         twig_globals;
 
     function do_it() {
@@ -348,35 +349,6 @@ define(function(require) {
                     $('.add-folder').click(add_folder_helper(null, decodeURIComponent(splitted[2])));
                 }
             }
-        }
-
-        function treeyfy(tree) {
-            tree.map(function(group) {
-                if (group.type === "user")
-                    group.organization = false;
-                delete(group.type);
-                group.folders.forEach(function(folder) {
-                    delete(folder.type);
-                    delete(folder.user);
-                    delete(folder.organization);
-                    folder.sub = group.folders.filter(function(potential_subfolder) {
-                        return (potential_subfolder.parent == folder.id) ? true : false;
-                    });
-                    if (!folder.sub.length)
-                        folder.sub = false;
-                });
-                group.folders = group.folders.filter(function(folder) {
-                    return (folder.parent == null) ? true : false;
-                });
-            });
-
-            tree = tree.sort(function(a, b) {
-                if (!a.organization) return -1;
-                if (!b.organization) return 1;
-                return a.organization.name.localeCompare(b.organization.name);
-            });
-
-            return tree;
         }
 
         function get_folders(tree) {
