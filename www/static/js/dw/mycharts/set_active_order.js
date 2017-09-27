23bcd593d9bd287f33e887d101596bd8c0f2d293
Exolon Voncor <git@exoz.net>
2017-09-27 17:27:03 +0200

    activate group and sort menu entries

--- www/static/js/dw/mycharts/set_active_order.js
+++ www/static/js/dw/mycharts/set_active_order.js
@@ -3,18 +3,19 @@ define(function(require) {
         twig = require('./twig_globals'),
         qstring_parser = require('./qstring_parser');
 
-    function set_active_sort() {
-        $('.sort-menu li')
+    function set_active(type) {
+        $('.' + type + '-menu li')
             .removeAttr('class')
             .each(function(idx, el) {
                 var je = $(el);
 
-                if (qstring_parser(je.find('a').attr('href'), 'sort') == twig.globals.current.sort)
+                if (qstring_parser(je.find('a').attr('href'), type) == twig.globals.current[type])
                     je.addClass('active');
             });
     }
 
     return function() {
-        set_active_sort();
+        set_active('sort');
+        set_active('group');
     };
 });
