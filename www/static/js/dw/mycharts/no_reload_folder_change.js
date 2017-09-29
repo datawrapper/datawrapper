7fce40919d2275bace6e67a70810d31a148180e2
Exolon Voncor <git@exoz.net>
2017-09-29 18:36:01 +0200

    dynamically generate subfolders after folder change

--- www/static/js/dw/mycharts/no_reload_folder_change.js
+++ www/static/js/dw/mycharts/no_reload_folder_change.js
@@ -2,13 +2,77 @@ define(function(require) {
     var $ = require('jquery'),
         twig = require('./twig_globals');
 
+    function link_reader(link) {
+        var parsed = link.slice(1).split('/'),
+            id = {
+                org: false,
+                folder: false
+            };
+
+        if (parsed[0] == 'mycharts')
+            id.folder = (parsed.length > 1) ? parsed[1] : false;
+        else {
+            id.org = parsed[1];
+            id.folder = (parsed.length > 2) ? parsed[2] : false;
+        }
+
+        return id;
+    }
+
+    function build_thumbnail(folder, org_id) {
+        var li = document.createElement('li'),
+            a = document.createElement('a'),
+            i = document.createElement('i'),
+            span = document.createElement('span');
+
+        li.classList.add('span2');
+        a.classList.add('thumbnail');
+        a.setAttribute('href', (org_id) ? '/organization/' + org_id + '/' + folder.id : '/mycharts/' + folder.id);
+        i.classList.add('im');
+        i.classList.add('im-folder');
+        i.classList.add('im-fw');
+        span.innerText = ' ' + folder.name;
+        a.appendChild(i);
+        a.appendChild(span);
+        li.appendChild(a);
+        return li;
+    }
+
     function set_active_folder(tar) {
         $('ul.folders-left li.active').removeAttr('class');
         tar.parent().addClass('active');
     }
 
     function repaint_subfolders(tar) {
-        console.log('subfolder repaint stub');
+        var id = link_reader(tar.attr('href')),
+            folders,
+            traverse = function(folders, searched_id) {
+                if (searched_id)
+                    return folders.reduce(function(old, cur) {
+                        if (cur.id == searched_id) return cur.sub;
+                        if (!old && cur.sub) return traverse(cur.sub, searched_id);
+                        return old;
+                    }, false);
+                else return folders;
+            },
+            subfolders = $('ul.subfolders');
+
+        $('ul.subfolders li.span2').not('.add-button').remove();
+
+        folders = traverse(twig.globals.preload.filter(function(candidate) {
+            if (!id.org && !candidate.organization) return true;
+            if (id.org == candidate.organization.id) return true;
+            return false;
+        })[0].folders, id.folder);
+
+        if (folders)
+            folders.forEach(function(folder) {
+                subfolders.prepend(build_thumbnail(folder, id.org));
+            });
+    }
+
+    function repaint_breadcrumb(tar) {
+        console.log('repaint_breadcrumb_stub');
     }
 
     function set_click() {
