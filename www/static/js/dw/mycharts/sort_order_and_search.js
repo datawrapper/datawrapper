7c14603d511f2724d091a34f90024a4f01292422
Exolon Voncor <git@exoz.net>
2017-10-08 19:14:33 +0200

    move qstringparser to the only left js file which is using it

--- www/static/js/dw/mycharts/sort_order_and_search.js
+++ www/static/js/dw/mycharts/sort_order_and_search.js
@@ -1,9 +1,21 @@
 define(function(require) {
     var $ = require('jquery'),
         twig = require('./twig_globals'),
-        qstring_parser = require('./qstring_parser'),
         no_reload_folder_change = require('./no_reload_folder_change'),
         cft;
+
+    function qstring_parser(href, variable) {
+        var vars = href.slice(href.lastIndexOf('?') + 1).split('&');
+
+        return vars.reduce(function(old, cur) {
+            if (old)
+                return old;
+            else {
+                var pair = cur.split('=');
+                return (decodeURIComponent(pair[0]) == variable) ? decodeURIComponent(pair[1]) : old;
+            }
+        }, false);
+    };
 
     function set_active() {
         $('.sort-menu li')
