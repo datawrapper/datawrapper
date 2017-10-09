6174c6d83bf5ece2100cd2f1f4cb61dd49ac4974
Exolon Voncor <git@exoz.net>
2017-10-10 00:31:45 +0200

    build_links!

---
+++ www/static/js/dw/mycharts/buildLink.js
@@ -0,0 +1,11 @@
+define(function(require) {
+    var twig = twig = require('./twig_globals');
+
+    return function buildLink(id) {
+        var link = '';
+
+        link += (id.organization) ? '/organization/' + id.organization : twig.globals.strings.mycharts_base;
+        if (id.folder) { link += '/' + id.folder }
+        return link;
+    }
+});
