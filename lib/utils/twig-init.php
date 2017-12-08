c7084b1fe5af5a90e71e5ad219d0104436d8d17d
Gregor Aisch <gka@users.noreply.github.com>
2017-12-08 12:26:15 +0100

    added prettyjson, useful for debugging

--- lib/utils/twig-init.php
+++ lib/utils/twig-init.php
@@ -30,9 +30,22 @@ function dwInitTwigEnvironment(Twig_Environment $twig) {
 
     $twig->addFilter(new Twig_SimpleFilter('json', function($arr) {
         $mask = 0;
-        if (!empty($opts)) {
-            if (!empty($opts['pretty'])) $mask = $mask | JSON_PRETTY_PRINT;
+        // PHP's json_encode has this *very* annoying behavior of failing
+        // quietly on non-unicode input. to fix this we're running
+        // through the entire array and checking for the correct
+        // encoding, and trying to fix it if needed.
+        if (is_array($arr)) {
+            array_walk_recursive ($arr, function (&$a) {
+                if (is_string($a) && !mb_check_encoding($a, 'UTF-8')) {
+                    $a = utf8_encode($a);
+                }
+            });
         }
+        return json_encode($arr, $mask);
+    }));
+
+    $twig->addFilter(new Twig_SimpleFilter('prettyjson', function($arr) {
+        $mask = JSON_PRETTY_PRINT;
         // PHP's json_encode has this *very* annoying behavior of failing
         // quietly on non-unicode input. to fix this we're running
         // through the entire array and checking for the correct
