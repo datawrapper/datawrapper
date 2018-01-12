3eaa2c70411884e3e45cae3da511962cdd1f7421
Gregor Aisch <gka@users.noreply.github.com>
2018-01-12 16:37:58 +0100

    remove allowfullscreen from embed codes

--- lib/utils/chart_publish.php
+++ lib/utils/chart_publish.php
@@ -282,7 +282,7 @@ function publish_get_embed_templates() {
         "id" => "responsive",
         "title" => __("publish / embed / responsive"),
         "text" => __("publish / embed / responsive / text"),
-        "template" => '<iframe id="datawrapper-chart-%chart_id%" src="%chart_url%" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="allowfullscreen" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen" oallowfullscreen="oallowfullscreen" msallowfullscreen="msallowfullscreen" style="width: 0; min-width: 100% !important;" height="%chart_height%"></iframe><script type="text/javascript">if("undefined"==typeof window.datawrapper)window.datawrapper={};window.datawrapper["%chart_id%"]={},window.datawrapper["%chart_id%"].embedDeltas=%embed_heights%,window.datawrapper["%chart_id%"].iframe=document.getElementById("datawrapper-chart-%chart_id%"),window.datawrapper["%chart_id%"].iframe.style.height=window.datawrapper["%chart_id%"].embedDeltas[Math.min(1e3,Math.max(100*Math.floor(window.datawrapper["%chart_id%"].iframe.offsetWidth/100),100))]+"px",window.addEventListener("message",function(a){if("undefined"!=typeof a.data["datawrapper-height"])for(var b in a.data["datawrapper-height"])if("%chart_id%"==b)window.datawrapper["%chart_id%"].iframe.style.height=a.data["datawrapper-height"][b]+"px"});</script>',
+        "template" => '<iframe id="datawrapper-chart-%chart_id%" src="%chart_url%" scrolling="no" frameborder="0" allowtransparency="true" style="width: 0; min-width: 100% !important;" height="%chart_height%"></iframe><script type="text/javascript">if("undefined"==typeof window.datawrapper)window.datawrapper={};window.datawrapper["%chart_id%"]={},window.datawrapper["%chart_id%"].embedDeltas=%embed_heights%,window.datawrapper["%chart_id%"].iframe=document.getElementById("datawrapper-chart-%chart_id%"),window.datawrapper["%chart_id%"].iframe.style.height=window.datawrapper["%chart_id%"].embedDeltas[Math.min(1e3,Math.max(100*Math.floor(window.datawrapper["%chart_id%"].iframe.offsetWidth/100),100))]+"px",window.addEventListener("message",function(a){if("undefined"!=typeof a.data["datawrapper-height"])for(var b in a.data["datawrapper-height"])if("%chart_id%"==b)window.datawrapper["%chart_id%"].iframe.style.height=a.data["datawrapper-height"][b]+"px"});</script>',
     ];
 
     // standard iframe
@@ -290,7 +290,7 @@ function publish_get_embed_templates() {
         "id" => "iframe",
         "title" => __("publish / embed / iframe"),
         "text" => __("publish / embed / iframe / text"),
-        "template" => '<iframe src="%chart_url%" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="allowfullscreen" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen" oallowfullscreen="oallowfullscreen" msallowfullscreen="msallowfullscreen" width="%chart_width%" height="%chart_height%"></iframe>',
+        "template" => '<iframe src="%chart_url%" scrolling="no" frameborder="0" allowtransparency="true" width="%chart_width%" height="%chart_height%"></iframe>',
     ];
 
     // add team embed codes
