b345aa76307ab85dd51899a0ea43b04a6d89378f
Gregor Aisch <gka@users.noreply.github.com>
2018-01-12 15:32:30 +0100

    bumped version

--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@
 
 
 
-define('DATAWRAPPER_VERSION', '1.15.0');  // must match with package.json
+define('DATAWRAPPER_VERSION', '1.16.0');  // must match with package.json
 
 define('ROOT_PATH', '../');
 
