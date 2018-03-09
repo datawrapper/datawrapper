cd906c45e6ed5e5762204a891d9b8c868052fc81
Gregor Aisch <gka@users.noreply.github.com>
2018-03-09 15:31:49 +0100

    revert changes

--- controller/chart/describe.php
+++ controller/chart/describe.php
@@ -60,7 +60,8 @@ $app->get('/chart/:id/describe', function ($id) use ($app) {
             'readonly' => !$chart->isDataWritable($user),
             'chartData' => $chart->loadData(),
             'transpose' => $chart->getMetadata('data.transpose'),
-            'firstRowIsHeader' => $chart->getMetadata('data.horizontal-header')
+            'firstRowIsHeader' => $chart->getMetadata('data.horizontal-header'),
+            'has_changes' => count($chart->getMetadata('data.changes', [])) > 0,
         ];
 
         $app->render('chart/describe'.($user->isAdmin() && $app->request()->get('beta') ? '-new' : '').'.twig', $page);
