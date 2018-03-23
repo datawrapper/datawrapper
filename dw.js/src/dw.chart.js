f6443a38d24014b6d1bbad75f6a9940fbb4778ef
Gregor Aisch <gka@users.noreply.github.com>
2018-03-23 14:21:36 +0100

    translations

--- dw.js/src/dw.chart.js
+++ dw.js/src/dw.chart.js
@@ -393,7 +393,7 @@ dw.chart = function(attributes) {
                 .replace(/^_+/, '')             // Trim - from start of text
                 .replace(/_+$/, '')             // Trim - from end of text
                 .replace(/^(\d)/, '_$1')        // If first char is a number, prefix with _
-                .replace(/(abstract|arguments|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|window|with|yield)/, '$1_'); // avoid reserved keywords
+                .replace(/^(abstract|arguments|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|window|with|yield)$/, '$1_'); // avoid reserved keywords
         }
     }
 
