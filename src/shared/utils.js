50f1ce4dd11fd2a4d55bc7a2172bf8c36c01d22a
hhack <hans.f.hack@googlemail.com>
2018-04-20 10:28:34 +0200

    utils no accepts credentials=omit

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,12 +1,15 @@
 import _isArray from 'underscore-es/isArray';
 
-function fetchJSON(url, method, body, callback) {
-    window.fetch(url, {
-        credentials: 'include',
+function fetchJSON(url, method, credentials, body, callback) {
+    var opts = {
         method: method,
         mode: 'cors',
         body: body
-    })
+    };
+
+    opts.credentials = credentials;
+
+    window.fetch(url, opts)
     .then((res) => {
         // console.log('status', res);
         if (res.status != 200) return new Error(res.statusText);
@@ -23,9 +26,16 @@ function fetchJSON(url, method, body, callback) {
     });
 }
 
-export function getJSON(url, callback) { return fetchJSON(url, 'GET', null, callback); }
-export function postJSON(url, body, callback) { return fetchJSON(url, 'POST', body, callback); }
-export function putJSON(url, body, callback) { return fetchJSON(url, 'PUT', body, callback); }
+export function getJSON(url, credentials, callback) { 
+    if (arguments.length == 2) {
+        callback = credentials;
+        credentials = "include";
+    }
+
+    return fetchJSON(url, 'GET', credentials, null, callback); 
+}
+export function postJSON(url, body, callback) { return fetchJSON(url, 'POST', "include", body, callback); }
+export function putJSON(url, body, callback) { return fetchJSON(url, 'PUT', "include", body, callback); }
 
 export function arrayToObject(o) {
     if (_isArray(o)) {
