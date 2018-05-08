13f85dfcbdcc9fd80b545b7600a26d620c022066
Gregor Aisch <gka@users.noreply.github.com>
2018-05-08 09:16:52 +0200

    added ColorSimple and TextArea control

--- src/controls/main.js
+++ src/controls/main.js
@@ -1,6 +1,7 @@
 
 import Checkbox from './Checkbox.html';
 import Color from './Color.html';
+import ColorSimple from './ColorSimple.html';
 import ColorPicker from './ColorPicker.html';
 import CustomFormat from './CustomFormat.html';
 import Group from './Group.html';
@@ -8,9 +9,10 @@ import NumberInput from './Number.html';
 import Radio from './Radio.html';
 import Select from './Select.html';
 import Section from './Section.html';
+import TextArea from './TextArea.html';
 import SelectAxisColumn from './SelectAxisColumn.html';
 
 export default {
-    Checkbox, Color, ColorPicker, Group, NumberInput,
-    Radio, Select, SelectAxisColumn, CustomFormat, Section
+    Checkbox, Color, ColorSimple, ColorPicker, Group, NumberInput,
+    Radio, Select, SelectAxisColumn, CustomFormat, Section, TextArea
 };
