fb8d69e8116a0d1fb321140d966a50768299299b
David Kokkelink <david.kokkelink@gmail.com>
2018-06-24 19:29:23 +0200

    improve IE compatibility

---
+++ www/static/js/misc/polyfills.js
@@ -0,0 +1,8 @@
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
