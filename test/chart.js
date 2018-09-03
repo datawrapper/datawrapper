c237976469c9b38f687b9217886486ebc74c4c4e
Gregor Aisch <gka@users.noreply.github.com>
2018-09-03 13:42:57 +0200

    need to fix chart test

---
+++ test/chart.js
@@ -0,0 +1,61 @@
+// var Chart = require('../src/chart/Chart.js');
+
+// global.c = Chart({
+// 	"id":"MD35m",
+// 	"title":"Majority of EU citizens in favor of abolishing Daylight Saving Time",
+// 	"theme":"datawrapper",
+// 	"createdAt":"2018-09-01 08:28:56",
+// 	"lastModifiedAt":"2018-09-01 08:30:54",
+// 	"type":"d3-bars-stacked",
+// 	"metadata":{"data":{"transpose":false,"vertical-header":true,"horizontal-header":true},"visualize":{"highlighted-series":["EU-28"],"highlighted-values":[],"rules":false,"thick":false,"x-grid":"off","y-grid":"on","scale-y":"linear","sort-by":"Keep","labeling":"right","sort-asc":true,"color-key":{"Keep":{"col":"#ffe59c","color":"#ffe59c","label":"Keep"},"bars":{"color":"#18a1cd","label":"Abolish","def_lbl":"bars"},"Abolish":{"col":"#c71e1d","color":"#c71e1d","label":"Abolish"}},"sort-bars":"keep","background":false,"force-grid":false,"resort-bars":false,"swap-labels":false,"block-labels":false,"fill-between":false,"label-colors":false,"label-margin":0,"line-symbols":false,"custom-colors":{"Keep":"#ffe59c","Abolish":"#c71e1d"},"interpolation":"linear","show-tooltips":true,"x-tick-format":"YYYY","y-grid-format":"0,0.[00]","y-grid-labels":"auto","show-color-key":false,"label-alignment":"left","line-symbols-on":"both","y-grid-subdivide":true,"custom-grid-lines":"","date-label-format":"YYYY","hide-group-labels":true,"hide-value-labels":false,"line-symbols-size":3.5,"line-value-labels":false,"stack-percentages":false,"line-symbols-shape":"circle","value-label-format":"0%","y-grid-label-align":"left","line-symbols-opacity":1,"area-fill-color-below":"#cccccc","value-label-alignment":"left","area-fill-color-between":"#cccccc"},"describe":{"source-name":"European Comission","source-url":"http://europa.eu/rapid/press-release_IP-18-5302_en.htm","number-format":"-","number-divisor":0,"number-append":"","number-prepend":"","intro":"Of the 4.6 million EU citizens who participated in a public consultation, <b>84 percent</b> are saying they are in favor of abolishing daylight savings."},"publish":{"embed-width":"400","embed-height":876,"chart-height":722},"axes":{"groups":"Group"},"json_error":null},
+// 	"authorId":29,
+// 	"showInGallery":true,
+// 	"language":"en-US",
+// 	"guestSession":null,
+// 	"lastEditStep":3,
+// 	"publishedAt":null,
+// 	"publicUrl":"http://charts.datawrapper.local/MD35m/index.html",
+// 	"publicVersion":0,
+// 	"organizationId":null,
+// 	"forkedFrom":null,"externalData":null,"forkable":false,"isFork":false,"inFolder":null,
+// 	"author":{
+// 		"id":29,
+// 		"email":"gka@vis4.net",
+// 		"name":"Gregor Aisch",
+// 		"website":null,
+// 		"socialmedia":null
+// 	}
+// });
+
+// const data = `Country	Group	Abolish	Keep
+// EU-28	A	84	16
+// Finland	B	95	5
+// Poland	B	95	5
+// Spain	B	93	7
+// Lithuania	B	91	9
+// Croatia	B	90	10
+// Hungary	B	90	10
+// Ireland	B	88	12
+// Sweden	B	88	12
+// Slovenia	B	87	13
+// Estonia	B	85	15
+// Latvia	B	85	15
+// Portugal	B	85	15
+// Belgium	B	84	16
+// Bulgaria	B	84	16
+// France	B	84	16
+// Germany	B	84	16
+// Czech Republic	B	83	17
+// United Kingdom	B	82	18
+// Denmark	B	81	19
+// Slovakia	B	80	20
+// Luxembourg	B	79	21
+// Netherlands	B	79	21
+// Romania	B	78	22
+// Austria	B	77	23
+// Italy	B	66	34
+// Malta	B	54	46
+// Cyprus	B	47	53
+// Greece	B	44	56`;
+
+// c.load(data);
