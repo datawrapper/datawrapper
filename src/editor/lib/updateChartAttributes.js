bbda6a4aeb869b010b3488821544ec321335ee2d
Gregor Aisch <gka@users.noreply.github.com>
2018-09-06 16:15:23 +0200

    simplified state updates

---
+++ src/editor/lib/updateChartAttributes.js
@@ -0,0 +1,54 @@
+/* globals dw, $, _ */
+
+export default function(iframe, attrs) {
+    const win = iframe.contentWindow;
+    const doc = iframe.contentDocument;
+
+    let render = false;
+    let needReload = false;
+
+    const requiresReload = ['type', 'theme', 'metadata.data.transpose', 'metadata.axes', 'language'];
+
+    requiresReload.forEach(function(key) {
+        if (changed(key)) {
+            needReload = true;
+            return;
+        }
+    });
+
+    if (changed('metadata.data.column-format')
+        || changed('metadata.data.changes')   || changed('metadata.data.column-order')
+        || changed('metadata.describe.computed-columns')) {
+        needReload = true;
+        return;
+    }
+
+    // check if we need to update chart
+    if (changed('metadata.visualize')) {
+        win.__dw.vis.chart().attributes(attrs);
+        render = true;
+    }
+
+    if (needReload) {
+        win.location.reload();
+        return;
+    }
+
+    win.__dw.vis.chart().attributes(attrs);
+    win.__dw.old_attrs = $.extend(true, {}, attrs);
+    // update dataset to reflect changes
+    win.__dw.vis.chart().load(win.__dw.params.data);
+
+    if (render) win.__dw.render();
+
+    function changed(key) {
+        var p0 = win.__dw.old_attrs,
+            p1 = attrs;
+        key = key.split('.');
+        _.each(key, function(k) {
+            p0 = p0[k] || {};
+            p1 = p1[k] || {};
+        });
+        return JSON.stringify(p0) != JSON.stringify(p1);
+    }
+}
