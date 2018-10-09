6b1ee9f08ccff3e09eab5bfe9b7f07058aec4f26
David Kokkelink <david.kokkelink@gmail.com>
2018-10-09 16:01:21 +0200

    add check password

---
+++ src/shared/checkPassword.js
@@ -0,0 +1,16 @@
+export default function(curPwd, pwd, pwd2) {
+	var msg, errFields;
+    if (curPwd === '') {        
+        msg = dw.backend.messages.provideCurPwd;
+    }
+    else if (pwd.length < 4) {        
+        msg = dw.backend.messages.pwdTooShort;
+    }
+    else if (pwd != pwd2) {        
+        msg = dw.backend.messages.pwdMismatch;
+    }
+    if (msg) {
+        return msg;
+    }
+    return true;
+}
