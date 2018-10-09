18be118a4c2f8aa6744cf50c359d70d36d8614ad
Gregor Aisch <gka@users.noreply.github.com>
2018-10-09 11:55:11 +0200

    typeahead styling

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -1,6 +1,4 @@
-import fs from 'fs';
 import less from 'less';
-import rollup from 'rollup';
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
