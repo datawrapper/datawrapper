3667f7b480e2b7e0ea918efed5eec79c9be5758b
Gregor Aisch <gka@users.noreply.github.com>
2018-10-16 16:54:23 +0200

    truncate action details

--- lib/api/actions.php
+++ lib/api/actions.php
@@ -8,7 +8,7 @@ $app->post('/actions/:key', function($key) use ($app) {
     disable_cache($app);
     $user = Session::getUser();
     $details = $app->request()->getBody();
-    $action = Action::logAction($user, $key, $details);
+    $action = Action::logAction($user, $key, substr($details, 0, 512));
     ok($action->toArray());
 });
 
