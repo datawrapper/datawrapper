69da008f601879c33ba2a0e709ebeec7d467729b
Gregor Aisch <gka@users.noreply.github.com>
2018-12-19 18:34:43 +0100

    allow console.warn and error

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "rules": {
     	"no-sequences": "error",
         "eqeqeq": ["error", "smart"],
-        "no-multiple-empty-lines": ["error", { "max": 1 }]
+        "no-multiple-empty-lines": ["error", { "max": 1 }],
+        "no-console": ["error", { allow: ["warn", "error"] }]
     }
 };
