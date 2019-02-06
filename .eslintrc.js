f5d473ce7cd3b19555db3d157e763f2d777ab721
Simon Jockers <hello@simonjockers.de>
2019-02-06 20:08:11 +0100

    chore: remove unused eslint options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,10 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true
+    parser: 'babel-eslint',
+    rules: {
+        'no-sequences': 'error',
+        eqeqeq: ['error', 'smart'],
+        'no-multiple-empty-lines': ['error', { max: 1 }],
+        'no-console': ['error', { allow: ['warn', 'error'] }]
     },
-    "parser": "babel-eslint",
-    "extends": "eslint:recommended",
-    "globals": {
-        "Reflect": true
-    },
-    "rules": {
-    	"no-sequences": "error",
-        "eqeqeq": ["error", "smart"],
-        "no-multiple-empty-lines": ["error", { "max": 1 }],
-        "no-console": ["error", { allow: ["warn", "error"] }]
-    },
-    "plugins": [
-        "html",
-        "@tivac/svelte"
-    ]
+    plugins: ['html', '@tivac/svelte']
 };
