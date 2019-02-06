e72ba8c8cedac2bbd2c4ba23d61c52455b781861
Simon Jockers <hello@simonjockers.de>
2019-02-06 13:07:33 +0100

    chore: add prettier to eslint setup (formats js and svelte files)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,26 @@ module.exports = {
         "commonjs": true
     },
     "parser": "babel-eslint",
-    "extends": "eslint:recommended",
+    "extends": [
+        "standard",
+        "plugin:@tivac/svelte/svelte",
+        "plugin:prettier/recommended",
+    ],
     "globals": {
-        "Reflect": true
+        "Reflect": true,
     },
     "rules": {
-    	"no-sequences": "error",
+        "no-sequences": "error",
         "eqeqeq": ["error", "smart"],
         "no-multiple-empty-lines": ["error", { "max": 1 }],
-        "no-console": ["error", { allow: ["warn", "error"] }]
+        "no-console": ["error", { "allow": ["warn", "error"] }],
+        "camelcase": ["warn",{"ignoreDestructuring": true}],
+        "no-console": ["error",{"allow": ["warn","error"]}],
+        "prettier/prettier": "error",
     },
     "plugins": [
         "html",
-        "@tivac/svelte"
-    ]
+        "@tivac/svelte",
+        "prettier",
+    ],
 };
