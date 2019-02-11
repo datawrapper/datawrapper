9a0891954358f256db2d802fe610612494bd2440
Gregor Aisch <gka@users.noreply.github.com>
2019-02-11 16:21:28 +0100

    eslint fix
    Signed-off-by: Gregor Aisch <gka@users.noreply.github.com>

--- src/shared/purifyHtml.js
+++ src/shared/purifyHtml.js
@@ -6,7 +6,7 @@
  */
 const TAGS = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
 const COMMENTS_AND_PHP_TAGS = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
-const default_allowed = "<a><b><br><br/><i><strong><sup><sub><strike><u><em><tt>";
+const defaultAllowed = '<a><b><br><br/><i><strong><sup><sub><strike><u><em><tt>';
 
 export default function(input, allowed) {
     if (input === null) return null;
@@ -22,11 +22,11 @@ export default function(input, allowed) {
     var d = document.createElement('div');
     d.innerHTML = input;
     var sel = d.querySelectorAll('*');
-    for (var i=0; i<sel.length; i++) {
-        for (var j=0; j<sel[i].attributes.length; j++) {
+    for (var i = 0; i < sel.length; i++) {
+        for (var j = 0; j < sel[i].attributes.length; j++) {
             var attrib = sel[i].attributes[j];
             if (attrib.specified) {
-                if (attrib.name.substr(0,2) == 'on') sel[i].removeAttribute(attrib.name);
+                if (attrib.name.substr(0, 2) === 'on') sel[i].removeAttribute(attrib.name);
             }
         }
     }
@@ -35,15 +35,19 @@ export default function(input, allowed) {
 
 function stripTags(input, allowed) {
     // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
-    allowed = (((allowed !== undefined ? allowed || '' : default_allowed) + '').toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('');
+    allowed = (
+        ((allowed !== undefined ? allowed || '' : defaultAllowed) + '')
+            .toLowerCase()
+            .match(/<[a-z][a-z0-9]*>/g) || []
+    ).join('');
 
     var before = input;
     var after = input;
     // recursively remove tags to ensure that the returned string doesn't contain forbidden tags after previous passes (e.g. '<<bait/>switch/>')
     while (true) {
         before = after;
-        after = before.replace(COMMENTS_AND_PHP_TAGS, '').replace(TAGS, function ($0, $1) {
-            return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : ''
+        after = before.replace(COMMENTS_AND_PHP_TAGS, '').replace(TAGS, function($0, $1) {
+            return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
         });
         // return once no more tags are removed
         if (before === after) {
