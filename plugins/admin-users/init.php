325653c4c1eb8e9c376e4d22589324cfb30f2ec3
Simon Jockers <hello@simonjockers.de>
2019-02-20 17:02:23 +0100

    user admin: remove bootstrapping data from twig (loaded via XHR anyways)

--- plugins/admin-users/init.php
+++ plugins/admin-users/init.php
@@ -5,19 +5,6 @@ $plugin->registerAdminPage(function() use ($plugin) {
         'url' => '/users',
         'title' => __('Users', $plugin->getName()),
         'controller' => function($app, $page) use ($plugin) {
-
-            $users = UserQuery::create()
-                ->orderByCreatedAt('desc')
-                ->limit(100)
-                ->find();
-
-            $page['state'] = ['users' => []];
-
-            foreach ($users as $user) {
-                // serialize from PHP classes to plain objects
-                $page['state']['users'][] = $user->serialize();
-            }
-
             $app->render('plugins/admin-users/admin-users.twig', $page);
         },
         'group' => __('Users'),
