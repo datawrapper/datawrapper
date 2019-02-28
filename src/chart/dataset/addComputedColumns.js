0739215440fedc5b18f3a730df2a3964f9b44419
Gregor Aisch <gka@users.noreply.github.com>
2019-02-28 18:29:05 +0100

    code linting and formatting
    Signed-off-by: Gregor Aisch <gka@users.noreply.github.com>

--- src/chart/dataset/addComputedColumns.js
+++ src/chart/dataset/addComputedColumns.js
@@ -8,6 +8,7 @@ import arrayMax from 'd3-array/src/max';
 import arraySum from 'd3-array/src/sum';
 import arrayMean from 'd3-array/src/mean';
 import arrayMedian from 'd3-array/src/median';
+import columnNameToVariable from '../../shared/columnNameToVariable';
 
 export default function(chart, dataset) {
     const virtualColumns = chart.getMetadata('describe.computed-columns', {});
@@ -17,7 +18,7 @@ export default function(chart, dataset) {
 
     dataset.eachColumn(function(col) {
         if (col.isComputed) return;
-        columnNameToVar[col.name()] = convertColumnNameToVariable(col.name());
+        columnNameToVar[col.name()] = columnNameToVariable(col.name());
         if (col.type() === 'number') {
             columnAggregates[col.name()] = {
                 min: arrayMin(col.values()),
@@ -35,13 +36,7 @@ export default function(chart, dataset) {
 
     function addComputedColumn(formula, name) {
         const datefmt = d => {
-            return (
-                d.getFullYear() +
-                '-' +
-                leftPad(1 + d.getMonth(), 2, 0) +
-                '-' +
-                leftPad(1 + d.getDate(), 2, 0)
-            );
+            return d.getFullYear() + '-' + leftPad(1 + d.getMonth(), 2, 0) + '-' + leftPad(1 + d.getDate(), 2, 0);
         };
         const values = data
             .map((row, rowIndex) => {
@@ -51,41 +46,11 @@ export default function(chart, dataset) {
                     if (!columnNameToVar[key]) return;
                     context.push('var ' + columnNameToVar[key] + ' = ' + JSON.stringify(val) + ';');
                     if (dataset.column(key).type() === 'number') {
-                        context.push(
-                            'var ' +
-                                columnNameToVar[key] +
-                                '__sum = ' +
-                                columnAggregates[key].sum +
-                                ';'
-                        );
-                        context.push(
-                            'var ' +
-                                columnNameToVar[key] +
-                                '__min = ' +
-                                columnAggregates[key].min +
-                                ';'
-                        );
-                        context.push(
-                            'var ' +
-                                columnNameToVar[key] +
-                                '__max = ' +
-                                columnAggregates[key].max +
-                                ';'
-                        );
-                        context.push(
-                            'var ' +
-                                columnNameToVar[key] +
-                                '__mean = ' +
-                                columnAggregates[key].mean +
-                                ';'
-                        );
-                        context.push(
-                            'var ' +
-                                columnNameToVar[key] +
-                                '__median = ' +
-                                columnAggregates[key].median +
-                                ';'
-                        );
+                        context.push('var ' + columnNameToVar[key] + '__sum = ' + columnAggregates[key].sum + ';');
+                        context.push('var ' + columnNameToVar[key] + '__min = ' + columnAggregates[key].min + ';');
+                        context.push('var ' + columnNameToVar[key] + '__max = ' + columnAggregates[key].max + ';');
+                        context.push('var ' + columnNameToVar[key] + '__mean = ' + columnAggregates[key].mean + ';');
+                        context.push('var ' + columnNameToVar[key] + '__median = ' + columnAggregates[key].median + ';');
                     }
                 });
                 context.push('var max = Math.max, min = Math.min;');
@@ -108,24 +73,6 @@ export default function(chart, dataset) {
         const virtualColumn = column(name, values);
         virtualColumn.isComputed = true;
         dataset.add(virtualColumn);
-    }
-
-    function convertColumnNameToVariable(name) {
-        // if you change this, change computed-columns.js as well
-        return name
-            .toString()
-            .toLowerCase()
-            .replace(/\s+/g, '_') // Replace spaces with _
-            .replace(/[^\w-]+/g, '') // Remove all non-word chars
-            .replace(/-/g, '_') // Replace multiple - with single -
-            .replace(/__+/g, '_') // Replace multiple - with single -
-            .replace(/^_+/, '') // Trim - from start of text
-            .replace(/_+$/, '') // Trim - from end of text
-            .replace(/^(\d)/, '_$1') // If first char is a number, prefix with _
-            .replace(
-                /^(abstract|arguments|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|window|with|yield)$/,
-                '$1_'
-            ); // avoid reserved keywords
     }
 
     function leftPad(s, l, pad) {
