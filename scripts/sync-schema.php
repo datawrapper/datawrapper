038b56af62aaea7ee78635411e5791abf05d0c46
Gregor Aisch <gka@users.noreply.github.com>
2019-03-11 18:19:53 +0100

    parse migration files

--- scripts/sync-schema.php
+++ scripts/sync-schema.php
@@ -18,25 +18,91 @@ function dbQuery($sql) {
         print $e->getMessage();
     }
 }
-function dbGetOne($sql) {
-	$res = dbQuery($sql);
-	$row = next($res);
-	return $row[0];
+
+function dbFetchColumn($sql) {
+    $res = dbQuery($sql);
+    return $res->fetchAll(PDO::FETCH_COLUMN, 0);
 }
 
-$tables = [];
-foreach (dbQuery('SHOW TABLES') as $table) {
-	$tables[] = $table[0];
+function dbFetchOne($sql) {
+    $res = dbQuery($sql);
+    return $res->fetchColumn(0);
 }
 
-// migrate db schema
+$tables = dbFetchColumn('SHOW TABLES');
 
-// find current core schema version
+// get list of scopes, start with `core`
 $scopes = ['core'];
 
+// add scope for plugins with a migrations folder
+foreach (glob(ROOT_PATH . 'plugins/*/migrations') as $path) {
+    $parts = explode('/', $path);
+    $scopes[] = $parts[count($parts)-2];
+}
+
+$migrations = [];
+
+// iterate over all scopes
 foreach ($scopes as $scope) {
-	$version = getSchemaVersion($scope);
-	print "$scope is on version $version\n";
+    // get current schema for scope
+    $version = getSchemaVersion($scope);
+    $migrations[$scope] = getMigrations($scope);
+    print "$scope is on version $version\n";
+    print_r(($migrations[$scope]));
+}
+
+/**
+ * returns a list of all db migrations for a given `scope`
+ */
+function getMigrations($scope) {
+    $path = $scope === 'core' ? 'migrations/*.sql' : 'plugins/'.$scope.'/migrations/*.sql';
+    $result = [];
+    foreach (glob(ROOT_PATH . $path) as $sql) {
+        $file = basename($sql);
+        $parts = explode('-', $file);
+        $result[] = [
+            'version' => intval(array_shift($parts)),
+            'key' => implode('-', $parts),
+            'content' => parseMigrationSQL(file_get_contents($sql))
+        ];
+    }
+    return $result;
+}
+
+/**
+ * splits a migration sql file into `adds`, `up` and `down`
+ *
+ * example migration.sql:
+ * -- Adds chart.utf8
+ * -- Up
+ * ALTER TABLE chart ADD COLUMN utf8 INT;
+ * -- Down
+ * ALTER TABLE chart DROP COLUMN utf8;
+ */
+function parseMigrationSQL($sql) {
+    $lines = explode("\n", $sql);
+    $up = [];
+    $down = [];
+    $adds = [];
+    $mode = '';
+    foreach ($lines as $line) {
+        if (substr($line, 0, 2) === '--') {
+            if (trim(strtolower(substr($line, 2))) === 'up') $mode = 'up';
+            if (trim(strtolower(substr($line, 2))) === 'down') $mode = 'down';
+            if (trim(strtolower(substr($line, 2, 5))) === 'adds') {
+                $mode = '';
+                $adds = array_map(trim, explode(',', trim(strtolower(substr($line, 7)))));
+            }
+        } else {
+            if ($mode === 'up') $up[] = $line;
+            if ($mode === 'down') $down[] = $line;
+        }
+    }
+    return [
+        'adds' => $adds,
+        'up' => implode("\n", $up),
+        'down' => implode("\n", $down),
+    ];
 }
 
 // check if `schema` DB table exists
@@ -49,13 +115,13 @@ function migrateToLatest($scope, $fromVersion) {
 }
 
 function getSchemaVersion($scope) {
-	global $tables;
-	if (in_array('schema', $tables)) {
-		return dbGetOne('SELECT version FROM schema WHERE scope = "'.$scope.'"') ?? 0;
-	}
-	if ($scope === "core") {
-		// check if chart table exists
+    global $tables;
+    if (in_array('schema', $tables)) {
+        return dbFetchOne('SELECT version FROM schema WHERE scope = "'.$scope.'"') ?? 0;
+    }
+    if ($scope === "core") {
+        // check if chart table exists
 
-	}
-	return 1;
+    }
+    return 0;
 }
