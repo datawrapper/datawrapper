a22255372bd18d520696fa307982f2c6dc06fa91
Gregor Aisch <gka@users.noreply.github.com>
2019-03-12 15:16:46 +0100

    refactor migrations into static class

---
+++ lib/Migrations.php
@@ -0,0 +1,199 @@
+<?php
+namespace dw;
+
+use Exception;
+use Propel;
+use PDO;
+use PDOException;
+
+class Migrations {
+
+    static function sync($scope) {
+        $version = self::getSchemaVersion($scope);
+        $migrations = self::getMigrations($scope);
+        self::log($scope, 'currently on version '.$version);
+
+        // print_r(($migrations[$scope]));
+        foreach ($migrations as $migration) {
+            if ($migration['version'] > $version) {
+                if (self::canSkipMigration($migration)) {
+                    self::log($scope, 'skipping migration '.$migration['version'].' ('.$migration['key'].')');
+                    self::updateSchemaVersion($scope, $migration['version']);
+                } else {
+                    self::log($scope, 'applying migration '.$migration['version'].' ('.$migration['key'].')', 'white');
+                    try {
+                        self::applyMigration($migration);
+                    } catch (Exception $e) {
+                        self::log($scope, 'migration '.$migration['version'].' ('.$migration['key'].') failed!', 'red', true);
+                        self::log($scope, $e->getMessage(), 'red', true);
+                    } finally {
+                        self::updateSchemaVersion($scope, $migration['version']);
+                    }
+                }
+            } else {
+                self::log($scope, 'already on version '.$migration['version'].' ('.$migration['key'].')', 'gray');
+            }
+        }
+    }
+
+    static $tables = [];
+
+    protected static function updateSchemaVersion($scope, $version) {
+        try {
+            self::dbQuery('INSERT INTO `schema` (scope, version) VALUES ("'.$scope.'", '.$version.') ON DUPLICATE KEY UPDATE version = '.$version, false);
+        } catch (Exception $e) {
+            self::log($scope, 'could not update schema version (yet)', 'yellow');
+        }
+    }
+
+    protected static function applyMigration($migration) {
+        print $migration['content']['up'];
+        self::dbQuery($migration['content']['up']);
+    }
+
+    protected static function canSkipMigration($migration) {
+        if (!empty($migration['content']['adds'])) {
+            $tables = self::dbFetchColumn('SHOW TABLES');
+            $canSkip = true;
+            foreach ($migration['content']['adds'] as $tableOrColumn) {
+                $parts = explode('.', $tableOrColumn);
+                $table = $parts[0];
+                $tableExists = in_array(strtolower($table), $tables);
+                if (count($parts) > 1) {
+                    // table column
+                    if ($tableExists) {
+                        // see if column exists
+                        $column = $parts[1];
+                        $columns = self::dbFetchColumn('DESCRIBE '.$table);
+                        $columnExists = in_array(strtolower($column), $columns);
+                        $canSkip = $canSkip && $columnExists;
+                    } else {
+                        $canSkip = false;
+                    }
+
+                } else {
+                    // check if table exists
+                    $canSkip = $canSkip && $tableExists;
+                }
+                # code...
+            }
+            return $canSkip;
+        }
+        return false;
+    }
+
+    /**
+     * splits a migration sql file into `adds`, `up` and `down`
+     *
+     * example migration.sql:
+     * -- Adds chart.utf8
+     * -- Up
+     * ALTER TABLE chart ADD COLUMN utf8 INT;
+     * -- Down
+     * ALTER TABLE chart DROP COLUMN utf8;
+     */
+    protected static function parseMigrationSQL($sql) {
+        $lines = explode("\n", $sql);
+        $up = [];
+        $down = [];
+        $adds = [];
+        $mode = '';
+        foreach ($lines as $line) {
+            if (substr($line, 0, 2) === '--') {
+                if (trim(strtolower(substr($line, 2))) === 'up') $mode = 'up';
+                if (trim(strtolower(substr($line, 2))) === 'down') $mode = 'down';
+                if (trim(strtolower(substr($line, 2, 5))) === 'adds') {
+                    $mode = '';
+                    $adds = array_map('trim', explode(',', trim(strtolower(substr($line, 7)))));
+                }
+            } else {
+                if ($mode === 'up') $up[] = $line;
+                if ($mode === 'down') $down[] = $line;
+            }
+        }
+        return [
+            'adds' => $adds,
+            'up' => implode("\n", $up),
+            'down' => implode("\n", $down),
+        ];
+    }
+
+    /**
+     * reads latest schema version from the `schema` table
+     */
+    static function getSchemaVersion($scope) {
+        if (empty(self::$tables)) {
+            self::$tables = self::dbFetchColumn('SHOW TABLES');
+        }
+        if (in_array('schema', self::$tables)) {
+            $version = self::dbFetchOne('SELECT version FROM `schema` WHERE scope = "'.$scope.'"');
+            if (!empty($version)) return $version;
+        }
+        return 0;
+    }
+
+    /**
+     * returns a list of all db migrations for a given `scope`
+     */
+    protected static function getMigrations($scope) {
+        $path = $scope === 'core' ? 'migrations/*.sql' : 'plugins/'.$scope.'/migrations/*.sql';
+        $result = [];
+        foreach (glob(ROOT_PATH . $path) as $sql) {
+            $file = basename($sql, '.sql');
+            $parts = explode('-', $file);
+            $result[] = [
+                'version' => intval(array_shift($parts)),
+                'key' => implode('-', $parts),
+                'content' => self::parseMigrationSQL(file_get_contents($sql))
+            ];
+        }
+        return $result;
+    }
+
+
+    protected static function dbQuery($sql, $verbose=true) {
+        try {
+            $pdo = Propel::getConnection();
+            return $pdo->query($sql);
+        } catch(PDOException $exception){
+            if ($verbose) self::log('mysql', $exception->getMessage(), 'red');
+            throw $exception;
+        } catch (Exception $e) {
+            if ($verbose) self::log('php', $exception->getMessage(), 'red');
+            throw $exception;
+        }
+    }
+
+    protected static function dbFetchColumn($sql) {
+        $res = self::dbQuery($sql);
+        return $res->fetchAll(PDO::FETCH_COLUMN, 0);
+    }
+
+    protected static function dbFetchOne($sql) {
+        $res = self::dbQuery($sql);
+        return $res->fetchColumn(0);
+    }
+
+    protected static $colors = [
+        'white' => '37',
+        'lightgray' => '29',
+        'gray' => '30',
+        'red' => '31',
+        'green' => '32',
+        'yellow' => '33',
+        'blue' => '34',
+        'pink' => '35',
+        'teal' => '36'
+    ];
+
+    protected function log($scope, $message, $color = 'lightgray', $bold = false) {
+        if (defined('NO_SLIM')) {
+            print '['.$scope.'] ';
+            if ($color && isset(self::$colors[$color])) print "\033[".($bold ? '1' : '0').';'.self::$colors[$color]."m";
+            print $message;
+            if ($color) print "\033[m";
+            print "\n";
+        }
+    }
+
+}
--- scripts/sync-schema.php
+++ scripts/sync-schema.php
@@ -3,33 +3,15 @@
 /**
  */
 
+define('NO_SLIM', 1);
 define('ROOT_PATH', dirname(dirname(__FILE__)) . '/');
 require_once ROOT_PATH . 'vendor/autoload.php';
 
+
 Propel::init(ROOT_PATH . "lib/core/build/conf/datawrapper-conf.php");
+require_once ROOT_PATH . 'lib/Migrations.php';
 
-function dbQuery($sql) {
-    try {
-        $pdo = Propel::getConnection();
-        return $pdo->query($sql);
-    } catch(PDOException $exception){
-        print $exception->getMessage();
-    } catch (Exception $e) {
-        print $e->getMessage();
-    }
-}
-
-function dbFetchColumn($sql) {
-    $res = dbQuery($sql);
-    return $res->fetchAll(PDO::FETCH_COLUMN, 0);
-}
-
-function dbFetchOne($sql) {
-    $res = dbQuery($sql);
-    return $res->fetchColumn(0);
-}
-
-$tables = dbFetchColumn('SHOW TABLES');
+use dw\Migrations as Migrations;
 
 // get list of scopes, start with `core`
 $scopes = ['core'];
@@ -40,87 +22,20 @@ foreach (glob(ROOT_PATH . 'plugins/*/migrations') as $path) {
     $scopes[] = $parts[count($parts)-2];
 }
 
-$migrations = [];
-
 // iterate over all scopes
 foreach ($scopes as $scope) {
     // get current schema for scope
-    $version = getSchemaVersion($scope);
-    $migrations[$scope] = getMigrations($scope);
-    print "$scope is on version $version\n";
-    print_r(($migrations[$scope]));
+    Migrations::sync($scope);
 }
 
-/**
- * returns a list of all db migrations for a given `scope`
- */
-function getMigrations($scope) {
-    $path = $scope === 'core' ? 'migrations/*.sql' : 'plugins/'.$scope.'/migrations/*.sql';
-    $result = [];
-    foreach (glob(ROOT_PATH . $path) as $sql) {
-        $file = basename($sql);
-        $parts = explode('-', $file);
-        $result[] = [
-            'version' => intval(array_shift($parts)),
-            'key' => implode('-', $parts),
-            'content' => parseMigrationSQL(file_get_contents($sql))
-        ];
-    }
-    return $result;
-}
 
-/**
- * splits a migration sql file into `adds`, `up` and `down`
- *
- * example migration.sql:
- * -- Adds chart.utf8
- * -- Up
- * ALTER TABLE chart ADD COLUMN utf8 INT;
- * -- Down
- * ALTER TABLE chart DROP COLUMN utf8;
- */
-function parseMigrationSQL($sql) {
-    $lines = explode("\n", $sql);
-    $up = [];
-    $down = [];
-    $adds = [];
-    $mode = '';
-    foreach ($lines as $line) {
-        if (substr($line, 0, 2) === '--') {
-            if (trim(strtolower(substr($line, 2))) === 'up') $mode = 'up';
-            if (trim(strtolower(substr($line, 2))) === 'down') $mode = 'down';
-            if (trim(strtolower(substr($line, 2, 5))) === 'adds') {
-                $mode = '';
-                $adds = array_map(trim, explode(',', trim(strtolower(substr($line, 7)))));
-            }
-        } else {
-            if ($mode === 'up') $up[] = $line;
-            if ($mode === 'down') $down[] = $line;
-        }
-    }
-    return [
-        'adds' => $adds,
-        'up' => implode("\n", $up),
-        'down' => implode("\n", $down),
-    ];
-}
+
+
 
 // check if `schema` DB table exists
 // if yes, get core schema version from there
 // if not, assume version 001 if `chart` db table exists, 000 otherwise
 
-/**
- * reads latest schema version from the `schema` table
- */
-function getSchemaVersion($scope) {
-    global $tables;
-    if (in_array('schema', $tables)) {
-        return dbFetchOne('SELECT version FROM schema WHERE scope = "'.$scope.'"') ?? 0;
-    }
-    return 0;
-}
 
-function migrateToLatest($scope, $fromVersion) {
-    // check if newer migration scripts exist in migrations/
 
-}
+
