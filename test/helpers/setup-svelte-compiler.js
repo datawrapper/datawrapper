46009149e181c4d33ea171efccacc0d5bc405a0f
Simon Jockers <hello@simonjockers.de>
2019-03-14 18:31:10 +0100

    chore: add source map support for svelte components to ava setup

--- test/helpers/setup-svelte-compiler.js
+++ test/helpers/setup-svelte-compiler.js
@@ -1,8 +1,18 @@
 const path = require('path');
 const addHook = require('pirates').addHook;
 const { compile } = require('svelte');
+const sourceMapSupport = require('source-map-support');
 
 const SVELTE_EXTENSION = '.html';
+
+// Add sourcemap support for svelte files
+const sourcemaps = {};
+sourceMapSupport.install({
+    retrieveSourceMap: function(filename) {
+        const map = sourcemaps[filename];
+        return map ? { url: filename, map } : null;
+    }
+});
 
 // We use LESS which requires async preprocessing, which we can't easily do here.
 // To circumvent preprocessing, we simply remove all style tags from the raw code.
@@ -13,16 +23,19 @@ const removeStyles = code => code.replace(/<style([^]*?)>([^]*?)<\/style>/i, '')
 const compileSvelte = (rawCode, filename) => {
     const code = removeStyles(rawCode);
     const { js } = compile(code, {
-        // the filename of the source file, used in e.g. generating sourcemaps.
+        // the filename of the source file, used in e.g. generating sourcemaps
         filename,
 
-        // component name, useful for debugging.
+        // component name, useful for debugging
         name: path.basename(filename, SVELTE_EXTENSION),
 
-        // explicitly set parser for svelte-upgrade.
+        // explicitly set parser for svelte-upgrade
         // TODO: Properly upgrade to v2 (or v3?!)
         parser: 'v2'
     });
+
+    // make the sourcemap available to be retrieved via source map support
+    sourcemaps[filename] = js.map;
 
     return js.code;
 };
