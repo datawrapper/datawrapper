09bb3bc6e5a59d1d38bda8c51d47c6bde1bdf9e5
David Kokkelink <david.kokkelink@gmail.com>
2019-04-09 20:51:44 +0200

    update embed code

--- src/embed/index.js
+++ src/embed/index.js
@@ -1,7 +1,9 @@
 window.addEventListener('message', function(event) {
     if (typeof event.data['datawrapper-height'] !== 'undefined') {
         for (var chartId in event.data['datawrapper-height']) {
-            var iframe = document.getElementById('datawrapper-chart-' + chartId);
+            var iframe = document.getElementById('datawrapper-chart-' + chartId) ||
+                document.querySelector("iframe[src*='" + chartId + "']");
+
             if (!iframe) continue;
             iframe.style.height = event.data['datawrapper-height'][chartId] + 'px';
         }
