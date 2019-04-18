79a884a2701e70987371d20c96c14d3237b9845b
Gregor Aisch <gka@users.noreply.github.com>
2019-04-19 00:24:51 +0200

    allow visualizations to make globalize and jquery optional

--- lib/utils/chart_content.php
+++ lib/utils/chart_content.php
@@ -23,6 +23,10 @@ function get_chart_content($chart, $user, $theme, $published = false, $debug = f
         $locale = $chart->getLanguage();
     }
 
+    // decide if we still need jquery, globalize etc.
+    $vis = DatawrapperVisualization::get($chart->getType());
+    $dependencies = $vis['dependencies'] ?? ['globalize' => true, 'jquery' => true];
+
     $static_path = $GLOBALS['dw_config']['static_path'];
     $abs = $protocol . '://' . $GLOBALS['dw_config']['domain'];
     if ($static_path == 'static/') $static_path = $abs . $static_path;
@@ -33,20 +37,28 @@ function get_chart_content($chart, $user, $theme, $published = false, $debug = f
     $culture = str_replace('_', '-', $locale);
 
     if ($published && !empty($GLOBALS['dw_config']['asset_domain'])) {
-        $base_js = array(
-            '//' . $GLOBALS['dw_config']['asset_domain'] . '/globalize.min.js',
-            '//' . $GLOBALS['dw_config']['asset_domain'] . '/cultures/globalize.culture.' . $culture . '.js',
-            '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
-            '//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js'
-        );
+        $base_js = [
+            '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js'
+        ];
+        if ($dependencies['globalize']) {
+            $base_js[] = '//' . $GLOBALS['dw_config']['asset_domain'] . '/globalize.min.js';
+            $base_js[] = '//' . $GLOBALS['dw_config']['asset_domain'] . '/cultures/globalize.culture.' . $culture . '.js';
+        }
+        if ($dependencies['jquery']) {
+            $base_js[] = '//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js';
+        }
     } else {
         // use "local" assets
         $base_js = array(
-            $abs . '/static/vendor/globalize/globalize.min.js',
-            $abs . '/static/vendor/globalize/cultures/globalize.culture.' . $culture . '.js',
-            $abs . '/static/vendor/underscore/underscore-min.js',
-            $abs . '/static/vendor/jquery/jquery.min.js'
+            $abs . '/static/vendor/underscore/underscore-min.js'
         );
+        if ($dependencies['globalize']) {
+            $base_js[] = $abs . '/static/vendor/globalize/globalize.min.js';
+            $base_js[] = $abs . '/static/vendor/globalize/cultures/globalize.culture.' . $culture . '.js';
+        }
+        if ($dependencies['jquery']) {
+            $base_js[] = $abs . '/static/vendor/jquery/jquery.min.js';
+        }
     }
 
     $vis_js = array();
