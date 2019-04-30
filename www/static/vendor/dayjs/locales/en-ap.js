694bdca9a414b92dc8f0ef545b2393ac086072fe
Gregor Aisch <gka@users.noreply.github.com>
2019-04-30 10:24:31 +0200

    improve "AP" style dayjs locale

--- www/static/vendor/dayjs/locales/en-ap.js
+++ www/static/vendor/dayjs/locales/en-ap.js
@@ -4,6 +4,7 @@
  * - Always use Arabic figures, without st, nd, rd or th
  * - Capitalize months
  * - When a month is used with a specific date, abbreviate only Jan., Feb., Aug.,Sept., Oct., Nov. and Dec. (e.g. Oct. 4 was the day of her birthday.)
+ * - use "a.m." and "p.m." instead of AM/PM
  */
 export default {
     name: 'en-ap',
@@ -12,5 +13,29 @@ export default {
     weekdaysShort: 'Sun._Mon._Tue._Wed._Thu._Fri._Sat.'.split('_'),
     monthsShort: 'Jan._Feb._March_April_May_June_July_Aug._Sept._Oct._Nov._Dec.'.split('_'),
     weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
-    ordinal: n => n
+    ordinal: n => n,
+    meridiem: hour => (hour < 12 ? 'a.m.' : 'p.m.'),
+    relativeTime: {
+        future: 'in %s',
+        past: '%s ago',
+        s: 'a few seconds',
+        m: 'a minute',
+        mm: '%d minutes',
+        h: 'an hour',
+        hh: '%d hours',
+        d: 'a day',
+        dd: '%d days',
+        M: 'a month',
+        MM: '%d months',
+        y: 'a year',
+        yy: '%d years'
+    },
+    formats: {
+        LT: 'HH:mm',
+        LTS: 'HH:mm:ss',
+        L: 'DD/MM/YYYY',
+        LL: 'D MMMM YYYY',
+        LLL: 'D MMMM YYYY HH:mm',
+        LLLL: 'dddd, D MMMM YYYY HH:mm'
+    }
 };
