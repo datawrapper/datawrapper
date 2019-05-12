f1c55e32f772aa83d91c0725f24cdc470b0fa72f
David Kokkelink <david@datawrapper.de>
2019-05-13 01:34:34 +0200

    add helper function to determine path of static files on filesystem

--- lib/utils/get_plugin_path.php
+++ lib/utils/get_plugin_path.php
@@ -3,3 +3,10 @@
 function get_plugin_path() {
     return $GLOBALS['dw_config']['plugin_directory'] ?? (ROOT_PATH . 'plugins/');
 }
+
+function get_plugin_static_path($name) {
+	return (isset($GLOBALS['dw_config']['copy_plugin_assets']) &&
+            $GLOBALS['dw_config']['copy_plugin_assets'] === false) ?
+		ROOT_PATH . '/plugins/' . $name . '/static/' :
+		ROOT_PATH . '/www/static/plugins/' . $name . '/';
+}
