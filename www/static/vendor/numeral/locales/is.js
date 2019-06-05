064f52bde8a372bda1b5a97292349db8bcb2f754
Gregor Aisch <gka@users.noreply.github.com>
2019-06-05 16:24:59 +0200

    add icelandic numeral locale

---
+++ www/static/vendor/numeral/locales/is.js
@@ -0,0 +1,18 @@
+export default {
+    delimiters: {
+        thousands: '.',
+        decimal: ','
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal: function(number) {
+        return '.';
+    },
+    currency: {
+        symbol: 'ISK'
+    }
+};
