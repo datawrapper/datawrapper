2601675117ae432c9222cd1ed45c157a2049fd1e
Gregor Aisch <gka@users.noreply.github.com>
2019-07-31 18:48:24 +0200

    return request origin if all origins are allowed

--- www/api/index.php
+++ www/api/index.php
@@ -100,7 +100,7 @@ $app->hook('slim.before.router', function () use ($app, $dw_config) {
                 $app->response()->status(200);
                 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
                 header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
-                header('Access-Control-Allow-Origin: *');
+                header('Access-Control-Allow-Origin: '.(empty($origin) ? '*' : $origin));
                 header('Access-Control-Allow-Credentials: true');
                 die();
             }
