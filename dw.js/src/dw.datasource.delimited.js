0341925a6e986f3f00cfdd932773d14dea7a28ea
David Kokkelink <david@datawrapper.de>
2019-09-11 12:46:53 +0200

    only include cookies for local requests

--- dw.js/src/dw.datasource.delimited.js
+++ dw.js/src/dw.datasource.delimited.js
@@ -16,12 +16,17 @@
 dw.datasource.delimited = function(opts) {
     function loadAndParseCsv() {
         if (opts.url) {
+            var host = (window.location.hostname || "").split("."),
+                rootDomain = "";
+
+            if (host.length >= 2) var rootDomain = host[host.length-2] + "." + host[host.length-1];
+
             return $.ajax({
                 url: opts.url + (opts.url.indexOf('?') > -1 ? '&' : '?') + 'v=' + new Date().getTime(),
                 method: 'GET',
                 dataType: 'text',
                 xhrFields: {
-                    withCredentials: true
+                    withCredentials: (opts.url.indexOf(rootDomain) > -1 )
                 }
             }).then(function(raw) {
                 return new DelimitedParser(opts).parse(raw);
