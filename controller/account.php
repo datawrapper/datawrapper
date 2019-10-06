0023fee9981f663ac9562d2ed6732f3cc7a0e634
David Kokkelink <david@datawrapper.de>
2019-10-06 20:29:13 +0200

    move switch team functionality to core

--- controller/account.php
+++ controller/account.php
@@ -67,7 +67,24 @@ call_user_func(function() {
             add_header_vars($page, 'account');
 
             $app->render('account.twig', $page);
-        }        
+        }
+    });
+
+    $app->put('/team/:org_id/activate', function($org_id) use ($app) {
+        disable_cache($app);
+        $user = DatawrapperSession::getUser();
+        $orgs = $user->getActiveOrganizations();
+        foreach ($orgs as $org) {
+            if ($org->getId() == $org_id) {
+                $_SESSION['dw-user-organization'] = $org_id;
+                print json_encode(array('status' => 'ok'));
+                return;
+            }
+        }
+        print json_encode(array(
+            'status' => 'error',
+            'message'=> __('Organization not found')
+        ));
     });
 
 });
