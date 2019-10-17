144ea7b641b464a168716d0b62f1520c2a209fee
David Kokkelink <david@datawrapper.de>
2019-10-17 04:59:17 +0200

    please the linting gods

--- src/resizer/rollup.config.js
+++ src/resizer/rollup.config.js
@@ -1,5 +1,4 @@
 /* eslint-env node, es6 */
-import less from 'less';
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
