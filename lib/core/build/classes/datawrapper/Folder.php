612d8f5e779d81cc22305d75dbcce4579a32faa4
Gregor Aisch <gka@users.noreply.github.com>
2019-10-29 15:02:55 +0100

    add breadcrumb folder nav to chart editor

--- lib/core/build/classes/datawrapper/Folder.php
+++ lib/core/build/classes/datawrapper/Folder.php
@@ -19,6 +19,9 @@ class Folder extends BaseFolder {
     public function getId() {
         return $this->getFolderId();
     }
+    public function getName() {
+        return $this->getFolderName();
+    }
 
     /*
      * check if a given user is allowed access to this folder
