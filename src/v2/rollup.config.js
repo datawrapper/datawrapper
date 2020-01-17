3249598bbabbd40c91e80fb1300490b4584d9137
Gregor Aisch <gka@users.noreply.github.com>
2020-01-17 14:46:21 +0100

    rewrite signin modal in svelte

--- src/v2/rollup.config.js
+++ src/v2/rollup.config.js
@@ -15,6 +15,7 @@ build('fields');
 build('team-settings');
 build('account');
 build('chart-breadcrumb');
+build('signin');
 build('publish', { append: '_old' });
 build('publish', { noAMD: true, entry: 'index.js' });
 build('publish/sidebar', { noAMD: true });
