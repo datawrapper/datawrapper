8c71f556451b94d64cde7a864b8cf5e9b908719d
Gregor Aisch <gka@users.noreply.github.com>
2020-02-17 12:11:57 +0100

    allow NULL as email address

--- migrations/011-unique-email.sql
+++ migrations/011-unique-email.sql
@@ -1,4 +1,5 @@
 -- Up
+ALTER TABLE `user` MODIFY COLUMN email varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 UPDATE `user` SET email = NULL WHERE email = 'DELETED' OR email = '';
 ALTER TABLE `user` ADD CONSTRAINT uniq_email UNIQUE KEY (email);
 
