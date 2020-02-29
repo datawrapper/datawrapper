d0305e0e1109faeb6def4c66afbbe2e84ae278d0
David Kokkelink <david@datawrapper.de>
2020-02-29 19:07:04 +0100

    remove unused hooks

--- lib/hooks/DatawrapperHooks.php
+++ lib/hooks/DatawrapperHooks.php
@@ -218,13 +218,6 @@ class DatawrapperHooks {
     const SEND_TEAM_INVITE_EMAIL = 'send_team_invite_email';
     const SEND_RESET_PASSWORD_EMAIL = 'send_reset_password_email';
     const SEND_CHANGE_EMAIL_EMAIL = 'send_change_email_email';
-    const SEND_FIRST_INVOICE_EMAIL = 'send_first_invoice_email';
-    const SEND_MANUAL_INVOICE_EMAIL = 'send_manual_invoice_email';
-    const SEND_INVOICE_EMAIL = 'send_invoice_email';
-    const SEND_CARD_EXPIRY_EMAIL = 'send_card_expiry_email';
-    const SEND_FAILED_PAYMENT_EMAIL = 'send_failed_payment_email';
-    const SEND_DOWNGRADE_EMAIL = 'send_downgrade_email';
-    const SEND_TEAM_SETUP_EMAIL = 'send_team_setup_email';
 
     // hooks to add markup in chart editor steps below nav
     const BEFORE_EDITOR_NAV = 'before_editor_nav';
@@ -242,7 +235,6 @@ class DatawrapperHooks {
     const PUBLISH_CUSTOM_PAGE_GUEST = 'publish_custom_page_guest';
     const PUBLISH_CUSTOM_PAGE_PENDING_ACTIVATION = 'publish_custom_page_pending_activation';
     const PUBLISH_CUSTOM_PAGE_NOT_ALLOWED = 'publish_custom_page_not_allowed';
-    const PUBLISH_CUSTOM_EXPORT_ACTIONS_HEADER = 'publish_custom_export_actions_header';
 
     // hooks for plugin installation/update
     const PLUGIN_INSTALLED = 'plugin_installed';
