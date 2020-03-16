0b894f71e86d2e1e42bf20a18acf4f81b27b7e92
David Kokkelink <david@datawrapper.de>
2020-03-16 14:44:41 +0100

    Trident is internet explorer

--- controller/polyfill.php
+++ controller/polyfill.php
@@ -4,7 +4,7 @@
 $app->get('/polyfill', function () use ($app) {
     disable_cache($app);
 
-    if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) || (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident/7.0; rv:11.0') !== false)) {
+    if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) || (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident/7.0;') !== false)) {
         $app->redirect('https://datawrapper.dwcdn.net/lib/polyfills/ie-11.js');
     } else {
         $app->response()->header('Content-Type', 'application/javascript');
