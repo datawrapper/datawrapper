bcb300d9d7592fbc6003d097005c787ce34f84aa
Exolon Voncor <git@exoz.net>
2020-03-26 14:55:22 +0100

    fix autogenerated expressions (test your stuff!)

--- migrations/012-add-prod-indices.sql
+++ migrations/012-add-prod-indices.sql
@@ -1,21 +1,19 @@
 -- Create indices that are present in production.
 
 -- Up
-CREATE INDEX action_key_IDX USING BTREE ON action (key, user_id);
+CREATE INDEX action_key_IDX USING BTREE ON action (`key`, user_id);
 CREATE INDEX action_action_time_IDX USING BTREE ON action (action_time);
 
-CREATE INDEX chart_idx_is_fork ON chart (is_fork);
-CREATE INDEX chart_author ON chart USING BTREE (author_id);
-CREATE INDEX chart_in_folder_IDX ON chart USING BTREE (in_folder);
-CREATE INDEX chart_organization_id_IDX ON chart USING BTREE (organization_id);
-CREATE INDEX chart_type_IDX ON chart USING BTREE (type);
-CREATE INDEX chart_deleted_IDX ON chart USING BTREE (deleted);
-CREATE INDEX chart_last_modified_at_IDX ON chart USING BTREE (last_modified_at);
-CREATE INDEX chart_forkable_IDX ON chart USING BTREE (forkable);
-CREATE INDEX chart_external_data_IDX ON chart USING BTREE (external_data);
-CREATE INDEX chart_last_edit_step_IDX ON chart USING BTREE (last_edit_step);
-CREATE INDEX chart_guest_session_IDX ON chart USING BTREE (guest_session);
-
+CREATE INDEX chart_idx_is_fork USING BTREE ON chart (is_fork);
+CREATE INDEX chart_author USING BTREE ON chart (author_id);
+CREATE INDEX chart_in_folder_IDX USING BTREE ON chart (in_folder);
+CREATE INDEX chart_organization_id_IDX USING BTREE ON chart (organization_id);
+CREATE INDEX chart_type_IDX USING BTREE ON chart (type);
+CREATE INDEX chart_deleted_IDX USING BTREE ON chart (deleted);
+CREATE INDEX chart_last_modified_at_IDX USING BTREE ON chart (last_modified_at);
+CREATE INDEX chart_forkable_IDX USING BTREE ON chart (forkable);
+CREATE INDEX chart_external_data_IDX USING BTREE ON chart (external_data);
+CREATE INDEX chart_last_edit_step_IDX USING BTREE ON chart (last_edit_step);
 CREATE INDEX chart_guest_session_IDX USING BTREE ON chart (guest_session);
 
 -- Down
@@ -32,6 +30,4 @@ DROP INDEX chart_last_modified_at_IDX ON chart;
 DROP INDEX chart_forkable_IDX ON chart;
 DROP INDEX chart_external_data_IDX ON chart;
 DROP INDEX chart_last_edit_step_IDX ON chart;
-DROP INDEX chart_guest_session_IDX ON chart;
-
 DROP INDEX chart_guest_session_IDX ON chart;
