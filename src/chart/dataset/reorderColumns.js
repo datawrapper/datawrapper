9171b5355526f958e0693ed1ccb9d24110970658
Gregor Aisch <gka@users.noreply.github.com>
2020-04-30 17:40:29 +0200

    remove obsolete JS, import from shared & controls + update build system

--- src/chart/dataset/reorderColumns.js
+++
@@ -1,7 +0,0 @@
-export default function(chart, dataset) {
-    var order = chart.getMetadata('data.column-order', []);
-    if (order.length && order.length === dataset.numColumns()) {
-        dataset.columnOrder(order);
-    }
-    return dataset;
-}
