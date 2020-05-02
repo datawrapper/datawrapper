9b7aa05fd64c9b26fd7c4836fd22a5196ba8c71c
David Kokkelink <david@datawrapper.de>
2020-05-02 14:07:24 +0200

    use correct endpoint

--- controller/chart/embed.php
+++ controller/chart/embed.php
@@ -5,7 +5,7 @@
  */
 $app->get('/chart/:id/', function ($id) use ($app) {
     check_chart_public($id, function($user, $chart) use ($app) {
-        $app->redirect("/preview/$id?published=1");
+        $app->redirect("/preview/" . $chart->getId() . "?published=1");
         disable_cache($app);
     });
 });
