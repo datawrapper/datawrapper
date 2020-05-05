5650c0edc854f7977bd32a9399e9142c95accf16
David Kokkelink <david@datawrapper.de>
2020-05-06 00:28:06 +0200

    remove memcache, legacy functions

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -124,12 +124,6 @@ if (!defined('NO_SLIM')) {
             'session.handler' => null
         ));
     }
-} 
-
-if (isset($dw_config['memcache'])) {
-    $memcfg = $dw_config['memcache'];
-    $memcache = new Memcache();
-    $memcache->connect($memcfg['host'], $memcfg['port']) or die ("Could not connect");
 }
 
 if (isset($dw_config['charts-s3'])) {
