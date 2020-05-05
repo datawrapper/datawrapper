969b7470a09d4db4f03200499022d1daa7d41689
David Kokkelink <david@datawrapper.de>
2020-05-06 00:14:19 +0200

    delete more unused files

--- templates/chart/embed.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";window.addEventListener("message",function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}})}();
