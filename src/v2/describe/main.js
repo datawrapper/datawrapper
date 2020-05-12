1dd36b64ce354390de6c08f340ac7ac13adb419d
Gregor Aisch <gka@users.noreply.github.com>
2020-05-12 12:58:49 +0200

    code formatting + linting

--- src/v2/describe/main.js
+++ src/v2/describe/main.js
@@ -4,14 +4,14 @@ import { Store } from 'svelte/store.js';
 const store = new Store({});
 
 const data = {
-	chart: {
-		id: ''
-	},
-	readonly: false,
-	chartData: '',
-	transpose: false,
-	firstRowIsHeader: true,
-	skipRows: 0
+    chart: {
+        id: ''
+    },
+    readonly: false,
+    chartData: '',
+    transpose: false,
+    firstRowIsHeader: true,
+    skipRows: 0
 };
 
 export default { App, data, store };
