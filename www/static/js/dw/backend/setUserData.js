345657541925f62ba6350d00ccde2fc44026b027
Jakub Valenta <jakub@datawrapper.de>
2020-09-14 10:34:57 +0200

    setUserData: Fix crash when sending updatedAt
    We must not send updatedAt to the /v3/me/data endpoint. It returns 400,
    because updatedAt is not a valid property.

--- www/static/js/dw/backend/setUserData.js
+++ www/static/js/dw/backend/setUserData.js
@@ -2,9 +2,11 @@ define(function (require) {
     var httpReq = require('./httpReq');
 
     return function (data) {
+        var payload = Object.assign({}, data);
+        delete payload.updatedAt;
         httpReq
             .patch('/v3/me/data', {
-                payload: data
+                payload: payload
             })
             .then(function (data) {
                 _.each(data, function (value, key) {
