92eba40f07c5c3b21bbf9a699ee4ae080824888a
David Kokkelink <david@datawrapper.de>
2022-06-27 19:22:36 +0200

    chore(web-components): Refactor flags and postEvent usage (#1713)

--- libs/shared/postEvent.js
+++ libs/shared/postEvent.js
@@ -1,28 +1,30 @@
 /**
- * Use this function to post event messages out of Datawrapper iframe embeds
- * to the parent website.
+ * Use this function to post event messages out of Datawrapper iframe and
+ * web component embeds to the parent website.
  *
  * @exports postEvent
  * @kind function
  *
  * @param {string} chartId - the chart id each message should be signed with
+ * @param {boolean} isIframe - render context (`true`: iframe, `false`: web component)
  * @returns {function}
  *
  * @example
  * import genPostEvent from '@datawrapper/shared/postEvent';
- * const postEvent = genPostEvent(chart.get('id'));
- * postEvent('bar:hover', {value: 123});
+ * const postEvent = genPostEvent(chart.get('id'), true);
+ * postEvent('bar.click', { value: 123 });
  */
-export default function postEvent(chartId) {
+export default function postEvent(chartId, isIframe) {
+    const host = isIframe ? window.parent : window;
     return function (event, data) {
-        if (window.parent && window.parent.postMessage) {
+        if (host && host.postMessage) {
             const evt = {
                 source: 'datawrapper',
                 chartId,
                 type: event,
                 data
             };
-            window.parent.postMessage(evt, '*');
+            host.postMessage(evt, '*');
         }
     };
 }
