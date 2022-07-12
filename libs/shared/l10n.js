c2b697b3159886c7a7899937da4e7501906d3f0e
Simon Jockers <449739+sjockers@users.noreply.github.com>
2022-07-12 13:03:56 +0200

    feat(shared): Add HTML sanitization and string replacement to l10n (#1789)

--- libs/shared/l10n.js
+++ libs/shared/l10n.js
@@ -1,8 +1,15 @@
-/* globals dw */
+import purifyHtml from './purifyHtml.js';
+
+const ALLOWED_HTML =
+    '<p><h1><h2><h3><h4><h5><h6><blockquote><ol><ul><li><pre><hr><br>' + // Block elements (Markdown official)
+    '<a><em><i><strong><b><code><img>' + // Inline elements (Markdown official)
+    '<table><tr><th><td>' + // Tables
+    '<small><span><div><sup><sub><tt>'; // Additional tags to support advanced customization
 
 const __messages = {};
-
 function initMessages(scope = 'core') {
+    /* globals dw */
+
     // let's check if we're in a chart
     if (scope === 'chart') {
         if (window.__dw && window.__dw.vis && window.__dw.vis.meta) {
@@ -18,35 +25,72 @@ function initMessages(scope = 'core') {
     }
 }
 
+function getText(key, scope, messages) {
+    try {
+        const msg = messages[scope];
+        return msg[key] || key;
+    } catch (e) {
+        return key;
+    }
+}
+
+// replace named placeholders marked with %, such as %name% or %id
+function replaceNamedPlaceholders(text, replacements = {}) {
+    Object.entries(replacements).forEach(([k, v]) => {
+        text = text.replace(new RegExp(`%${k}%|%${k}(?!\\w)`, 'g'), v);
+    });
+    return text;
+}
+
+// replace numbered placeholders marked with $, such as $0, $1 etc
+function replaceNumberedPlaceholders(text, replacements = []) {
+    return text.replace(/\$(\d)/g, (m, i) => {
+        if (replacements[+i] === undefined) return m;
+        return purifyHtml(replacements[+i], '');
+    });
+}
+
 /**
- * translates a message key. translations are originally stored in a
- * Google spreadsheet that we're pulling into Datawrapper using the
- * `scripts/update-translations` script, which stores them as `:locale.json`
- * files in the /locale folders (both in core as well as inside plugin folders)
+ * translates a message key, replaces placeholders within translated strings,
+ * and sanitizes the result of the translation so that it can be safely used in HTML
  *
- * for the client-side translation to work we are also storing the translations
- * in the global `window.dw.backend.__messages` object. plugins that need
+ * @param {string} key -- the key to be translated, e.g. "signup / hed"
+ * @param {string} scope -- the translation scope, e.g. "core" or a plugin name
+ * @param {object} messages -- translation strings in the format of { scope: { key: value }}
+ * @param {string|object} replacements -- replacements for placeholders in the translations strings
+ * @returns {string} -- the translated text
+ */
+export function translate(key, scope = 'core', messages, ...replacements) {
+    let text = getText(key, scope, messages);
+    if (typeof replacements[0] === 'string') {
+        // use legacy, parameterized string replacements ($0, $1, etc.)
+        text = replaceNumberedPlaceholders(text, replacements);
+    } else {
+        // use object for string replacement (i.e. %key% for { key: 'value' })
+        text = replaceNamedPlaceholders(text, replacements[0]);
+    }
+    return purifyHtml(text, ALLOWED_HTML);
+}
+
+/**
+ * helper for finding a translation key based on a globally accessible dictionary
+ * (to be used e.g. in visualization plugins and legacy Svelte 2 code)
+ *
+ * translates a message key, replaces placeholders within translated strings,
+ * and sanitizes the result of the translation so that it can be safely used in HTML
+ *
+ * for the client-side translation to work we are pulling the translations
+ * from the global `window.dw.backend.__messages` object. plugins that need
  * client-side translations must set `"svelte": true` in their plugin.json
  *
  * @param {string} key -- the key to be translated, e.g. "signup / hed"
  * @param {string} scope -- the translation scope, e.g. "core" or a plugin name
+ * @param {string|object} replacements -- replacements for placeholders in the translations strings
  * @returns {string} -- the translated text
  */
-function __(key, scope = 'core') {
+export function __(key, scope = 'core', ...replacements) {
     key = key.trim();
     if (!__messages[scope]) initMessages(scope);
     if (!__messages[scope][key]) return 'MISSING:' + key;
-    var translation = __messages[scope][key];
-
-    if (typeof translation === 'string' && arguments.length > 2) {
-        // replace $0, $1 etc with remaining arguments
-        translation = translation.replace(/\$(\d)/g, (m, i) => {
-            i = 2 + Number(i);
-            if (arguments[i] === undefined) return m;
-            return arguments[i];
-        });
-    }
-    return translation;
+    return translate(key, scope, __messages, ...replacements);
 }
-
-export { __ };
