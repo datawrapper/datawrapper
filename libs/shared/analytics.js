23cc88172356b3582d7f2014d105d0a9ba61141b
Gregor Aisch <gka@users.noreply.github.com>
2022-08-19 12:55:24 +0000

    fix(frontend): track views to single-page app views (#2036)
    * fix(frontend): track views to single-page app views
    
    * also track river page views + actions
    
    * chore(frontend): add more integration tests
    
    * chore(shared): bump version and publish
    
    * chore(frontend): set dev mode in test rollups
    
    * create chart to get hello tests to run
    
    * use default theme
    
    * chore(shared): make equalish test more reliable
    
    in order to avoid test runs like this
    https://github.com/datawrapper/code/runs/7917895550?check_suite_focus=true
    
    * chart needs to be published

--- libs/shared/analytics.js
+++ libs/shared/analytics.js
@@ -20,17 +20,19 @@ let previousPageUrl;
  * apps in Datawrapper, such as the locator maps UI. The page title
  * and URL are automatically detected using the window object.
  *
- * @param {number} loadTime - optional page load time, has to be measured
- *    manually
+ * @param {number|string} userId - optional user id of authenticated user
+ * @param {string} teamId - optional team id of page context
  */
-export function trackPageView(loadTime = 0) {
+export function trackPageView(userId, teamId) {
     if (window._paq) {
         if (previousPageUrl) {
             window._paq.push(['setReferrerUrl', previousPageUrl]);
         }
-        window._paq.push(['setGenerationTimeMs', loadTime]);
+
         window._paq.push(['setCustomUrl', window.location.pathname]);
         window._paq.push(['setDocumentTitle', window.document.title]);
+        if (userId) window._paq.push(['setUserId', userId]);
+        if (teamId) window._paq.push(['setCustomDimension', 1, teamId]);
         window._paq.push(['trackPageView']);
         previousPageUrl = window.location.pathname;
     }
