d81db09f3777c5ff5e713d84a55a8b119978c79f
Elliot Bentley <mail@elliotbentley.com>
2022-10-11 14:23:47 +0100

    feat(d3-bars): show totals for stacked bars (#2226)
    * Totals labels for stacked bars
    
    * Theming for stacked bar labels
    
    * Align decimals
    
    * Show absolute totals for % stacked bars
    
    * Localise controls
    
    * Apply correct classes when totals suppressed with someNegative
    
    * Only disable totals if a bar contains mixed pos/neg values; show explanations in controls
    
    Co-authored-by: elanals <19191012+elanals@users.noreply.github.com>

---
+++ libs/shared/hideExtraZeros.test.js
@@ -0,0 +1,37 @@
+import test from 'ava';
+
+import { forceOptionalDecimals, hideExtraZeros } from './hideExtraZeros.js';
+
+test('forceOptionalDecimals', async t => {
+    t.is(forceOptionalDecimals('$1,000.[0]'), '$1,000.0');
+
+    t.is(forceOptionalDecimals('0%'), '0%');
+
+    t.is(forceOptionalDecimals('0.[00]%'), '0.[00]%');
+
+    t.is(forceOptionalDecimals('0.0'), '0.0');
+
+    t.is(forceOptionalDecimals(' 0.0 '), '0.0');
+});
+
+test('hideExtraZeros', async t => {
+    t.is(
+        hideExtraZeros('$723.0', '$1,000.[0]'),
+        `$723<span class="decimals" aria-hidden="true">.0</span>`
+    );
+
+    t.is(hideExtraZeros('$5,710.2', '$1,000.[0]'), `$5,710.2`);
+
+    t.is(
+        hideExtraZeros('$34.0', '$1,000.[0]'),
+        `$34<span class="decimals" aria-hidden="true">.0</span>`
+    );
+
+    // if suffix, don't bother trying to align
+    t.is(hideExtraZeros('723.00%', '0.[00]%'), `723.00%`);
+
+    t.is(
+        hideExtraZeros(' $723.0 ', ' $1,000.[0] '),
+        `$723<span class="decimals" aria-hidden="true">.0</span>`
+    );
+});
