26aea1bcc5465882ee488a99d418c7033dc5116e
Antonio Sarcevic <antonio@datawrapper.de>
2024-01-08 16:11:20 +0100

    feat(app): Implement use:presenceNotifier in Svelte 2 component (#4934)

---
+++ libs/shared/src/presenceNotifier.ts
@@ -0,0 +1,65 @@
+import type { Action } from 'svelte/action';
+import { Writable } from 'svelte/store';
+
+export type CollaborationRoom = Writable<{
+    pinAnchors: Map<string, { element: HTMLElement; offset?: { x: number; y: number } }>;
+}> & {
+    /** Used to send focus and blur messages to other users in the same collaboration room. */
+    sendPresenceMessage: (path: string, isFocus?: boolean) => void;
+};
+
+type Options = { room: CollaborationRoom; path: string; offset?: { x: number; y: number } };
+
+/**
+ *  Svelte Action enabling the control level presence indicator placement.
+ *
+ *  Handles the registering of anchor points and commuinicating
+ *  when users are focusing and bluring inputs over web sockets.
+ */
+export const presenceNotifier = ((element: HTMLElement, options: Options) => {
+    let { room, path, offset } = options;
+
+    if (!room) {
+        // without a collaboration room we do nothing
+        return {};
+    }
+
+    if (!path?.trim()) {
+        console.warn('[presenceNotifier] invalid control path: ' + path, element);
+        return {};
+    }
+
+    function registerAnchor() {
+        room.update($room => {
+            $room.pinAnchors.set(path, { element, offset });
+            return $room;
+        });
+    }
+    registerAnchor();
+
+    const handleFocusIn = () => room.sendPresenceMessage(path, true);
+    const handleFocusOut = () => room.sendPresenceMessage(path, false);
+    element.addEventListener('focusin', handleFocusIn);
+    element.addEventListener('focusout', handleFocusOut);
+
+    return {
+        update(options: Options) {
+            // make options reactive
+            ({ room, path, offset } = options);
+            registerAnchor();
+        },
+        destroy() {
+            // send blur, to remove pin for others
+            handleFocusOut();
+
+            // unregister anchor
+            room.update($room => {
+                $room.pinAnchors.delete(path);
+                return $room;
+            });
+
+            element.removeEventListener('focusin', handleFocusIn);
+            element.removeEventListener('focusout', handleFocusOut);
+        }
+    };
+}) satisfies Action<HTMLElement, Options>;
