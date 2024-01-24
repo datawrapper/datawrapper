e8e996935b2e53983b15c65675919af3992ff404
Julian Freyberg <62940341+jfreyberg@users.noreply.github.com>
2024-01-24 10:57:50 +0100

    feat(app): add unique ids to chart metadata annotations (#4993)

---
+++ libs/shared/src/generateAnnotationId.ts
@@ -0,0 +1,11 @@
+import { nanoid } from 'nanoid';
+
+/**
+ * Generates a unique id for annotations.
+ * We need to assign ids to annotations so that we can perform fine-grained conflict resolution on them (see {@link CRDT}).
+ * It's part of shared because it's used by old and new controls.
+ * @returns a random string of length 10
+ */
+export function generateAnnotationId() {
+    return nanoid(10);
+}
