e22d90ffb56e8507449466f4b7fada54e9499dd6
Linus Aarnio <42450444+linusaarnio@users.noreply.github.com>
2024-01-30 14:26:54 +0200

    chore(collaboration): Refactor logic from socket.ts into separate classes and add tests (#5110)
    * Refactor logic from socket.ts into CollaboraticeChart and CollaborationClient
    
    * Add tests
    
    * Cleanup

--- libs/shared/src/crdt/index.ts
+++ libs/shared/src/crdt/index.ts
@@ -1,4 +1,4 @@
 export { CRDT } from './crdt.js';
 export { CRDTWithClock } from './crdtWithClock.js';
-export { nodeIdFromTimestamp, validateTimestamp } from './clock.js';
+export { nodeIdFromTimestamp, validateTimestamp, Timestamps } from './clock.js';
 export type { Patch } from './crdt.js';
