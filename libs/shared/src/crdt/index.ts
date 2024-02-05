62e1eecc1f0bed98a91556afc6183a95fca753ed
Marten Sigwart <marten@datawrapper.de>
2024-02-05 17:54:47 +0200

    feat(collaboration): support multiple clients with same user (#5176)
    * add test case to check for re-broadcasted foreign patches
    
    * install testcontainers
    
    * rewrite tests to use test containers, add two tests
    
    * don't emit already received patches as foreign updates
    
    * remove unused nodeId
    
    * remove unused
    
    * reject in case of failure
    
    Co-authored-by: Linus Aarnio <42450444+linusaarnio@users.noreply.github.com>
    
    * use Timestamp type
    
    Co-authored-by: Linus Aarnio <42450444+linusaarnio@users.noreply.github.com>
    
    * cleanup
    
    * export & import Timestamp
    
    * apply random 5-digit number postfix to node ID to ensure uniqueness
    
    ---------
    
    Co-authored-by: Linus Aarnio <42450444+linusaarnio@users.noreply.github.com>

--- libs/shared/src/crdt/index.ts
+++ libs/shared/src/crdt/index.ts
@@ -1,4 +1,4 @@
 export { CRDT } from './crdt.js';
 export { CRDTWithClock } from './crdtWithClock.js';
-export { Clock, Timestamps } from './clock.js';
+export { Clock, Timestamp, Timestamps } from './clock.js';
 export type { Patch } from './crdt.js';
