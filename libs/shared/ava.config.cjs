a6c385c61e17c2e1aab16980a4277ef268051eba
Linus Aarnio <42450444+linusaarnio@users.noreply.github.com>
2024-02-05 22:17:46 +0200

    chore(configs): Use --import instead of --loader for configuring tsx as a loader for ava (#5183)

--- libs/shared/ava.config.cjs
+++ libs/shared/ava.config.cjs
@@ -2,6 +2,6 @@ module.exports = {
     extensions: ['ts'],
     files: ['src/**/*.test.*', '!src/**/*.perf.test.*'],
     require: ['./test/helpers/setup-browser-env.ts'],
-    nodeArguments: ['--loader=tsx', '--no-warnings'],
+    nodeArguments: ['--import=tsx', '--no-warnings'],
     timeout: '40s'
 };
