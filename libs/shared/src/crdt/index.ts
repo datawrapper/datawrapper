7bbd677df139d798c426db2a1fd70547074b38af
Jack Goodall <jack@datawrapper.de>
2024-02-20 11:29:00 +0100

    feat(collaboration): refactor reconnection logic (#5254)
    Co-authored-by: Marten Sigwart <marten@datawrapper.de>

--- libs/shared/src/crdt/index.ts
+++ libs/shared/src/crdt/index.ts
@@ -1,4 +1,4 @@
-export { JsonCRDT } from './JsonCRDT.js';
+export { JsonCRDT, SerializedJsonCRDT } from './JsonCRDT.js';
 export { SimpleCRDT } from './SimpleCRDT.js';
 export { Clock, Timestamp, Timestamps } from './Clock.js';
 export type { CRDT, Diff, Update } from './CRDT.js';
