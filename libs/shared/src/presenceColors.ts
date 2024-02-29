7141cd219f4c29b2226bd592957605db32b7bd0b
Jack Goodall <jack@datawrapper.de>
2024-02-29 18:35:14 +0100

    feat(collaboration): assign random default color to onboarding users (#5367)

---
+++ libs/shared/src/presenceColors.ts
@@ -0,0 +1,18 @@
+export const presenceColors = [
+    'color-dw-presence-10',
+    'color-dw-presence-20',
+    'color-dw-presence-30',
+    'color-dw-presence-40',
+    'color-dw-presence-50',
+    'color-dw-presence-60',
+    'color-dw-presence-70',
+    'color-dw-presence-80',
+    'color-dw-presence-90',
+    'color-dw-presence-100',
+    'color-dw-presence-110',
+    'color-dw-presence-120'
+] as const;
+
+export function getRandomPresenceColor() {
+    return presenceColors[Math.floor(Math.random() * presenceColors.length)];
+}
