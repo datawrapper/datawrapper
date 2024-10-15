ab9929c48cf1e90c360255632c8cf18c3d03744e
elanals <19191012+elanals@users.noreply.github.com>
2024-10-15 10:44:06 +0200

    feat(vis-d3-maps): force area axis for arrow shape type (#7003)

---
+++ libs/chart-core/lib/dw/utils/resolveVisAxesDefinitions.js
@@ -0,0 +1,27 @@
+import omit from 'lodash/omit.js';
+/**
+ * Returns visualization axes config with the 'optional' property of each axis
+ * resolved based on overrideOptionalKey, overrideOptionalValue, and the passed overrideKeys
+ */
+export default function resolveVisAxesDefinitions(visAxes, overrideKeys) {
+    return Object.fromEntries(
+        Object.entries(visAxes).map(([axisType, axisDef]) => {
+            if (axisDef.optional && axisDef.overrideOptionalKey) {
+                const overrideValue = overrideKeys[axisDef.overrideOptionalKey];
+                const forceNonOptional =
+                    'overrideOptionalKeyValue' in axisDef
+                        ? overrideValue === axisDef.overrideOptionalKeyValue
+                        : overrideValue;
+                return [
+                    axisType,
+                    {
+                        ...omit(axisDef, ['overrideOptionalKey', 'overrideOptionalKeyValue']),
+                        optional: !forceNonOptional
+                    }
+                ];
+            } else {
+                return [axisType, axisDef];
+            }
+        })
+    );
+}
