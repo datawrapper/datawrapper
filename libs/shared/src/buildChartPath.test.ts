cafb2711149a3f5740af979ee99e8d32f201e92c
Julian Freyberg <62940341+jfreyberg@users.noreply.github.com>
2024-11-28 11:14:24 +0100

    chore(shared): move `buildChartPath` to shared (#7386)
    ## Motivation
    
    Towards: APP-3584
    
    ## Changes
    
    - move `buildChartPath` to shared
    - adjust tests: `vitest` -> `ava`
    
    ## Testing instructions for reviewer
    
    Confirm: Tests migrations are valid & import has been updated
    
    ## Remaining issues
    
    ## Checklist
    
    Please ensure that all affected **bundles are rebuilt**, all affected **UI is accessible**, and the guidelines for [secure development](https://www.notion.so/datawrapper/Development-3ebd0f0ac2c2401a8120f41aca8e02b4?pvs=4#f8851e29f3cb4dbf96af38f4e788bea8) were followed.
    
    - [x] Unit tests cover the change (or there is no unit test setup).
    - [x] Chart tests pass (or the PR doesn't change chart rendering or export).
    - [x] Changelog is updated (or the change doesn't need an entry).

---
+++ libs/shared/src/buildChartPath.test.ts
@@ -0,0 +1,46 @@
+import test from 'ava';
+import { buildChartPath } from './buildChartPath';
+
+const testCases = [
+    [{ chartId: 'ABCDE' }, '/edit/ABCDE/edit'],
+    [{ chartId: 'ABCDE', workspace: null, step: null, searchParams: null }, '/edit/ABCDE/edit'],
+    [{ workspace: 'test-workspace', chartId: 'ABCDE' }, '/test-workspace/edit/ABCDE/edit'],
+    [{ chartId: 'ABCDE', step: 'describe' }, '/edit/ABCDE/describe'],
+    [{ chartId: 'ABCDE', step: 'preview' }, '/edit/ABCDE/preview'],
+    [{ workspace: 'test', chartId: 'ABCDE', step: 'someStep' }, '/test/edit/ABCDE/someStep'],
+    [{ workspace: null, chartId: 'ABCDE', step: 'anotherStep' }, '/edit/ABCDE/anotherStep'],
+    [{ workspace: 'test', chartId: 'ABCDE', step: null }, '/test/edit/ABCDE/edit'],
+    [{ workspace: null, chartId: 'ABCDE', step: null }, '/edit/ABCDE/edit'],
+    [
+        {
+            chartId: 'ABCDE',
+            step: 'describe',
+            searchParams: new URLSearchParams({
+                comment: '123'
+            })
+        },
+        '/edit/ABCDE/describe?comment=123'
+    ],
+    [
+        {
+            chartId: 'ABCDE',
+            searchParams: new URLSearchParams({
+                a: '1',
+                b: '2'
+            })
+        },
+        '/edit/ABCDE/edit?a=1&b=2'
+    ]
+];
+
+for (const [input, expected] of testCases) {
+    test(`buildChartPath for ${JSON.stringify(input)} is "${expected}"`, t => {
+        const { workspace, chartId, step, searchParams } = input as {
+            workspace?: string | null;
+            chartId: string;
+            step?: string | null;
+            searchParams?: URLSearchParams | null;
+        };
+        t.is(buildChartPath({ workspace, chartId, step, searchParams }), expected as string);
+    });
+}
