1e34ec50cabbbe961eb05f99cca512b1b6db6b94
jfreyberg <62940341+jfreyberg@users.noreply.github.com>
2024-12-11 08:48:47 +0000

    chore(workspaces): add `buildCreatePath` util (#7451)
    ## Motivation
    
    Towards: APP-3590
    
    ## Changes
    
    - add `buildCreatePath` util
    - cover it with test cases
    
    ## Testing instructions for reviewer
    
    Confirm all relevant cases are handled and covered by tests.
    
    ## Remaining issues
    
    ## Checklist
    
    Please ensure that all affected **bundles are rebuilt**, all affected **UI is accessible**, and the guidelines for [secure development](https://www.notion.so/datawrapper/Development-3ebd0f0ac2c2401a8120f41aca8e02b4?pvs=4#f8851e29f3cb4dbf96af38f4e788bea8) were followed.
    
    - [x] Unit tests cover the change (or there is no unit test setup).
    - [x] Chart tests pass (or the PR doesn't change chart rendering or export).
    - [x] Changelog is updated (or the change doesn't need an entry).

---
+++ libs/shared/src/buildCreatePath.test.ts
@@ -0,0 +1,156 @@
+import test from 'ava';
+import { buildCreatePath } from './buildCreatePath';
+
+const testCases = [
+    [{ type: 'chart' }, '/create/chart'], // workspace can be omitted
+    [{ workspace: null, type: 'chart' }, '/create/chart'], // workspace can be null
+    [{ workspace: null, type: 'table' }, '/create/table'],
+    [{ workspace: null, type: 'map' }, '/select/map'],
+    [{ workspace: null, type: 'd3-maps-choropleth' }, '/create/map?type=d3-maps-choropleth'],
+    [{ workspace: null, type: 'd3-maps-symbols' }, '/create/map?type=d3-maps-symbols'],
+    [{ workspace: null, type: 'locator-map' }, '/create/map?type=locator-map'],
+    [{ workspace: 'some-workspace', type: 'chart' }, '/some-workspace/create/chart'],
+    [{ workspace: 'workspace', type: 'table' }, '/workspace/create/table'],
+    [{ workspace: 'workspace', type: 'map' }, '/workspace/select/map'],
+    [
+        { workspace: 'workspace', type: 'd3-maps-choropleth' },
+        '/workspace/create/map?type=d3-maps-choropleth'
+    ],
+    [
+        { workspace: 'workspace', type: 'd3-maps-symbols' },
+        '/workspace/create/map?type=d3-maps-symbols'
+    ],
+    [{ workspace: 'workspace', type: 'locator-map' }, '/workspace/create/map?type=locator-map'],
+
+    // in a team
+    [
+        { workspace: 'workspace', type: 'locator-map', teamId: 'aTeam' },
+        '/workspace/create/map?type=locator-map&team=aTeam'
+    ],
+    [
+        { workspace: 'workspace', type: 'd3-maps-choropleth', teamId: 'aTeam' },
+        '/workspace/create/map?type=d3-maps-choropleth&team=aTeam'
+    ],
+    [
+        { workspace: 'workspace', type: 'd3-maps-symbols', teamId: 'aTeam' },
+        '/workspace/create/map?type=d3-maps-symbols&team=aTeam'
+    ],
+    [{ workspace: 'workspace', type: 'map', teamId: 'aTeam' }, '/workspace/select/map?team=aTeam'],
+    [
+        { workspace: 'workspace', type: 'chart', teamId: 'aTeam' },
+        '/workspace/create/chart?team=aTeam'
+    ],
+    [{ workspace: null, type: 'chart', teamId: 'aTeam' }, '/create/chart?team=aTeam'],
+
+    // in a folder
+    [
+        { workspace: 'workspace', type: 'locator-map', folderId: 'folderA' },
+        '/workspace/create/map?type=locator-map&folder=folderA'
+    ],
+    [
+        { workspace: 'workspace', type: 'd3-maps-choropleth', folderId: 'folderA' },
+        '/workspace/create/map?type=d3-maps-choropleth&folder=folderA'
+    ],
+    [
+        { workspace: 'workspace', type: 'd3-maps-symbols', folderId: 'folderA' },
+        '/workspace/create/map?type=d3-maps-symbols&folder=folderA'
+    ],
+    [
+        { workspace: 'workspace', type: 'map', folderId: 'folderA' },
+        '/workspace/select/map?folder=folderA'
+    ],
+    [
+        { workspace: 'workspace', type: 'chart', folderId: 'folderA' },
+        '/workspace/create/chart?folder=folderA'
+    ],
+    [{ workspace: null, type: 'chart', folderId: 'folderA' }, '/create/chart?folder=folderA'],
+
+    // in a team and folder (only folder makes it to the path)
+    [
+        { workspace: 'workspace', type: 'locator-map', folderId: 'folderA', teamId: 'aTeam' },
+        '/workspace/create/map?type=locator-map&folder=folderA'
+    ],
+    [
+        { workspace: 'workspace', type: 'map', folderId: 'folderA', teamId: 'aTeam' },
+        '/workspace/select/map?folder=folderA'
+    ],
+    [
+        { workspace: 'workspace', type: 'chart', folderId: 'folderA', teamId: 'aTeam' },
+        '/workspace/create/chart?folder=folderA'
+    ],
+    [
+        { workspace: null, type: 'chart', folderId: 'folderA', teamId: 'aTeam' },
+        '/create/chart?folder=folderA'
+    ],
+
+    // office and embedded params
+    [
+        {
+            workspace: 'test-workspace',
+            type: 'locator-map',
+            folderId: 'folderA',
+            office: 'true',
+            embedded: 'true'
+        },
+        '/test-workspace/create/map?type=locator-map&folder=folderA&office=true&embedded=true'
+    ],
+    [
+        {
+            workspace: 'test-workspace',
+            type: 'chart',
+            folderId: 'folderA',
+            embedded: '1'
+        },
+        '/test-workspace/create/chart?folder=folderA&embedded=1'
+    ],
+    [
+        {
+            workspace: 'test-workspace',
+            type: 'chart',
+            embedded: '1'
+        },
+        '/test-workspace/create/chart?embedded=1'
+    ],
+    [
+        {
+            workspace: null,
+            type: 'map',
+            embedded: '1'
+        },
+        '/select/map?embedded=1'
+    ],
+    [
+        {
+            workspace: null,
+            type: 'map',
+            teamId: 'some-team',
+            embedded: null,
+            office: 'some-office',
+            randomIgnoredParam: 'random'
+        },
+        '/select/map?team=some-team&office=some-office'
+    ]
+];
+
+for (const [input, expected] of testCases) {
+    test(`buildCreatePath for ${JSON.stringify(input)} is "${expected}"`, t => {
+        const { workspace, teamId, folderId, office, embedded, type } = input as {
+            workspace?: string | null;
+            teamId?: string | null;
+            folderId?: string | number | null;
+            office?: string | boolean | undefined | null;
+            embedded?: string | boolean | undefined | null;
+            type:
+                | 'chart'
+                | 'map'
+                | 'table'
+                | 'd3-maps-choropleth'
+                | 'd3-maps-symbols'
+                | 'locator-map';
+        };
+        t.is(
+            buildCreatePath({ workspace, teamId, folderId, office, embedded, type }),
+            expected as string
+        );
+    });
+}
---
+++ libs/shared/src/buildCreatePath.ts
@@ -0,0 +1,62 @@
+/**
+ * Construct the path to an visualization creation route given a workspace and type.
+ * There is no validation of the parameters, so the caller is responsible for ensuring that they are valid.
+ */
+
+export type CreateChartType =
+    | 'chart'
+    | 'map'
+    | 'table'
+    | 'd3-maps-choropleth'
+    | 'd3-maps-symbols'
+    | 'locator-map';
+
+export function buildCreatePath({
+    workspace,
+    teamId,
+    folderId,
+    type,
+    office,
+    embedded
+}: {
+    workspace?: string | null | undefined; // TODO: @launch:workspaces - once all data is migrated to workspaces, this parameter should become required
+    type: CreateChartType;
+    teamId?: string | null | undefined;
+    folderId?: string | number | null | undefined;
+    office?: string | boolean | undefined | null;
+    embedded?: string | boolean | undefined | null;
+}): string {
+    let path = '/';
+
+    const params = new URLSearchParams();
+    if (workspace) {
+        path += `${workspace}/`;
+    }
+    if (type === 'map') {
+        path += `select/${type}`;
+    } else if (type === 'chart' || type === 'table') {
+        path += `create/${type}`;
+    } else {
+        path += `create/map`;
+        params.set('type', type);
+    }
+
+    if (folderId) {
+        params.set('folder', folderId.toString());
+    } else if (teamId) {
+        params.set('team', teamId);
+    }
+
+    if (office) {
+        params.set('office', office.toString());
+    }
+    if (embedded) {
+        params.set('embedded', embedded.toString());
+    }
+
+    if (params.toString()) {
+        path += `?${params.toString()}`;
+    }
+
+    return path;
+}
