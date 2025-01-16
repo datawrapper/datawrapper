6bc688baefd7aedca2d55ac4a7cdc1090eae3eec
Ivan Lokhov <ilokhov@users.noreply.github.com>
2025-01-16 17:33:18 +0100

    chore(shared): pass chroma-js to colorLightness (#7716)

--- libs/shared/src/colorLightness.ts
+++ libs/shared/src/colorLightness.ts
@@ -1,31 +1,21 @@
-// There are no type definitions for these modules.
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import hex2rgb from 'chroma-js/src/io/hex/hex2rgb.js';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import rgb2lab from 'chroma-js/src/io/lab/rgb2lab.js';
-
 /**
- * Returns the Lab lightness value of a given hexidecimal
- * RGB color. Uses chroma-js to convert from Hex to Lab, but
- * only adds a few hundreds bytes to your build.
- *
- * To use this function, you have to manually install chroma-js using
- * `npm install chroma-js`.
- *
- * @deprecated please use the shared chroma instance
+ * Returns the Lab lightness value of a given hexidecimal RGB color.
  *
  * @exports colorLightness
  * @kind function
  *
  * @param {string} hexColor - the RGB color as hexadecimal string, e.g. "#330066"
+ * @param chroma - chroma instance
  * @returns {number} - the L*a*b lightness, between 0 (black) and 100 (white)
  *
  * @example
  * import colorLightness from '@datawrapper/shared/colorLightness';
- * colorLightness('#ff3399') // 57.9
+ * colorLightness('#ff3399', chroma) // 57.9
  */
-export default function colorLightness(hexColor: string): number {
-    return rgb2lab(hex2rgb(hexColor))[0];
+
+export default function colorLightness(
+    hexColor: string,
+    chroma: typeof import('chroma-js')
+): number {
+    return chroma(hexColor).lab()[0];
 }
