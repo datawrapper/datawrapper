a5e405a4c812847f1eda8b737da0bb8d71952833
Julian Freyberg <62940341+jfreyberg@users.noreply.github.com>
2025-02-12 10:35:14 +0100

    chore(settings): create `settings.sso` schema (#7873)

---
+++ libs/shared/src/schemas/settings/sso.ts
@@ -0,0 +1,33 @@
+import Joi from 'joi';
+
+const protocolsSchema = Joi.object({
+    openId: Joi.object({
+        clientSecret: Joi.string().allow(null).allow(''),
+        clientId: Joi.string().allow(null).allow(''),
+        domain: Joi.string().allow(null).allow(''),
+    }),
+    saml: Joi.object({
+        certificate: Joi.string().allow(null).allow(''),
+        disableRequestedAuthnContext: Joi.boolean().default(false),
+        entityId: Joi.string().allow(null).allow(''),
+        url: Joi.string().allow(null).allow(''),
+    }),
+});
+
+export const ssoSchema = Joi.object({
+    protocols: protocolsSchema,
+    protocol: Joi.string().allow('openId').allow('saml'),
+    enabled: Joi.boolean().default(false),
+    automaticProvisioning: Joi.boolean().default(false),
+});
+
+export const ssoSecretsSchema = Joi.object({
+    protocols: Joi.object({
+        openId: Joi.object({
+            clientSecret: Joi.string().allow(null).allow(''),
+        }),
+        saml: Joi.object({
+            certificate: Joi.string().allow(null).allow(''),
+        }),
+    }),
+});
