8ac3554a6a719f426b789b1a7fdd744990757abd
Linus Aarnio <42450444+linusaarnio@users.noreply.github.com>
2025-02-13 16:15:21 +0200

    fix(security): prevent HTML injection in notification emails (#7976)

---
+++ libs/shared/src/comments.ts
@@ -0,0 +1,16 @@
+export const allowedCommentTags = [
+    'p',
+    'span', // mentions
+    'br',
+    'a',
+    'strong', // bold
+    'em', // italic
+    'u', // underlined
+    's', // strikethrough
+    'ul',
+    'ol',
+    'li',
+    'blockquote',
+    'code', // inline code + code blocks
+    'pre', // code-blocks
+];
