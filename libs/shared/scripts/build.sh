bc6761b167f9684ca12a303913b277ffbfec6f4c
Pascal Bürkle <25956751+pabueco@users.noreply.github.com>
2025-02-14 18:54:19 +0100

    chore(code): improve post-checkout script & reduce build time of untracked libs (#7966)

--- libs/shared/scripts/build.sh
+++ libs/shared/scripts/build.sh
@@ -8,38 +8,55 @@ set -eu
 dir=$(cd "$(dirname "$0")" || exit 1; pwd -P)
 cd "$dir"/..
 
-# Build CJS into dist.new/cjs
-echo "Building CJS modules..."
-rm -Rf dist.new/*
-pnpm exec tsc --project ./tsconfig.build.cjs.json --outDir dist.new/cjs
+build_args=""
 
-echo "Transforming CJS modules..."
+# Check if incremental build should be used by checking if the arguments contain the flag.
+case "$*" in
+  *--incremental*)
+    build_args="--incremental"
+    ;;
+  *)
+    rm -Rf dist.new/*
+    ;;
+esac
+
 (
-# Change extensions to .cjs, so that the modules can be imported from this `type: module` package.
-find dist.new/cjs -name '*.js' -exec sh -c 'mv -v "$1" "${1%.js}.cjs"' _ {} \;
-find dist.new/cjs -name '*.js.map' -exec sh -c 'mv -v "$1" "${1%.js.map}.cjs.map"' _ {} \;
-find dist.new/cjs -name '*.ts' -exec sh -c 'mv -v "$1" "${1%.ts}.cts"' _ {} \;
-find dist.new/cjs -name '*.ts.map' -exec sh -c 'mv -v "$1" "${1%.ts.map}.cts.map"' _ {} \;
-if sed --help 2>&1 | grep -Eq 'GNU sed|BusyBox'; then
-    find dist.new/cjs -name '*.cjs' -exec sed -i -E 's|require\("(\.{1,2}/.+)\.js"\)|require("\1.cjs")|' {} \;
-    find dist.new/cjs -name '*.cts.map' -exec sed -i -E 's|"file":"(.+).d.ts"|"file":"\1.d.cts"|' {} \;
-else
-    find dist.new/cjs -name '*.cjs' -exec sed -i '' -E 's|require\("(\.{1,2}/.+)\.js"\)|require("\1.cjs")|' {} \;
-    find dist.new/cjs -name '*.cts.map' -exec sed -i '' -E 's|"file":"(.+).d.ts"|"file":"\1.d.cts"|' {} \;
-fi
-) > /dev/null
+    # Build CJS into dist.new/cjs
+    echo "Building CJS modules..."
+    pnpm exec tsc ${build_args} --project ./tsconfig.build.cjs.json --outDir dist.new/cjs
 
-# Build ESM into dist.new/esm
-echo "Building ESM modules..."
-pnpm exec tsc --project ./tsconfig.build.esm.json --outDir dist.new/esm
+    echo "Transforming CJS modules..."
+    (
+    # Change extensions to .cjs, so that the modules can be imported from this `type: module` package.
+    find dist.new/cjs -name '*.js' -exec sh -c 'mv -v "$1" "${1%.js}.cjs"' _ {} \;
+    find dist.new/cjs -name '*.js.map' -exec sh -c 'mv -v "$1" "${1%.js.map}.cjs.map"' _ {} \;
+    find dist.new/cjs -name '*.ts' -exec sh -c 'mv -v "$1" "${1%.ts}.cts"' _ {} \;
+    find dist.new/cjs -name '*.ts.map' -exec sh -c 'mv -v "$1" "${1%.ts.map}.cts.map"' _ {} \;
+    if sed --help 2>&1 | grep -Eq 'GNU sed|BusyBox'; then
+        find dist.new/cjs -name '*.cjs' -exec sed -i -E 's|require\("(\.{1,2}/.+)\.js"\)|require("\1.cjs")|' {} \;
+        find dist.new/cjs -name '*.cts.map' -exec sed -i -E 's|"file":"(.+).d.ts"|"file":"\1.d.cts"|' {} \;
+    else
+        find dist.new/cjs -name '*.cjs' -exec sed -i '' -E 's|require\("(\.{1,2}/.+)\.js"\)|require("\1.cjs")|' {} \;
+        find dist.new/cjs -name '*.cts.map' -exec sed -i '' -E 's|"file":"(.+).d.ts"|"file":"\1.d.cts"|' {} \;
+    fi
+    ) > /dev/null
+) &
+(
+    # Build ESM into dist.new/esm
+    echo "Building ESM modules..."
+    pnpm exec tsc ${build_args} --project ./tsconfig.build.esm.json --outDir dist.new/esm
+)
+
+# Wait for both build processes to finish
+wait
 
 # Merge the content of dist.new/ into dist/ to avoid downtime
 echo "Merging dist.new/ into dist/..."
 (
 if [ -e dist ]; then
-    cp -av dist.new/* dist
+    cp -a dist.new/* dist
 else
-    cp -av dist.new dist
+    cp -a dist.new dist
 fi
 ) > /dev/null
 echo "Done."
