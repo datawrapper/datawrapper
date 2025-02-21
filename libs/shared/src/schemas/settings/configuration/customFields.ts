3bcdcb08e1d4814e7950f79d56c4ff610f17ed50
Julian Freyberg <62940341+jfreyberg@users.noreply.github.com>
2025-02-21 12:31:11 +0100

    feat(settings): `customFields` migration script (#7957)

--- libs/shared/src/schemas/settings/configuration/customFields.ts
+++ libs/shared/src/schemas/settings/configuration/customFields.ts
@@ -3,9 +3,10 @@ import Joi from 'joi';
 const CustomFieldSchema = Joi.object({
     key: Joi.string().required(),
     description: Joi.string().required(),
-    items: Joi.string().allow(''),
+    items: Joi.array().items(Joi.string()).optional(),
     title: Joi.string().required(),
     type: Joi.string().required().valid('radio', 'checkbox', 'textArea', 'text'),
+    displayInArchive: Joi.boolean().default(false),
 });
 
 export const customFieldsSchema = Joi.object({
