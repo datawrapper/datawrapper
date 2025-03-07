816ef6e5ba77cac55a85981e663174ab7bd1227a
Pascal Bürkle <25956751+pabueco@users.noreply.github.com>
2025-03-07 17:18:17 +0100

    fix(collaboration): queue and batch updates & remove direct broadcast to other clients  (#8232)

--- libs/shared/src/collaboration.ts
+++ libs/shared/src/collaboration.ts
@@ -11,3 +11,15 @@ export const CHART_EDITOR_WRITABLE_KEYS = [
     'publishedAt',
     'publicUrl',
 ];
+
+export const EVENTS = {
+    CHART_UPDATES: 'chart.updates',
+    CHART_DATA_UPDATES: 'chart.data.updates',
+    CHART_EVENT: 'chart.event',
+    // The publish state is emitted by the client that triggered the publish.
+    // So this is a client event and needs to be prefixed by `client-`.
+    // See Pusher docs: https://pusher.com/docs/channels/using_channels/events/#triggering-client-events
+    CHART_PUBLISH_STATE: 'client-chart.publish.state',
+} as const;
+
+export const SOCKET_ID_HEADER = 'x-socket-id';
