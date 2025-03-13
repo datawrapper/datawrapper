e6fc5fcea5aa98de98e7159390fb9f51d939fea4
Julian Freyberg <62940341+jfreyberg@users.noreply.github.com>
2025-03-13 14:35:45 +0100

    chore(settings): adjust settings migrations post prod test #2 (#8345)

--- libs/shared/src/schemas/settings/configuration/hidden.ts
+++ libs/shared/src/schemas/settings/configuration/hidden.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const hiddenSchema = Joi.object({
     enableEditThisChart: Joi.boolean().default(false),
-    zipEmbedJs: Joi.boolean().default(false),
+    zipEmbedJs: Joi.string().allow(null).default(null).valid('embed.js'),
     privateBasemapGroups: Joi.object().pattern(Joi.string(), Joi.boolean()),
     customFormats: Joi.object({
         date: Joi.array().items(
